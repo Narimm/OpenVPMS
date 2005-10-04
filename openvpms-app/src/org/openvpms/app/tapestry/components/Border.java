0c1c37a23cc21ef701353e821e6550b92d68babe
Tony De Keizer <tony@verticalconnect.net>
2005-10-04 02:53:21 +0000

    svn path=/trunk/openvpms-app/; revision=172

--- openvpms-app/src/org/openvpms/app/tapestry/components/Border.java
+++ openvpms-app/src/org/openvpms/app/tapestry/components/Border.java
@@ -1,8 +1,22 @@
 /*
- * Created on 19.06.2004
+ *  Version: 1.0
  *
- * 
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
  */
+
+
 package org.openvpms.app.tapestry.components;
 
 
@@ -15,9 +29,9 @@ import org.openvpms.component.presentation.tapestry.component.OpenVpmsComponent;
  * 
  * The Border component's corresponding java class file. Mainly used for
  * navigation and the application's overall look and feel.
- * 
- * @author andyman
- * 
+ *
+ * @author   <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version  $LastChangedDate$
  */
 public abstract class Border extends OpenVpmsComponent implements
         PageRenderListener {
@@ -44,8 +58,4 @@ public abstract class Border extends OpenVpmsComponent implements
 
 		
     }
-
-
-
-
 }
