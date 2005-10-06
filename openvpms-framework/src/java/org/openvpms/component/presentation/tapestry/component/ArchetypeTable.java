78498a33e0a7d9f0b60c5db8d75a69003240520e
Tony De Keizer <tony@verticalconnect.net>
2005-10-06 23:51:05 +0000

    Updated presentation components and pages.
    svn path=/trunk/openvpms-framework/; revision=187

---
+++ openvpms-framework/src/java/org/openvpms/component/presentation/tapestry/component/ArchetypeTable.java
@@ -0,0 +1,103 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
+ */
+
+package org.openvpms.component.presentation.tapestry.component;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.tapestry.IRequestCycle;
+
+/**
+ * 
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate$
+ */
+public abstract class ArchetypeTable extends ArchetypeComponent {
+    public abstract List getInstances();
+
+    public abstract void setInstances(List instances);
+
+    public abstract Object getCurrentObject();
+
+    public abstract void setCurrentObject(Object CurrentObject);
+
+    private List selected = new ArrayList();
+
+    protected void prepareForRender(IRequestCycle arg0) {
+        // TODO Auto-generated method stub
+        super.prepareForRender(arg0);
+        buildSelectedList();
+    }
+
+    void buildSelectedList() {
+        if (getInstances() != null) {
+            selected = new ArrayList();
+            for (Iterator iter = getInstances().iterator(); iter.hasNext();) {
+                iter.next();
+                selected.add(new Boolean(false));
+            }
+        }
+    }
+
+    public void showAddPage(IRequestCycle cycle) {
+    }
+
+    public void remove(IRequestCycle cycle) {
+        int i = 0;
+        ArrayList deleting = new ArrayList();
+        for (Iterator iter = getInstances().iterator(); iter.hasNext();) {
+
+            Object element = (Object) iter.next();
+
+            if (((Boolean) getSelected().get(i)).booleanValue()) {
+                deleting.add(element);
+            }
+            i++;
+        }
+        getInstances().removeAll(deleting);
+    }
+
+    public List getSelectedList() {
+        ArrayList selectedList = new ArrayList();
+        selectedList.addAll(getInstances());
+        return selectedList;
+    }
+
+    public void setSelectedList(List selected) {
+        if (selected != null) {
+            getInstances().clear();
+            getInstances().addAll(selected);
+        }
+    }
+
+    /**
+     * @return Returns the toBeDeleted.
+     */
+    public List getSelected() {
+        return selected;
+    }
+
+    /**
+     * @param toBeDeleted
+     *            The toBeDeleted to set.
+     */
+    public void setSelected(List toBeDeleted) {
+        this.selected = toBeDeleted;
+    }
+}
---
+++ openvpms-framework/src/java/org/openvpms/component/presentation/tapestry/component/EditLink.java
@@ -0,0 +1,58 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
+ */
+
+
+package org.openvpms.component.presentation.tapestry.component;
+
+import org.apache.tapestry.IRequestCycle;
+import org.openvpms.component.business.domain.im.common.IMObject;
+import org.openvpms.component.business.service.archetype.descriptor.ArchetypeDescriptor;
+import org.openvpms.component.presentation.tapestry.page.EditPage;
+import org.openvpms.component.presentation.tapestry.page.OpenVpmsPage;
+
+/**
+ *
+ * @author   <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version  $LastChangedDate$
+ */
+public abstract class EditLink extends Link
+{
+  
+    public abstract ArchetypeDescriptor getArchetypeDescriptor();
+
+    public abstract void setArchetypeDescriptor(ArchetypeDescriptor archetypeDescriptor);
+    
+    public static String SUFFIX = "Edit";
+
+    public String getArchetypeName()
+    {
+        return ((IMObject)getModel()).getArchetypeId().getShortName();
+    }
+
+    public abstract Object getModel();
+
+    public abstract void setModel(Object model);
+
+    public void click(IRequestCycle cycle)
+    {
+        EditPage page = (EditPage) findPage(cycle, SUFFIX);
+        page.setModel(getModel());
+        ((OpenVpmsPage)getPage()).pushCallback();
+        cycle.activate(page);
+    }
+}
