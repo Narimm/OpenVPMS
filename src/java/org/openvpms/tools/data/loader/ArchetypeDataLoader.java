2da563e44d2683b63875ea1200d14bb84db11550
jalateras <jalateras@9e4fa352-aafb-0310-9071-df624684024b>
2006-02-22 04:43:17 +0000

    process a directory of files
    git-svn-id: svn://localhost/openvpms/framework/trunk@548 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/tools/data/loader/ArchetypeDataLoader.java
+++ src/java/org/openvpms/tools/data/loader/ArchetypeDataLoader.java
@@ -20,19 +20,26 @@ package org.openvpms.tools.data.loader;
 
 // java core
 import java.io.File;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 // log4j
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.ConsoleAppender;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
 
 // hibernate
 import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
+import org.jdom.output.XMLOutputter;
 
 // spring framework
 import org.springframework.context.ApplicationContext;
@@ -46,6 +53,12 @@ import org.openvpms.component.business.domain.im.common.IMObjectReference;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.archetype.ValidationException;
 import org.openvpms.component.business.service.archetype.ValidationError;
+import org.openvpms.tools.archetype.loader.ArchetypeLoader;
+
+import com.martiansoftware.jsap.FlaggedOption;
+import com.martiansoftware.jsap.JSAP;
+import com.martiansoftware.jsap.JSAPResult;
+import com.martiansoftware.jsap.Switch;
 
 /**
  * This tool will reads the specified XML document and process all of the 
@@ -64,8 +77,8 @@ public class ArchetypeDataLoader {
     /**
      * Define a logger for this class
      */
-    @SuppressWarnings("unused")
-    private static final Logger logger = Logger.getLogger(ArchetypeDataLoader.class);
+    private Logger logger;
+
 
     /**
      * A reference to the application context
@@ -84,9 +97,20 @@ public class ArchetypeDataLoader {
         new HashMap<String, IMObjectReference>();
     
     /**
-     * The name of the file holding the data
+     * Indicates whether the directory search is recursive
      */
-    private String fileName; 
+    private boolean subdir;
+    
+    /**
+     * Specifies the file extension to filter. Defaults to adl
+     */
+    private String extension = "xml";
+    
+    /**
+     * Indicates whether to display verbose information while loading the 
+     * archetypes
+     */
+    private boolean verbose;
     
     /**
      * A reference to the data that will be processed
@@ -94,15 +118,35 @@ public class ArchetypeDataLoader {
     private Document data;
 
     /**
+     * Used to process the command line
+     */
+    private JSAP jsap = new JSAP();
+    
+    /**
+     * The results once the commnad line has been processed
+     */
+    private JSAPResult config;
+    
+    /**
+     * A reference to an XMLOutputter, which is used to format
+     * XML data
+     */
+    private XMLOutputter formatter = new XMLOutputter();
+    
+    /**
      * Process the records in the specified XML document
      * 
-     * @param fileName
-     *            the file that holds the data
+     * @param args
+     *            the command line
      */
-    public ArchetypeDataLoader(String fileName) throws Exception {
+    public ArchetypeDataLoader(String[] args) throws Exception {
         // init
         init();
-        this.fileName = fileName;
+        
+        config = jsap.parse(args);
+        if (!config.success()) {
+            displayUsage();
+        }
     }
 
     /**
@@ -113,30 +157,62 @@ public class ArchetypeDataLoader {
      *            argument
      */
     public static void main(String[] args) throws Exception {
-        logger.info("Start Loading Archetype Instance Data");
-        ArchetypeDataLoader loader = new ArchetypeDataLoader(args[0]);
-        loader.processData();
-        logger.info("End Loading Archetype Instance Data");
+        ArchetypeDataLoader loader = new ArchetypeDataLoader(args);
+        loader.load();
     }
 
     /**
+     * Load all the elements
+     * 
+     * @throws Exception
+     */
+    private void load() throws Exception {
+        verbose = config.getBoolean("verbose");
+        if (config.getString("file") != null) {
+            processFile(config.getString("file"));
+        } else if (config.getString("dir") != null) {
+            processDir(config.getString("dir"));
+        } else {
+            displayUsage();
+        }
+    }
+        
+    /**
+     * Process all the files in the directory
+     * 
+     * @param
+     *      the directory
+     */
+    private void processDir(String dir) throws Exception {
+        String[] extensions = {extension};
+        Collection collection = FileUtils.listFiles(new File(dir), 
+                extensions, subdir);
+        for (Object obj : collection) {
+            File file = (File)obj;
+            processFile(file.getAbsolutePath());
+        }
+    }
+    
+    /**
      * Process the data elements. 
      * 
+     * @param file
+     *            the file to process
      * @throws Exception
      *            propagate all exceptions to client
      */
-    private void processData() throws Exception {
+    private void processFile(String file) throws Exception {
         for (int iteration = 0; iteration < 2; iteration++) { 
             boolean firstParse = (iteration == 0 ? true : false);
             
             if (firstParse) {
-                logger.debug("Executing first parse...");
+                logger.info("\n[Executing first parse for : " + file + "]\n");
             } else {
-                logger.debug("Executing second parse...");
+                logger.info("\n[Executing second parse for : " + file + "]\n");
             }
             
             reset();
-            Element root = getData().getRootElement();
+            Element root = getData(file).getRootElement();
             for (Object obj1 : root.getChildren())  {
                 processElement(null, (Element)obj1, firstParse);
             }
@@ -166,7 +242,6 @@ public class ArchetypeDataLoader {
     private IMObject processElement(IMObject parent, Element elem, boolean firstParse) 
     throws Exception {
         IMObject object = null;
-
         if (elem.getName().equals("data")) {
             // if its the first parse then don't process data that
             // contains attribute value references.
@@ -187,17 +262,16 @@ public class ArchetypeDataLoader {
             // display an error.
             String shortName = elem.getAttributeValue("archetype");
             if (StringUtils.isEmpty(shortName)) {
-                logger.error("Failed to process " 
-                        + elem.toString()
-                        + " because it does not define an -arhetype- attribute");
+                logger.error("\n[ERR: No archetype attribute defined" 
+                        + "]\n" + formatter.outputString(elem));
                 return object;
             }
 
             ArchetypeDescriptor adesc = archetypeService
                 .getArchetypeDescriptor(shortName);
             if (adesc == null) {
-                logger.error("Failed to process element because "
-                        + "there is no archetype defined for " + shortName);
+                logger.error("\n[ERR: No archetype defined for  " + shortName 
+                        + "]\n" + formatter.outputString(elem));
                 return object;
             }
 
@@ -216,9 +290,8 @@ public class ArchetypeDataLoader {
                 
                 NodeDescriptor ndesc = adesc.getNodeDescriptor(attr.getName());
                 if (ndesc == null) {
-                    logger.error("Failed to process " + elem.toString()
-                            + " because the attribute " + attr.getName() 
-                            + " is invalid");
+                    logger.error("\n[ERR: Invalid Attribute " + attr.getName() 
+                            + "]\n" + formatter.outputString(elem));
                         valid = false;
                         break;
                     }
@@ -243,10 +316,9 @@ public class ArchetypeDataLoader {
                         ndesc.setValue(object, attr.getValue());
                     }
                 } catch (Exception exception) {
-                    logger.error("Failed to process " + elem.toString() 
-                            + ". Error trying to set the attribute " 
-                            + attr.getName() + " with value " + attr.getValue(),
-                            exception);
+                    logger.error("\n[ERR: Trying to set attr " + attr.getName() 
+                            + " with value " + attr.getValue() 
+                            + "]\n" + exception + formatter.outputString(elem));
                     valid = false;
                     break;
                 }
@@ -264,9 +336,8 @@ public class ArchetypeDataLoader {
                     Element childElem = (Element)obj1;
                     String collectionNode = childElem.getAttributeValue("collection");
                     if (StringUtils.isEmpty(collectionNode)) {
-                        logger.error("Failed to process " 
-                                + childElem.toString()
-                                + " a child element must have a collection attribute");
+                        logger.error("\n[ERR: Failed to process child element "  
+                                + "]\n" + formatter.outputString(childElem));
                         continue;
                     }
                     IMObject child = processElement(object, childElem, firstParse);
@@ -275,10 +346,9 @@ public class ArchetypeDataLoader {
                         (ndesc.isParentChild())){
                         ndesc.addChildToCollection(object, child);
                     } else {
-                        logger.error("Failed to process " 
-                                + childElem.toString()
-                                + ". The collection node name must be a "
-                                + "parent-child collection");
+                        logger.error("\n[ERR: Failed to process child element. " 
+                                + "The collection node must a parent-child coll"
+                                + "]\n" + formatter.outputString(childElem));
                         continue;
                     }
                 }
@@ -293,21 +363,20 @@ public class ArchetypeDataLoader {
                         idCache.put(id, new IMObjectReference(object));
                     }
                     
-                    logger.debug("Creating data with archetype: " 
-                            + object.getArchetypeId().getShortName()
-                            + " and name " 
-                            + object.getName());
+                    if (verbose) {
+                        logger.info("\n[PROCESSED]\n" + formatter.outputString(elem));
+                    }
                 }
             } catch(ValidationException validation) {
-                logger.error("Failed to process " + elem.toString()
-                        + ". Got the following validation errors:"); 
+                logger.error("\n[ERR: Failed to process element. " 
+                        + "Validation errors follow].");
                 for (ValidationError error : validation.getErrors()) {
                     logger.error(error.toString());
                 }
+                logger.error(formatter.outputString(elem));
             } catch (Exception exception) {
-                logger.error("Failed to process " + elem.toString()
-                        + ". Error trying to save the object" 
-                        + ". [Exception] " + exception.toString());
+                logger.error("\n[ERR: Frror trying to save object" 
+                        + "]\n" + exception + formatter.outputString(elem));
             }
         }
         
@@ -351,6 +420,10 @@ public class ArchetypeDataLoader {
         context =  new ClassPathXmlApplicationContext(
                 "org/openvpms/tools/data/loader/archetype-data-loader-appcontext.xml");
         archetypeService = (IArchetypeService)context.getBean("archetypeService");
+        
+        // set up the options for the command line and creater the logger
+        createOptions();
+        createLogger();
     }
     
     /**
@@ -363,15 +436,79 @@ public class ArchetypeDataLoader {
     /**
      * Get a reference to the data. This is a stateful attribute
      * 
+     * @param file
+     *            the file
      * @return Document
      */
-    private Document getData() 
+    private Document getData(String file) 
     throws Exception {
         if (data == null) {
             SAXBuilder builder = new SAXBuilder();
-            data = builder.build(new File(fileName));
+            data = builder.build(new File(file));
         }
         
         return data;
     }
+
+    /**
+     * Configure the options for this applications
+     * 
+     * @throws Exception
+     *            let the caller handle the error
+     */
+    private void createOptions() 
+    throws Exception {
+        jsap.registerParameter(new FlaggedOption("dir")
+                .setShortFlag('d')
+                .setLongFlag("dir")
+                .setHelp("Directory where data files reside."));
+        jsap.registerParameter(new Switch("subdir")
+                .setShortFlag('s')
+                .setLongFlag("subdir")
+                .setDefault("false")
+                .setHelp("Search the subdirectories as well."));
+        jsap.registerParameter(new FlaggedOption("file")
+                .setShortFlag('f')
+                .setLongFlag("file")
+                .setHelp("Name of file containing data"));
+        jsap.registerParameter(new Switch("verbose")
+                .setShortFlag('v')
+                .setLongFlag("verbose")
+                .setDefault("false")
+                .setHelp("Displays verbose info to the console."));
+    }
+
+    /**
+     * Creater the logger
+     *
+     * @throws Exception
+     */
+    private void createLogger() 
+    throws Exception {
+        BasicConfigurator.configure(); 
+
+        // set the root logger level to error
+        Logger.getRootLogger().setLevel(Level.ERROR);
+        Logger.getRootLogger().removeAllAppenders();
+        
+        logger = Logger.getLogger(ArchetypeLoader.class);
+        logger.setLevel(Level.INFO);
+        logger.addAppender(new ConsoleAppender(new PatternLayout("%m%n")));
+    }
+    
+    /**
+     * Print usage information
+     */
+    private void displayUsage() {
+        System.err.println();
+        System.err.println("Usage: java "
+                            + ArchetypeDataLoader.class.getName());
+        System.err.println("                "
+                            + jsap.getUsage());
+        System.err.println();
+        System.err.println(jsap.getHelp());
+        System.exit(1);
+        
+    }
+
 }
