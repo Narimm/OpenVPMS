1eafe1beee21985de7123b610701ab3dfa1ac9fb
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-03-29 05:41:38 +0000

    Added support for OVPMS-256 Extension Functions - provide a function to return a formatted contact string for a party
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@694 9e4fa352-aafb-0310-9071-df624684024b

---
+++ src/java/org/openvpms/archetype/function/party/PartyFunctions.java
@@ -0,0 +1,158 @@
+package org.openvpms.archetype.function.party;
+
+import java.util.List;
+
+import org.apache.commons.jxpath.ExpressionContext;
+import org.apache.commons.jxpath.Pointer;
+
+import org.openvpms.component.business.domain.im.archetype.descriptor.ArchetypeDescriptor;
+import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
+import org.openvpms.component.business.domain.im.common.IMObject;
+import org.openvpms.component.business.domain.im.party.Contact;
+import org.openvpms.component.business.domain.im.party.Party;
+import org.openvpms.component.business.service.archetype.IArchetypeService;
+
+
+/**
+ * JXPath extension functions that operate on {@link Party} instances.
+ *
+ * @author <a href="mailto:tma@netspace.net.au">Tim Anderson</a>
+ * @version $LastChangedDate$
+ */
+public class PartyFunctions {
+
+    /**
+     * The archetype service.
+     */
+    private static IArchetypeService _service;
+
+
+    /**
+     * Construct a new <code>PartyFunctions</code>.
+     *
+     * @param service the archetype service
+     */
+    public PartyFunctions(IArchetypeService service) {
+        _service = service;
+    }
+
+    /**
+     * Returns a stringified form of a party's contacts.
+     *
+     * @param context the expression context. Expected to reference a party.
+     * @return the stringified form of the party's contacts or
+     *         <code>null</code>
+     */
+    public static String contacts(ExpressionContext context) {
+        Pointer pointer = context.getContextNodePointer();
+        if (pointer == null || !(pointer.getValue() instanceof Party)) {
+            return null;
+        }
+        StringBuffer result = new StringBuffer();
+
+        Party party = (Party) pointer.getValue();
+        for (Contact contact : party.getContacts()) {
+            String description = getContactDescription(contact);
+            if (description != null) {
+                if (result.length() != 0) {
+                    result.append(", ");
+                }
+                result.append(description);
+            }
+        }
+        return result.toString();
+    }
+
+    /**
+     * Returns the description of a contact.
+     *
+     * @param contact the contact
+     * @return the description of <code>object</code>. May be <code>null</code>
+     */
+    private static String getContactDescription(Contact contact) {
+        String shortName = contact.getArchetypeId().getShortName();
+        ArchetypeDescriptor archetype
+                = _service.getArchetypeDescriptor(shortName);
+
+        StringBuffer result = new StringBuffer();
+        String description = getValue(contact, "description", archetype);
+        if (description != null) {
+            result.append(description);
+        }
+
+        NodeDescriptor purposes
+                = archetype.getNodeDescriptor("purposes");
+
+        if (purposes != null) {
+            List<IMObject> list = purposes.getChildren(contact);
+            if (!list.isEmpty()) {
+                if (result.length() != 0) {
+                    result.append(" ");
+                }
+                result.append("(");
+                result.append(getValues(list, "name"));
+                result.append(")");
+            }
+        }
+        return result.toString();
+    }
+
+    /**
+     * Returns a concatenated list of values for a set of objects.
+     *
+     * @param objects the objects
+     * @param node    the node name
+     * @return the stringified value of <code>node</code> for each object,
+     *         separated by ", "
+     */
+    private static String getValues(List<IMObject> objects, String node) {
+        StringBuffer result = new StringBuffer();
+
+        for (IMObject object : objects) {
+            ArchetypeDescriptor archetype = getArchetype(object);
+            if (archetype != null) {
+                String value = getValue(object, node, archetype);
+                if (value != null) {
+                    if (result.length() != 0) {
+                        result.append(", ");
+                    }
+                    result.append(value);
+                }
+            }
+        }
+        return result.toString();
+    }
+
+    /**
+     * Returns the stringified value of a node.
+     *
+     * @param object    the object
+     * @param node      the node name
+     * @param archetype the archetype's descriptor
+     * @return the stringified value of <code>node</code>. May be
+     *         <code>null</code>
+     */
+    private static String getValue(IMObject object, String node,
+                                   ArchetypeDescriptor archetype) {
+        String result = null;
+        NodeDescriptor descriptor = archetype.getNodeDescriptor(node);
+        if (descriptor != null) {
+            Object value = descriptor.getValue(object);
+            if (value != null) {
+                result = value.toString();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Helper to return an object's archetype descriptor.
+     *
+     * @param object the object
+     * @return the object's archetype descriptor. May be <code>null</code>
+     */
+    private static ArchetypeDescriptor getArchetype(IMObject object) {
+        String shortName = object.getArchetypeId().getShortName();
+        return _service.getArchetypeDescriptor(shortName);
+    }
+}
