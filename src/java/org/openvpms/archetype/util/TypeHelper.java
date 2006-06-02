a551005bc4c5a09d6a18f575e246902075e6564a
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-06-02 01:43:27 +0000

    Added rules to cascade deletes of act.patientClinicalEpisode and act.patientClinicalEvent
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@952 9e4fa352-aafb-0310-9071-df624684024b

---
+++ src/java/org/openvpms/archetype/util/TypeHelper.java
@@ -0,0 +1,95 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
+ */
+
+package org.openvpms.archetype.util;
+
+import org.openvpms.component.business.domain.archetype.ArchetypeId;
+import org.openvpms.component.business.domain.im.common.IMObject;
+import org.openvpms.component.business.domain.im.common.IMObjectReference;
+import org.openvpms.component.system.common.util.StringUtilities;
+
+
+/**
+ * Archetype helper methods.
+ *
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ */
+public class TypeHelper {
+
+    /**
+     * Determines if an object is an instance of a particular archetype.
+     *
+     * @param object    the object. May be <code>null</code>
+     * @param shortName the archetype short name. May contain wildcards
+     * @return <code>true</code> if object is an instance of
+     *         <code>shortName</code>
+     */
+    public static boolean isA(IMObject object, String shortName) {
+        if (object != null) {
+            return matches(object.getArchetypeId(), shortName);
+        }
+        return false;
+    }
+
+    /**
+     * Determines if an object is one of a set of archetypes.
+     *
+     * @param object     the object. May be <code>null</code>
+     * @param shortNames the archetype short names. May contain wildcards
+     * @return <code>true</code> if object is one of <code>shortNames</code>
+     */
+    public static boolean isA(IMObject object, String ... shortNames) {
+        if (object != null) {
+            for (String shortName : shortNames) {
+                if (isA(object, shortName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determines if an object reference refers to an instance of a particular
+     * archetype.
+     *
+     * @param reference the object. May be <code>null</code>
+     * @param shortName the archetype short name. May contain wildcards
+     * @return <code>true</code> if the reference refers to an instance of
+     *         <code>shortName</code>
+     */
+    public static boolean isA(IMObjectReference reference, String shortName) {
+        if (reference != null) {
+            return matches(reference.getArchetypeId(), shortName);
+        }
+        return false;
+    }
+
+    /**
+     * Determines if an archetype id matches a short name. The short name
+     * may contain wildcards.
+     *
+     * @param id        the archetype identifier
+     * @param shortName the short name
+     */
+    public static boolean matches(ArchetypeId id, String shortName) {
+        String regexp = StringUtilities.toRegEx(shortName);
+        return id.getShortName().matches(regexp);
+    }
+}
---
+++ src/test/java/org/openvpms/archetype/rules/patient/MedicalRecordRulesTestCase.java
@@ -0,0 +1,273 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
+ */
+
+package org.openvpms.archetype.rules.patient;
+
+import org.openvpms.archetype.test.ArchetypeServiceTest;
+import org.openvpms.component.business.domain.im.common.Act;
+import org.openvpms.component.business.domain.im.common.ActRelationship;
+import org.openvpms.component.business.domain.im.common.Entity;
+import org.openvpms.component.business.domain.im.common.Participation;
+import org.openvpms.component.business.domain.im.party.Party;
+import org.openvpms.component.business.domain.im.security.User;
+
+
+/**
+ * Tests the {@link MedicalRecordRules} class when invoked by the
+ * <em>archetypeService.remove.act.patientClinicalEpisode.after.drl</em>
+ * and <em>archetypeService.remove.act.patientClinicalEvent.after.drl</em> '
+ * rules. In order for these tests to be successful, the archetype service
+ * must be configured to trigger the above rules.
+ *
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ */
+public class MedicalRecordRulesTestCase extends ArchetypeServiceTest {
+
+    /**
+     * The patient.
+     */
+    private Party _patient;
+
+    /**
+     * The clinician.
+     */
+    private User _clinician;
+
+
+    /**
+     * Verifies that deletion of an <em>act.patientClinicalEpisode</em>
+     * deletes all of the children.
+     */
+    public void testDeleteClinicalEpisode() {
+        Act episode = createEpisode();
+        Act event = createEvent();
+        Act problem = createProblem();
+        addActRelationship(episode, event,
+                           "actRelationship.patientClinicalEpisodeEvent");
+        addActRelationship(event, problem,
+                           "actRelationship.patientClinicalEventItem");
+        save(episode);
+        save(event);
+        save(problem);
+
+        // make sure each of the objects can be retrieved
+        assertNotNull(get(episode.getObjectReference()));
+        assertNotNull(get(event.getObjectReference()));
+        assertNotNull(get(problem.getObjectReference()));
+
+        remove(episode);  // remove should cascade
+
+        // make sure none of the objects can be retrieved
+        assertNull(get(episode.getObjectReference()));
+        assertNull(get(event.getObjectReference()));
+        assertNull(get(problem.getObjectReference()));
+    }
+
+    /**
+     * Verifies that deletion of an <em>act.patientClinicalEvent</em>
+     * deletes all of the children.
+     */
+    public void testDeleteClinicalEvent() {
+        Act episode = createEpisode();
+        Act event = createEvent();
+        Act problem = createProblem();
+        addActRelationship(episode, event,
+                           "actRelationship.patientClinicalEpisodeEvent");
+        addActRelationship(event, problem,
+                           "actRelationship.patientClinicalEventItem");
+        save(episode);
+        save(event);
+        save(problem);
+
+        // make sure each of the objects can be retrieved
+        assertNotNull(get(episode.getObjectReference()));
+        assertNotNull(get(event.getObjectReference()));
+        assertNotNull(get(problem.getObjectReference()));
+
+        remove(event);  // remove should cascade to delete problem
+
+        // make sure the episode, but none of the child acts can be retrieved
+        assertNotNull(get(episode.getObjectReference()));
+        assertNull(get(event.getObjectReference()));
+        assertNull(get(problem.getObjectReference()));
+    }
+
+    /**
+     * Verifies that deletion of an <em>act.patientClinicalProblem</em>
+     * doesn't affect its children.
+     */
+    public void testDeleteClinicalProblem() {
+        Act episode = createEpisode();
+        Act event = createEvent();
+        Act problem = createProblem();
+        Act note = createNote();
+        addActRelationship(episode, event,
+                           "actRelationship.patientClinicalEpisodeEvent");
+        addActRelationship(event, problem,
+                           "actRelationship.patientClinicalEventItem");
+        addActRelationship(event, note,
+                           "actRelationship.patientClinicalEventItem");
+        addActRelationship(problem, note,
+                           "actRelationship.patientClinicalProblemItem");
+        save(episode);
+        save(event);
+        save(problem);
+        save(note);
+
+        // make sure each of the objects can be retrieved
+        assertNotNull(get(episode.getObjectReference()));
+        assertNotNull(get(event.getObjectReference()));
+        assertNotNull(get(problem.getObjectReference()));
+
+        remove(problem);  // remove shouldn't cascade to delete note
+
+        // make sure the all but the problem can be retrieved
+        assertNotNull(get(episode.getObjectReference()));
+        assertNotNull(get(event.getObjectReference()));
+        assertNull(get(problem.getObjectReference()));
+        assertNotNull(get(note.getObjectReference()));
+    }
+
+    /**
+     * Sets up the test case.
+     *
+     * @throws Exception for any error
+     */
+    @Override
+    protected void onSetUp() throws Exception {
+        super.onSetUp();
+        _clinician = createClinician();
+        _patient = createPatient();
+    }
+
+    /**
+     * Helper to create an <em>act.patientClinicalEpisode</em>.
+     *
+     * @return a new act
+     */
+    protected Act createEpisode() {
+        Act act = createAct("act.patientClinicalEpisode");
+        addParticipation(act, _patient, "participation.patient");
+        addParticipation(act, _clinician, "participation.clinician");
+        return act;
+    }
+
+    /**
+     * Helper to create an <em>act.patientClinicalEvent</em>.
+     *
+     * @return a new act
+     */
+    protected Act createEvent() {
+        Act act = createAct("act.patientClinicalEvent");
+        addParticipation(act, _patient, "participation.patient");
+        addParticipation(act, _clinician, "participation.clinician");
+        return act;
+    }
+
+    /**
+     * Helper to create an <em>act.patientClinicalProblem</em>.
+     *
+     * @return a new act
+     */
+    protected Act createProblem() {
+        Act act = createAct("act.patientClinicalProblem");
+        addParticipation(act, _patient, "participation.patient");
+        return act;
+    }
+
+    /**
+     * Helper to create an <em>act.patientClinicalNote</em>.
+     *
+     * @return a new act
+     */
+    protected Act createNote() {
+        Act act = createAct("act.patientClinicalNote");
+        addParticipation(act, _patient, "participation.patient");
+        return act;
+    }
+
+    /**
+     * Helper to create a new act.
+     *
+     * @param shortName the act short name
+     * @return a new act
+     */
+    protected Act createAct(String shortName) {
+        Act act = (Act) create(shortName);
+        assertNotNull(act);
+        return act;
+    }
+
+    /**
+     * Helper to add a relationship between two acts.
+     *
+     * @param source    the source act
+     * @param target    the target act
+     * @param shortName the act relationship short name
+     */
+    protected void addActRelationship(Act source, Act target,
+                                      String shortName) {
+        ActRelationship relationship = (ActRelationship) create(shortName);
+        assertNotNull(relationship);
+        relationship.setSource(source.getObjectReference());
+        relationship.setTarget(target.getObjectReference());
+        source.addActRelationship(relationship);
+        target.addActRelationship(relationship);
+    }
+
+    /**
+     * Adds a participation.
+     *
+     * @param act           the act to add to
+     * @param entity        the participation entity
+     * @param participation the participation short name
+     */
+    protected void addParticipation(Act act, Entity entity,
+                                    String participation) {
+        Participation p = (Participation) create(participation);
+        assertNotNull(p);
+        p.setAct(act.getObjectReference());
+        p.setEntity(entity.getObjectReference());
+        act.addParticipation(p);
+    }
+
+    /**
+     * Creates a new patient.
+     *
+     * @return a new patient
+     */
+    protected Party createPatient() {
+        Party patient = (Party) create("party.patientpet");
+        assertNotNull(patient);
+        return patient;
+    }
+
+    /**
+     * Creates a new user.
+     *
+     * @return a new user
+     */
+    protected User createClinician() {
+        User user = (User) create("security.user");
+        assertNotNull(user);
+        user.setName("vet");
+        return user;
+    }
+
+}
