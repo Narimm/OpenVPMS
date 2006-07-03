feb995adf2ef823bc18b4d6ba32195182da4e678
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-07-03 07:20:30 +0000

    update to latest version of frameworks, archetypes
    git-svn-id: svn://localhost/openvpms/reports/trunk@1061 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/report/jasper/ReportHelper.java
+++ src/java/org/openvpms/report/jasper/ReportHelper.java
@@ -21,19 +21,10 @@ package org.openvpms.report.jasper;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.design.JasperDesign;
 import net.sf.jasperreports.engine.xml.JRXmlLoader;
-import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
-import org.openvpms.component.business.service.archetype.IArchetypeService;
-import org.openvpms.component.system.common.exception.OpenVPMSException;
-import org.openvpms.component.system.common.util.StringUtilities;
 
 import java.io.InputStream;
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 
 /**
@@ -43,12 +34,6 @@ import java.util.Set;
  * @version $LastChangedDate: 2006-05-02 05:16:31Z $
  */
 public class ReportHelper {
-
-    /**
-     * The logger.
-     */
-    private static final Log _log = LogFactory.getLog(ReportHelper.class);
-
 
     /**
      * Returns the value class of a node.
@@ -63,68 +48,6 @@ public class ReportHelper {
             return String.class;
         }
         return descriptor.getClazz();
-    }
-
-    /**
-     * Returns archetype short names from a descriptor.
-     * This expands any wildcards. If the {@link NodeDescriptor#getFilter}
-     * is non-null, matching shortnames are returned, otherwise matching short
-     * names from {@link NodeDescriptor#getArchetypeRange()} are returned.
-     *
-     * @param service    the archetype service
-     * @param descriptor the node descriptor
-     * @return a list of short names
-     */
-    public static String[] getShortNames(IArchetypeService service,
-                                         NodeDescriptor descriptor) {
-        String filter = descriptor.getFilter();
-        String[] names;
-        if (!StringUtils.isEmpty(filter)) {
-            names = getShortNames(service, new String[]{filter}, false);
-        } else {
-            names = getShortNames(service, descriptor.getArchetypeRange(),
-                                  false);
-        }
-        return names;
-    }
-
-    /**
-     * Returns archetype short names matching the specified criteria.
-     *
-     * @param shortNames  the shortNames. May contain wildcards
-     * @param primaryOnly if <code>true</code> only include primary archetypes
-     * @return a list of short names matching the criteria
-     */
-    public static String[] getShortNames(IArchetypeService service,
-                                         String[] shortNames,
-                                         boolean primaryOnly) {
-        Set<String> result = new HashSet<String>();
-        try {
-            for (String shortName : shortNames) {
-                List<String> matches = service.getArchetypeShortNames(
-                        shortName, primaryOnly);
-                result.addAll(matches);
-            }
-        } catch (OpenVPMSException exception) {
-            _log.error(exception, exception);
-        }
-        return result.toArray(new String[0]);
-    }
-
-    /**
-     * Determines if a set of short names match a particular archetype.
-     *
-     * @param shortNames the short names
-     * @param type       the archetype wildcard
-     */
-    public static boolean matches(String[] shortNames, String type) {
-        String regexp = StringUtilities.toRegEx(type);
-        for (String shortName : shortNames) {
-            if (!shortName.matches(regexp)) {
-                return false;
-            }
-        }
-        return true;
     }
 
     /**
