e8899013e1ea8c2f2ae3b44229ace5ac8f2058a6
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2006-07-31 00:10:05 +0000

    change to oexport reports from openoffice templates as PDF
    git-svn-id: svn://localhost/openvpms/reports/trunk@1200 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/report/openoffice/OpenOfficeIMObjectReport.java
+++ src/java/org/openvpms/report/openoffice/OpenOfficeIMObjectReport.java
@@ -23,21 +23,21 @@ import com.sun.star.beans.XPropertySet;
 import com.sun.star.container.XEnumerationAccess;
 import com.sun.star.container.XNameAccess;
 import com.sun.star.frame.XComponentLoader;
-import com.sun.star.frame.XStorable;
 import com.sun.star.frame.XModel;
-import com.sun.star.io.XInputStream;
+import com.sun.star.frame.XStorable;
 import com.sun.star.io.IOException;
+import com.sun.star.io.XInputStream;
 import com.sun.star.lang.XComponent;
 import com.sun.star.lang.XMultiComponentFactory;
 import com.sun.star.lib.uno.adapter.ByteArrayToXInputStreamAdapter;
 import com.sun.star.lib.uno.adapter.XOutputStreamToByteArrayAdapter;
-import com.sun.star.text.XTextFieldsSupplier;
 import com.sun.star.text.XTextDocument;
+import com.sun.star.text.XTextFieldsSupplier;
 import com.sun.star.uno.UnoRuntime;
 import com.sun.star.uno.XComponentContext;
-import com.sun.star.util.XRefreshable;
-import com.sun.star.util.XCloseable;
 import com.sun.star.util.CloseVetoException;
+import com.sun.star.util.XCloseable;
+import com.sun.star.util.XRefreshable;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.document.Document;
 import org.openvpms.component.business.service.archetype.ArchetypeServiceHelper;
@@ -46,6 +46,7 @@ import org.openvpms.report.IMObjectReport;
 import org.openvpms.report.IMObjectReportException;
 import org.openvpms.report.NodeResolver;
 import org.openvpms.report.jasper.ReportHelper;
+import org.apache.commons.io.FilenameUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -53,8 +54,8 @@ import java.util.List;
 
 
 /**
- * Service to bootsrap a <code>XComponentContext</code> from an UNO
- * installation. The OpenOffice binaries must be in the path.
+ * Generates a report for an <code>IMObject</code>, using an OpenOffice document
+ * as the template.
  *
  * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
  * @version $LastChangedDate: 2006-05-02 05:16:31Z $
@@ -75,7 +76,13 @@ public class OpenOfficeIMObjectReport implements IMObjectReport {
      * The document template.
      */
     private final Document _template;
-    private static final String USER_FIELD_PREFIX = "com.sun.star.text.FieldMaster.User.";
+
+    /**
+     * Prefix for user fields. The value of these fields are populated with
+     * data from the IMObject.
+     */
+    private static final String USER_FIELD_PREFIX
+            = "com.sun.star.text.FieldMaster.User.";
 
 
     /**
@@ -106,8 +113,10 @@ public class OpenOfficeIMObjectReport implements IMObjectReport {
                                               template);
 
             // access the TextFields and the TextFieldMasters collections
-            XNameAccess namedFieldMasters = textFieldsSupplier.getTextFieldMasters();
-            XEnumerationAccess enumeratedFields = textFieldsSupplier.getTextFields();
+            XNameAccess namedFieldMasters
+                    = textFieldsSupplier.getTextFieldMasters();
+            XEnumerationAccess enumeratedFields
+                    = textFieldsSupplier.getTextFields();
             List<String> fieldNames = getUserFieldNames(namedFieldMasters);
 
             for (String name : fieldNames) {
@@ -116,23 +125,49 @@ public class OpenOfficeIMObjectReport implements IMObjectReport {
                 // access corresponding field master
                 Object fieldMaster = namedFieldMasters.getByName(
                         USER_FIELD_PREFIX + name);
-                // query the XPropertySet interface, we need to set the Content property
-                XPropertySet xPropertySet = (XPropertySet) UnoRuntime.queryInterface(
+
+                // query the XPropertySet interface, in order to set the
+                // Content property
+                XPropertySet propertySet
+                        = (XPropertySet) UnoRuntime.queryInterface(
                         XPropertySet.class, fieldMaster);
 
-                // insert the column value into field master
-                xPropertySet.setPropertyValue("Content", value);
+                // insert the value into field master
+                propertySet.setPropertyValue("Content", value);
             }
-            // afterwards we must refresh the textfields collection
-            XRefreshable xRefreshable = (XRefreshable) UnoRuntime.queryInterface(
+            // refresh the textfields collection
+            XRefreshable refreshable = (XRefreshable) UnoRuntime.queryInterface(
                     XRefreshable.class, enumeratedFields);
-            xRefreshable.refresh();
+            refreshable.refresh();
             return export(template);
-        } catch (com.sun.star.uno.Exception e) {
-            throw new IMObjectReportException(e);
+        } catch (com.sun.star.uno.Exception exception) {
+            throw new IMObjectReportException(exception);
         }
     }
 
+    /**
+     * Returns a list of user field names.
+     *
+     * @param namedFieldMasters the a list of all field masters
+     * @return a list of user field names
+     */
+    private List<String> getUserFieldNames(XNameAccess namedFieldMasters) {
+        List<String> result = new ArrayList<String>();
+        for (String name : namedFieldMasters.getElementNames()) {
+            if (name.startsWith(USER_FIELD_PREFIX)) {
+                result.add(name.substring(USER_FIELD_PREFIX.length()));
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns the value of a field.
+     *
+     * @param name     the field name
+     * @param resolver the node resolver
+     * @return the value of the field
+     */
     private String getValue(String name, NodeResolver resolver) {
         NodeResolver.State state = resolver.resolve(name);
         Object value = state.getValue();
@@ -161,48 +196,19 @@ public class OpenOfficeIMObjectReport implements IMObjectReport {
     }
 
     /**
-     * Returns a list of user field names.
-     *
-     * @param namedFieldMasters the a list of all field masters
-     * @return a list of user field names
-     */
-    private List<String> getUserFieldNames(XNameAccess namedFieldMasters) {
-        List<String> result = new ArrayList<String>();
-        for (String name : namedFieldMasters.getElementNames()) {
-            if (name.startsWith(USER_FIELD_PREFIX)) {
-                result.add(name.substring(USER_FIELD_PREFIX.length()));
-            }
-        }
-        return result;
-    }
-
-    protected XComponent newDocComponent(String docType)
-            throws java.lang.Exception {
-        String loadUrl = "private:factory/" + docType;
-        _serviceManager = this.getServiceManager();
-        Object desktop = _serviceManager.createInstanceWithContext(
-                "com.sun.star.frame.Desktop", _context);
-        XComponentLoader xComponentLoader = (XComponentLoader)
-                UnoRuntime.queryInterface(XComponentLoader.class, desktop);
-        PropertyValue[] loadProps = new PropertyValue[0];
-        return xComponentLoader.loadComponentFromURL(loadUrl, "_blank",
-                                                     0, loadProps);
-    }
-
-    /**
-     * Load a document as template.
+     * Loads a document as template.
      *
      * @return the new document component
      * @throws com.sun.star.uno.Exception if the document can't be created
      */
-    protected XComponent newDocComponentFromTemplate()
+    private XComponent newDocComponentFromTemplate()
             throws com.sun.star.uno.Exception {
         // get the remote service manager
-        _serviceManager = getServiceManager();
+        XMultiComponentFactory serviceManager = getServiceManager();
         // retrieve the Desktop object, we need its XComponentLoader
-        Object desktop = _serviceManager.createInstanceWithContext(
+        Object desktop = serviceManager.createInstanceWithContext(
                 "com.sun.star.frame.Desktop", _context);
-        XComponentLoader xComponentLoader = (XComponentLoader)
+        XComponentLoader componentLoader = (XComponentLoader)
                 UnoRuntime.queryInterface(XComponentLoader.class, desktop);
 
         byte[] content = _template.getContents();
@@ -216,31 +222,47 @@ public class OpenOfficeIMObjectReport implements IMObjectReport {
         // AsTemplate tells office to create a new document from the given
         // stream
 
-        return xComponentLoader.loadComponentFromURL("private:stream", "_blank",
-                                                     0, properties);
+        return componentLoader.loadComponentFromURL("private:stream", "_blank",
+                                                    0, properties);
     }
 
+    /**
+     * Exports the template as a PDF, serializing to a {@link Document}.
+     *
+     * @param template the template
+     * @return a new document, containing the serialized template
+     * @throws CloseVetoException
+     * @throws IOException
+     */
     private Document export(XComponent template) throws CloseVetoException,
                                                         IOException {
-                XTextDocument aTextDocument = (XTextDocument) UnoRuntime.queryInterface(com.sun.star.text.XTextDocument.class, template);
-        XOutputStreamToByteArrayAdapter stream = new XOutputStreamToByteArrayAdapter();
+        XTextDocument aTextDocument = (XTextDocument) UnoRuntime.queryInterface(
+                XTextDocument.class, template);
+        XOutputStreamToByteArrayAdapter stream
+                = new XOutputStreamToByteArrayAdapter();
 
-        XStorable xStorable = (XStorable) UnoRuntime.queryInterface(XStorable.class, aTextDocument);
+        XStorable xStorable = (XStorable) UnoRuntime.queryInterface(
+                XStorable.class, aTextDocument);
         PropertyValue[] properties = new PropertyValue[]{
                 property("OutputStream", stream),
                 property("Overwrite", true),
+                property("FilterName", "writer_pdf_Export")
         };
 
         xStorable.storeToURL("private:stream", properties);
         stream.closeOutput();
 
-        XModel xModel = (XModel) UnoRuntime.queryInterface(XModel.class, template);
-        XCloseable xCloseable = (XCloseable) UnoRuntime.queryInterface(XCloseable.class, xModel);
+        XModel xModel = (XModel) UnoRuntime.queryInterface(XModel.class,
+                                                           template);
+        XCloseable xCloseable = (XCloseable) UnoRuntime.queryInterface(
+                XCloseable.class, xModel);
         xCloseable.close(false);
 
         IArchetypeService service = ArchetypeServiceHelper.getArchetypeService();
         Document doc = (Document) service.create("document.other");
-        doc.setName(_template.getName());
+        String name = FilenameUtils.removeExtension(_template.getName()) 
+                + ".pdf";
+        doc.setName(name);
         byte[] content = stream.getBuffer();
         doc.setContents(content);
         doc.setDocSize(content.length);
