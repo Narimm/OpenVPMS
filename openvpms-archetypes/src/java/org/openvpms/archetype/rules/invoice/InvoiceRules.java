23ba915f931826be67d8933ca1d504a6c98d0441
Tim Anderson <tma@netspace.net.au>
2006-09-06 04:26:46 +0000

    Add support for OVPMS-342 Charging - Product Documents
    svn path=/archetypes/trunk/; revision=1365

--- openvpms-archetypes/src/java/org/openvpms/archetype/rules/invoice/InvoiceRules.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/rules/invoice/InvoiceRules.java
@@ -18,6 +18,7 @@
 
 package org.openvpms.archetype.rules.invoice;
 
+import org.apache.commons.lang.StringUtils;
 import org.openvpms.archetype.rules.patient.ReminderRules;
 import org.openvpms.component.business.domain.im.act.Act;
 import org.openvpms.component.business.domain.im.act.ActRelationship;
@@ -30,7 +31,8 @@ import org.openvpms.component.business.service.archetype.ArchetypeServiceExcepti
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.archetype.helper.ActBean;
 import org.openvpms.component.business.service.archetype.helper.ArchetypeQueryHelper;
-import org.openvpms.component.business.service.archetype.helper.IMObjectBean;
+import org.openvpms.component.business.service.archetype.helper.EntityBean;
+import org.openvpms.component.business.service.archetype.helper.IMObjectBeanException;
 import org.openvpms.component.business.service.archetype.helper.TypeHelper;
 
 import java.util.Date;
@@ -61,88 +63,138 @@ public class InvoiceRules {
     }
 
     /**
-     * Add reminders to an <em>act.customerAccountInvoiceItem</em>.
+     * Invoked after an invoice item has been saved. Updates any reminders
+     * and documents associated with the product.
      * <p/>
      * TODO - all modifications should be done within a transaction
      *
-     * @param item the item
+     * @param act the act
+     */
+    public void saveInvoiceItem(FinancialAct act) {
+        if (!TypeHelper.isA(act, "act.customerAccountInvoiceItem")) {
+            throw new IllegalArgumentException("Invalid argument 'act'");
+        }
+        ActBean actBean = new ActBean(act, _service);
+        Entity product = actBean.getParticipant("participation.product");
+        EntityBean productBean = null;
+        if (product != null) {
+            productBean = new EntityBean(product, _service);
+        }
+        boolean save = addReminders(actBean, productBean);
+        save |= addDocuments(actBean, productBean);
+        if (save) {
+            actBean.save();
+        }
+    }
+
+    /**
+     * Invoked <em>prior</em> to an invoice being removed. Removes any reminders
+     * or documents that don't have status 'Completed.
+     * <p/>
+     * TODO - all modifications should be done within a transaction
+     *
+     * @param invoice the invoice
+     */
+    public void removeInvoice(FinancialAct invoice) {
+        if (!TypeHelper.isA(invoice, "act.customerAccountChargesInvoice")) {
+            throw new IllegalArgumentException("Invalid argument 'invoice'");
+        }
+        ActBean bean = new ActBean(invoice);
+        List<Act> acts = bean.getActs("act.customerAccountInvoiceItem");
+        for (Act act : acts) {
+            removeInvoiceItem((FinancialAct) act);
+        }
+    }
+
+    /**
+     * Invoked when an invoice item has been removed. Removes any reminders
+     * or documents that don't have status 'Completed.
+     * <p/>
+     * NOTE: when invoked on deletion of an invoice, this must occur prior
+     * to the invoice being deleted, in order for the invoice items to be
+     * resolved. When invoked on deletion of an invoice item, it can occur
+     * after the invoice item has been removed.
+     *
+     * @param act the act
+     */
+    public void removeInvoiceItem(FinancialAct act) {
+        if (!TypeHelper.isA(act, "act.customerAccountInvoiceItem")) {
+            throw new IllegalArgumentException("Invalid argument 'act'");
+        }
+        removeInvoiceItemReminders(act);
+        removeInvoiceItemDocuments(act);
+    }
+
+    /**
+     * Add reminders to an <em>act.customerAccountInvoiceItem</em>.
+     *
+     * @param item    the invoice item
+     * @param product the product. May be <code>null</code>
+     * @return <code>true</code> if the item needs to be saved
      * @throws ArchetypeServiceException for any archetype service error
      */
-    public void addReminders(FinancialAct item) {
+    private boolean addReminders(ActBean item, EntityBean product) {
         boolean save = false;
-        ActBean bean = new ActBean(item, _service);
-        List<Act> acts = bean.getActs("act.patientReminder");
-        IMObject product = bean.getParticipant("participation.product");
+        List<Act> acts = item.getActsForNode("reminders");
 
         // remove any existing reminders not referenced by the current product
         for (Act act : acts) {
             if (product == null || !hasProduct(act, product)) {
-                ActRelationship r = bean.getRelationship(act);
-                bean.removeRelationship(r);
+                ActRelationship r = item.getRelationship(act);
+                item.removeRelationship(r);
                 save = true;
             }
         }
 
         // add any reminders associated with the current product
-        if (product != null) {
-            IMObjectBean productBean = new IMObjectBean(product);
-            if (productBean.hasNode("reminders")) {
-                List<IMObject> reminders = productBean.getValues("reminders");
-                if (!reminders.isEmpty()) {
-                    for (IMObject object : reminders) {
-                        EntityRelationship relationship
-                                = (EntityRelationship) object;
-                        Entity reminderType = (Entity) getObject(
-                                relationship.getTarget());
-                        if (reminderType != null &&
-                                !hasReminder(acts, reminderType)) {
-                            addReminder(bean, reminderType);
-                            save = true;
-                        }
-                    }
+        if (product != null && product.hasNode("reminders")) {
+            List<IMObject> reminders = product.getValues("reminders");
+            for (IMObject object : reminders) {
+                EntityRelationship rel = (EntityRelationship) object;
+                Entity reminderType = (Entity) getObject(rel.getTarget());
+                if (reminderType != null && !hasReminder(acts, reminderType)) {
+                    addReminder(item, reminderType);
+                    save = true;
                 }
             }
         }
-        if (save) {
-            bean.save();
-        }
+        return save;
     }
 
     /**
-     * Removes any reminders associated with an
-     * <em>act.customerAccountChargesInvoice</em>
-     * <em>act.customerAccountInvoiceItem</em>.
-     * For <em>act.customerAccountChargesInvoice</em>, the reminders are
-     * associated via the child <em>act.customerAccountInvoiceItem</em>s.
-     * <p/>
-     * TODO - all modifications should be done within a transaction
+     * Add documents to an <em>act.customerAccountInvoiceItem</em>.
      *
-     * @param act the act
+     * @param item    the invoice item
+     * @param product the product. May be <code>null</code>
+     * @return <code>true</code> if the item needs to be saved
      * @throws ArchetypeServiceException for any archetype service error
      */
-    public void removeReminders(FinancialAct act) {
-        if (TypeHelper.isA(act, "act.customerAccountChargesInvoice")) {
-            removeInvoiceReminders(act);
-        } else if (TypeHelper.isA(act, "act.customerAccountInvoiceItem")) {
-            removeInvoiceItemReminders(act);
-        }
-    }
+    private boolean addDocuments(ActBean item, EntityBean product) {
+        boolean save = false;
+        List<Act> acts = item.getActsForNode("documents");
 
-    /**
-     * Removes reminders associated with an invoice through its child
-     * <em>act.customerAccountInvoiceItem</em>.
-     *
-     * @param invoice the invoice
-     * @throws ArchetypeServiceException for any archetype service error
-     */
-    protected void removeInvoiceReminders(FinancialAct invoice) {
-        ActBean bean = new ActBean(invoice);
-        List<Act> acts = bean.getActs();
+        // remove any existing documents not referenced by the current product
         for (Act act : acts) {
-            if (TypeHelper.isA(act, "act.customerAccountInvoiceItem")) {
-                removeInvoiceItemReminders((FinancialAct) act);
+            if (product == null || !hasProduct(act, product)) {
+                ActRelationship r = item.getRelationship(act);
+                item.removeRelationship(r);
+                save = true;
             }
         }
+
+        // add any documents associated with the current product
+        if (product != null && product.hasNode("documents")) {
+            List<IMObject> documents = product.getValues("documents");
+            for (IMObject object : documents) {
+                EntityRelationship rel = (EntityRelationship) object;
+                Entity document = (Entity) getObject(rel.getTarget());
+                if (document != null && !hasDocument(acts, document)) {
+                    addDocument(item, document);
+                    save = true;
+                }
+            }
+        }
+        return save;
     }
 
     /**
@@ -152,14 +204,38 @@ public class InvoiceRules {
      *
      * @param item the invoice item
      * @throws ArchetypeServiceException for any archetype service error
+     * @throws IMObjectBeanException     if the reminders node does't exist
      */
-    protected void removeInvoiceItemReminders(FinancialAct item) {
+    private void removeInvoiceItemReminders(FinancialAct item) {
         ActBean bean = new ActBean(item, _service);
-        List<Act> acts = bean.getActs("act.patientReminder");
+        List<Act> acts = bean.getActsForNode("reminders");
 
         for (Act act : acts) {
             ActRelationship r = bean.getRelationship(act);
             if (!"Completed".equals(act.getStatus())) {
+                _service.remove(act);
+                bean.removeRelationship(r);
+            }
+        }
+    }
+
+    /**
+     * Deletes any documents associated with an
+     * <em>act.customerAccountInvoiceItem</em> that don't have status
+     * 'Completed' or 'Posted'.
+     *
+     * @param item the invoice item
+     * @throws ArchetypeServiceException for any archetype service error
+     * @throws IMObjectBeanException     if the documents node does't exist
+     */
+    private void removeInvoiceItemDocuments(FinancialAct item) {
+        ActBean bean = new ActBean(item, _service);
+        List<Act> acts = bean.getActsForNode("documents");
+
+        for (Act act : acts) {
+            String status = act.getStatus();
+            ActRelationship r = bean.getRelationship(act);
+            if (!"Completed".equals(status) && !"Posted".equals(status)) {
                 _service.remove(act);
                 bean.removeRelationship(r);
             }
@@ -195,6 +271,34 @@ public class InvoiceRules {
     }
 
     /**
+     * Adds an <em>act.patientDocument*</em> to an
+     * <em>act.customerAccountInvoiceItem</em>.
+     *
+     * @param item     the invoice item
+     * @param document the document template
+     */
+    private void addDocument(ActBean item, Entity document) {
+        EntityBean bean = new EntityBean(document, _service);
+        String shortName = bean.getString("archetype");
+        if (StringUtils.isEmpty(shortName)) {
+            shortName = "act.patientDocumentForm";
+        }
+        IMObject object = _service.create(shortName);
+        if (TypeHelper.isA(object, "act.patientDocument*")) {
+            Act act = (Act) object;
+            act.setActivityStartTime(item.getAct().getActivityStartTime());
+            ActBean documentAct = new ActBean(act);
+            IMObjectReference patient = item.getParticipantRef(
+                    "participation.patient");
+            documentAct.addParticipation("participation.patient", patient);
+            documentAct.addParticipation("participation.document", document);
+            documentAct.save();
+            item.addRelationship("actRelationship.invoiceItemDocument",
+                                 documentAct.getAct());
+        }
+    }
+
+    /**
      * Determines if an act references a product.
      *
      * @param act     the act
@@ -202,14 +306,37 @@ public class InvoiceRules {
      * @return <code>true</code> if the act references <code>product</code>;
      *         otherwise <code>false</code>
      */
-    private boolean hasProduct(Act act, IMObject product) {
+    private boolean hasProduct(Act act, EntityBean product) {
         ActBean bean = new ActBean(act, _service);
+        IMObjectReference productRef = product.getObject().getObjectReference();
         IMObjectReference ref = bean.getParticipantRef("participation.product");
-        return ref != null && product.getObjectReference().equals(ref);
+        return ref != null && productRef.equals(ref);
     }
 
     /**
-     * Determines if a reminder type is referenced by a set of
+     * Determines if a document template is referenced by a set of
+     * <em>act.patientReminder</em>s.
+     *
+     * @param documents        the <em>act.patientDocument*</em>s
+     * @param documentTemplate the document template
+     * @return <code>true</code> if at least one act references the reminder
+     *         type; otherwise <code>false</code>
+     */
+    private boolean hasDocument(List<Act> documents, Entity documentTemplate) {
+        IMObjectReference reminderRef = documentTemplate.getObjectReference();
+        for (Act act : documents) {
+            ActBean bean = new ActBean(act, _service);
+            IMObjectReference ref
+                    = bean.getParticipantRef("participation.documentTemplate");
+            if (ref != null && reminderRef.equals(ref)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Determines if a document act is referenced by a set of
      * <em>act.patientReminder</em>s.
      *
      * @param reminders    the <em>act.patientReminder</em>s
