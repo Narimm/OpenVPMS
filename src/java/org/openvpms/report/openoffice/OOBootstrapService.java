b3f011464982d27b1b86eea9cccfc3b60d621d8c
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2007-01-18 01:13:41 +0000

    Improve diagnostic support
    git-svn-id: svn://localhost/openvpms/reports/trunk@1750 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/report/openoffice/OOBootstrapService.java
+++ src/java/org/openvpms/report/openoffice/OOBootstrapService.java
@@ -32,6 +32,7 @@ import com.sun.star.uno.XComponentContext;
 import com.sun.star.util.XCloseable;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.commons.lang.StringUtils;
 import static org.openvpms.report.openoffice.OpenOfficeException.ErrorCode.FailedToStartService;
 
 import java.io.BufferedReader;
@@ -50,6 +51,11 @@ import java.io.PrintStream;
 public abstract class OOBootstrapService {
 
     /**
+     * The command path.
+     */
+    private String path;
+
+    /**
      * The connection parameters.
      */
     private final String parameters;
@@ -62,6 +68,12 @@ public abstract class OOBootstrapService {
     private boolean headless = true;
 
     /**
+     * The no. of times to attempt connection to the service, before giving
+     * up.
+     */
+    private int retries = DEFAULT_RETRIES;
+
+    /**
      * The time to wait after terminating the desktop, in milliseconds. Used to
      * ensure that subsequent startups don't fail due to socket errors.
      */
@@ -71,6 +83,11 @@ public abstract class OOBootstrapService {
      * The current process.
      */
     private Process process;
+
+    /**
+     * Default no. of retries.
+     */
+    private static final int DEFAULT_RETRIES = 10;
 
     /**
      * The logger.
@@ -98,10 +115,6 @@ public abstract class OOBootstrapService {
                                           "service already running");
         }
         log.info("Starting OpenOffice");
-        if (log.isDebugEnabled()) {
-            log.debug("Attempting to start soffice using parameters="
-                    + parameters);
-        }
         try {
             // create default local component context
             XComponentContext localContext =
@@ -111,9 +124,13 @@ public abstract class OOBootstrapService {
                                               "no local component context");
             }
 
-            String office =
-                    System.getProperty("os.name").startsWith("Windows") ?
+            String office;
+            if (StringUtils.isEmpty(path)) {
+                office = System.getProperty("os.name").startsWith("Windows") ?
                             "soffice.exe" : "soffice";
+            } else {
+                office = path;
+            }
 
             // command line arguments
             String[] args;
@@ -121,8 +138,16 @@ public abstract class OOBootstrapService {
             if (headless) {
                 args = new String[]{office, "-headless", accept};
             } else {
+                // -nocrashreport not required in 2.1, added for compatibility
+                // with 2.0.4
                 args = new String[]{office, "-nologo", "-nodefault",
-                                    "-norestore", "-nolockcheck", accept};
+                                    "-norestore", "-nocrashreport",
+                                    "-nolockcheck", accept};
+            }
+
+            if (log.isDebugEnabled()) {
+                log.debug("Attempting to start soffice using: "
+                        + StringUtils.join(args, " "));
             }
 
             // start the office process
@@ -146,33 +171,26 @@ public abstract class OOBootstrapService {
                     ";urp;StarOffice.ComponentContext";
 
             // wait until office is started
-            Thread.sleep(500);
-            for (; ;) {
-                try {
-                    // try to connect to office
-                    Object context = urlResolver.resolve(connect);
-                    context = UnoRuntime.queryInterface(
-                            XComponentContext.class, context);
-                    if (context == null) {
-                        throw new OpenOfficeException(FailedToStartService,
-                                                      "no component context");
-                    }
-                    break;
-                } catch (NoConnectException exception) {
-                    // ensure the process is still running
-
-                	try {
-                        int exit = process.exitValue();
-//                            throw new OpenOfficeException(
-//                                    FailedToStartService,
-//                                    "terminated with exit code=" + exit);                        	
-                        log.error("OpennOffice process failed"+exit);
-                        break;
-                    } catch (IllegalThreadStateException ignore) {
-                        // process is running
-                    	break;
+            boolean connected = false;
+            for (int i = 0; i < retries && !connected; ++i) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Attempting to connect, attempt "
+                            + (i + 1) + " of " + retries);
+                }
+                connected = connect(urlResolver, connect);
+                if (!connected) {
+                    try {
+                        // wait 500 ms, then try to connect again
+                        Thread.sleep(500);
+                    } catch (InterruptedException ignore) {
+                        // no-op
                     }
                 }
+            }
+            if (!connected) {
+                throw new OpenOfficeException(
+                        FailedToStartService, "failed to connect after "
+                        + retries + " attempts");
             }
         } catch (OpenOfficeException exception) {
             if (process != null) {
@@ -180,6 +198,7 @@ public abstract class OOBootstrapService {
             }
             throw exception;
         } catch (Throwable exception) {
+            log.debug(exception, exception);
             if (process != null) {
                 process.destroy();
             }
@@ -239,6 +258,96 @@ public abstract class OOBootstrapService {
     }
 
     /**
+     * Returns a new {@link OOConnection} to the service.
+     *
+     * @return a new connection to the service
+     * @throws OpenOfficeException if a connection cannot be established
+     */
+    public abstract OOConnection getConnection();
+
+    /**
+     * Sets the path of the command to execute.
+     * If not specified defaults to <em>soffice.exe</em> on Windows or
+     * <em>soffice</em> on other OSes.
+     *
+     * @param path the command path
+     */
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    /**
+     * Determines whether the service should be started headless or not.
+     * In headless mode, the service can be reliably terminated, however
+     * it prevents any viewing of OO documents by the local user.
+     *
+     * @param headless if <code>true</code> starts the server headless.
+     *                 Defaults to <code>true</code>
+     */
+    public void setHeadless(boolean headless) {
+        this.headless = headless;
+    }
+
+    /**
+     * Sets the no. of times to retry connection to the service before giving
+     * up.
+     *
+     * @param retries the no. of times to retry connection
+     */
+    public void setConnectionRetries(int retries) {
+        this.retries = (retries > 0) ? retries : DEFAULT_RETRIES;
+    }
+
+    /**
+     * Sets the amount of time to wait after terminating the desktop, when
+     * stopping an OpenOffice service. This ensures that subsequent restarts
+     * don't fail with socket errors.
+     *
+     * @param wait the time to wait, in milliseconds. Defaults to
+     *             <code>1000</code>
+     */
+    public void setTerminateWait(long wait) {
+        this.terminateWait = wait;
+    }
+
+    /**
+     * Attempts connection to an OpenOffice service.
+     *
+     * @param urlResolver the URL resolver
+     * @param connect     the connection string
+     * @return <code>true</code> if connection is successful, otherwise
+     *         <code>false</code>
+     * @throws OpenOfficeException        if the context cannot be resolved
+     * @throws com.sun.star.uno.Exception for any OpenOffice error
+     */
+    private boolean connect(XUnoUrlResolver urlResolver, String connect)
+            throws com.sun.star.uno.Exception {
+        boolean connected = false;
+        try {
+            // try to connect to office
+            Object context = urlResolver.resolve(connect);
+            context = UnoRuntime.queryInterface(
+                    XComponentContext.class, context);
+            if (context == null) {
+                throw new OpenOfficeException(FailedToStartService,
+                                              "no component context");
+            }
+            connected = true;
+        } catch (NoConnectException exception) {
+            // see if the process is still running. Don't use
+            // process exit to terminate the loop as soffice may
+            // handle connections via soffice.bin?
+            try {
+                int exit = process.exitValue();
+                log.debug("Process terminated with exit code=" + exit);
+            } catch (IllegalThreadStateException ignore) {
+                // process is running
+            }
+        }
+        return connected;
+    }
+
+    /**
      * Cleans up the desktop, before terminating it.
      *
      * @param desktop the desktop
@@ -269,38 +378,6 @@ public abstract class OOBootstrapService {
         } catch (Throwable exception) {
             log.debug(exception, exception);
         }
-    }
-
-    /**
-     * Returns a new {@link OOConnection} to the service.
-     *
-     * @return a new connection to the service
-     * @throws OpenOfficeException if a connection cannot be established
-     */
-    public abstract OOConnection getConnection();
-
-    /**
-     * Determines whether the service should be started headless or not.
-     * In headless mode, the service can be reliably terminated, however
-     * it prevents any viewing of OO documents by the local user.
-     *
-     * @param headless if <code>true</code> starts the server headless.
-     *                 Defaults to <code>true</code>
-     */
-    public void setHeadless(boolean headless) {
-        this.headless = headless;
-    }
-
-    /**
-     * Sets the amount of time to wait after terminating the desktop, when
-     * stopping an OpenOffice service. This ensures that subsequent restarts
-     * don't fail with socket errors.
-     *
-     * @param wait the time to wait, in milliseconds. Defaults to
-     *             <code>1000</code>
-     */
-    public void setTerminateWait(long wait) {
-        this.terminateWait = wait;
     }
 
     /**
