2c2613319eb0ef90d90c8cba11dc22895962ab67
Tony De Keizer <tony@verticalconnect.net>
2007-01-23 05:23:04 +0000

    svn path=/archetypes/trunk/; revision=1795

--- openvpms-archetypes/src/java/org/openvpms/archetype/rules/deposit/DepositQuery.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/rules/deposit/DepositQuery.java
@@ -22,7 +22,6 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.collections.comparators.NullComparator;
