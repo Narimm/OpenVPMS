ffb89b2aaaa0928ff9f24d12a99d6b7a92a485a3
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2007-05-09 07:16:15 +0000

    Performance enhancements for OVPMS-599 Scheduling - option to allow workflow management via scheduler and/or worklists
    git-svn-id: svn://localhost/openvpms/framework/trunk@2055 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/component/business/dao/hibernate/im/entity/AbstractObjectLoader.java
+++ src/java/org/openvpms/component/business/dao/hibernate/im/entity/AbstractObjectLoader.java
@@ -18,8 +18,6 @@
 
 package org.openvpms.component.business.dao.hibernate.im.entity;
 
-import org.hibernate.HibernateException;
-
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,7 +28,7 @@ import java.util.Map;
  * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
  * @version $LastChangedDate: 2006-05-02 05:16:31Z $
  */
-public class AbstractObjectLoader implements ObjectLoader {
+public abstract class AbstractObjectLoader implements ObjectLoader {
 
     /**
      * The default loader, if none is specified.
@@ -48,24 +46,34 @@ public class AbstractObjectLoader implements ObjectLoader {
     /**
      * Sets the loader for a particular class name.
      *
-     * @param className   the class name
-     * @param loader the loader for the class
+     * @param className the class name
+     * @param loader    the loader for the class
      */
     public void setLoader(String className, ObjectLoader loader) {
         loaders.put(className, loader);
     }
 
     /**
-     * Loads an object.
+     * Sets the loader for a particular class.
      *
-     * @param object the object to load
-     * @throws HibernateException for any hibernate error
+     * @param clazz  the class
+     * @param loader the loader for the class
      */
-    public void load(Object object) {
+    public void setLoader(Class clazz, ObjectLoader loader) {
+        setLoader(clazz.getName(), loader);
+    }
+
+    /**
+     * Returns the loader for an object.
+     *
+     * @param object the object
+     * @return the loader for <tt>object</tt>
+     */
+    public ObjectLoader getLoader(Object object) {
         ObjectLoader loader = loaders.get(object.getClass().getName());
         if (loader == null) {
             loader = defaultLoader;
         }
-        loader.load(object);
+        return loader;
     }
 }
