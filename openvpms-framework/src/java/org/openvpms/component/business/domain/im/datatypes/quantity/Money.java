cda5205780d2f6a7eb1cf8b650ba74593b247253
Tim Anderson <tma@netspace.net.au>
2007-09-03 05:56:47 +0000

    Cleaned up Money javadocs, add ZERO, ONE, and TEN constants
    svn path=/framework/trunk/; revision=2337

--- openvpms-framework/src/java/org/openvpms/component/business/domain/im/datatypes/quantity/Money.java
+++ openvpms-framework/src/java/org/openvpms/component/business/domain/im/datatypes/quantity/Money.java
@@ -23,161 +23,291 @@ import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.math.MathContext;
 
+
 /**
- * This is a marker class to denote a Money class. 
- * 
+ * This is a marker class to denote a Money class.
+ * <p/>
  * NOTE: This will evolve once we get requirements
- * 
- * @author   <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version  $LastChangedDate$
+ *
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate$
  */
 public class Money extends BigDecimal {
 
     /**
-     * Default SUID
+     * Serialisation version identifier.
      */
     private static final long serialVersionUID = 1L;
 
     /**
-     * default constructor
+     * The value 0, with a scale of 0.
+     */
+    public static final Money ZERO = new Money(0);
+
+    /**
+     * The value 1, with a scale of 0.
+     */
+    public static final Money ONE = new Money(1);
+
+    /**
+     * The value 10, with a scale of 0.
+     */
+    public static final Money TEN = new Money(10);
+
+
+    /**
+     * Default constructor.
      */
     public Money() {
-        super(0.0);
+        super(0);
     }
-    
+
     /**
-     * Construct from a {@link BigDecimal}
-     * 
-     * @param value
+     * Construct from a {@link BigDecimal}.
+     *
+     * @param value the value
      */
     public Money(BigDecimal value) {
         super(value.toString());
     }
-    
+
     /**
-     * @param arg0
-     * @param arg1
-     * @param arg2
+     * Translates a character array representation of a
+     * <tt>Money</tt> into a <tt>Money</tt>, accepting the
+     * same sequence of characters as the {@link #Money(String)}
+     * constructor, while allowing a sub-array to be specified.
+     *
+     * @param in     <tt>char</tt> array that is the source of characters.
+     * @param offset first character in the array to inspect.
+     * @param len    number of characters to consider.
+     * @throws NumberFormatException if <tt>in</tt> is not a valid
+     *                               representation of a <tt>Money</tt> or
+     *                               the defined subarray is not wholly within
+     *                               <tt>in</tt>.
      */
-    public Money(char[] arg0, int arg1, int arg2) {
-        super(arg0, arg1, arg2);
+    public Money(char[] in, int offset, int len) {
+        super(in, offset, len);
     }
 
     /**
-     * @param arg0
-     * @param arg1
-     * @param arg2
-     * @param arg3
+     * Translates a character array representation of a
+     * <tt>Money</tt> into a <tt>Money</tt>, accepting the
+     * same sequence of characters as the {@link #Money(String)}
+     * constructor, while allowing a sub-array to be specified and
+     * with rounding according to the context settings.
+     *
+     * @param in     <tt>char</tt> array that is the source of characters.
+     * @param offset first character in the array to inspect.
+     * @param len    number of characters to consider..
+     * @param mc     the context to use.
+     * @throws ArithmeticException   if the result is inexact but the
+     *                               rounding mode is <tt>UNNECESSARY</tt>.
+     * @throws NumberFormatException if <tt>in</tt> is not a valid
+     *                               representation of a <tt>Money</tt> or the
+     *                               defined subarray is not wholly within
+     *                               <tt>in</tt>.
      */
-    public Money(char[] arg0, int arg1, int arg2, MathContext arg3) {
-        super(arg0, arg1, arg2, arg3);
+    public Money(char[] in, int offset, int len, MathContext mc) {
+        super(in, offset, len, mc);
     }
 
     /**
-     * @param arg0
+     * Translates a character array representation of a
+     * <tt>Money</tt> into a <tt>Money</tt>, accepting the
+     * same sequence of characters as the {@link #Money(String)}
+     * constructor.
+     *
+     * @param in <tt>char</tt> array that is the source of characters.
+     * @throws NumberFormatException if <tt>in</tt> is not a valid
+     *                               representation of a <tt>Money</tt>.
      */
-    public Money(char[] arg0) {
-        super(arg0);
+    public Money(char[] in) {
+        super(in);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates a character array representation of a
+     * <tt>Money</tt> into a <tt>Money</tt>, accepting the
+     * same sequence of characters as the {@link #Money(String)}
+     * constructor and with rounding according to the context
+     * settings.
+     *
+     * @param in <tt>char</tt> array that is the source of characters.
+     * @param mc the context to use.
+     * @throws ArithmeticException   if the result is inexact but the
+     *                               rounding mode is <tt>UNNECESSARY</tt>.
+     * @throws NumberFormatException if <tt>in</tt> is not a valid
+     *                               representation of a <tt>Money</tt>.
      */
-    public Money(char[] arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(char[] in, MathContext mc) {
+        super(in, mc);
     }
 
     /**
-     * @param arg0
+     * Translates the string representation of a <tt>Money</tt>
+     * into a <tt>Money</tt>.  The string representation consists
+     * of an optional sign, <tt>'+'</tt> (<tt>'&#92;u002B'</tt>) or
+     * <tt>'-'</tt> (<tt>'&#92;u002D'</tt>), followed by a sequence of
+     * zero or more decimal digits ("the integer"), optionally
+     * followed by a fraction, optionally followed by an exponent.
+     *
+     * @param value String representation of <tt>Money</tt>.
+     * @throws NumberFormatException if <tt>val</tt> is not a valid
+     *                               representation of a <tt>Money</tt>.
+     * @see BigDecimal#BigDecimal(String)
      */
-    public Money(String arg0) {
-        super(arg0);
+    public Money(String value) {
+        super(value);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates the string representation of a <tt>Money</tt>
+     * into a <tt>Money</tt>, accepting the same strings as the
+     * {@link #Money(String)} constructor, with rounding
+     * according to the context settings.
+     *
+     * @param value string representation of a <tt>Money</tt>.
+     * @param mc    the context to use.
+     * @throws ArithmeticException   if the result is inexact but the
+     *                               rounding mode is <tt>UNNECESSARY</tt>.
+     * @throws NumberFormatException if <tt>val</tt> is not a valid
+     *                               representation of a Money.
      */
-    public Money(String arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(String value, MathContext mc) {
+        super(value, mc);
     }
 
     /**
-     * @param arg0
+     * Translates a <tt>double</tt> into a <tt>Money</tt> which
+     * is the exact decimal representation of the <tt>double</tt>'s
+     * binary floating-point value.  The scale of the returned
+     * <tt>Money</tt> is the smallest value such that
+     * <tt>(10<sup>scale</sup> &times; val)</tt> is an integer.
+     *
+     * @param value <tt>double</tt> value to be converted to <tt>Money</tt>.
+     * @throws NumberFormatException if <tt>value</tt> is infinite or NaN.
      */
-    public Money(double arg0) {
-        super(arg0);
+    public Money(double value) {
+        super(value);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates a <tt>double</tt> into a <tt>Money</tt>, with
+     * rounding according to the context settings.  The scale of the
+     * <tt>Money</tt> is the smallest value such that
+     * <tt>(10<sup>scale</sup> &times; val)</tt> is an integer.
+     *
+     * @param val <tt>double</tt> value to be converted to <tt>Money</tt>.
+     * @param mc  the context to use.
+     * @throws ArithmeticException   if the result is inexact but the
+     *                               RoundingMode is UNNECESSARY.
+     * @throws NumberFormatException if <tt>val</tt> is infinite or NaN.
      */
-    public Money(double arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(double val, MathContext mc) {
+        super(val, mc);
     }
 
     /**
-     * @param arg0
+     * Translates a <tt>BigInteger</tt> into a <tt>Money</tt>.
+     * The scale of the <tt>Money</tt> is zero.
+     *
+     * @param value <tt>BigInteger</tt> value to be converted to <tt>Money</tt>.
      */
-    public Money(BigInteger arg0) {
-        super(arg0);
+    public Money(BigInteger value) {
+        super(value);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates a <tt>BigInteger</tt> into a <tt>Money</tt>
+     * rounding according to the context settings.  The scale of the
+     * <tt>Money</tt> is zero.
+     *
+     * @param value <tt>BigInteger</tt> value to be converted to <tt>Money</tt>.
+     * @param mc    the context to use
+     * @throws ArithmeticException if the result is inexact but the
+     *                             rounding mode is <tt>UNNECESSARY</tt>.
      */
-    public Money(BigInteger arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(BigInteger value, MathContext mc) {
+        super(value, mc);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates a <tt>BigInteger</tt> unscaled value and an
+     * <tt>int</tt> scale into a <tt>Money</tt>.  The value of the
+     * <tt>Money</tt> is
+     * <tt>(unscaledVal &times; 10<sup>-scale</sup>)</tt>.
+     *
+     * @param unscaledVal unscaled value of the <tt>Money</tt>.
+     * @param scale       scale of the <tt>Money</tt>.
      */
-    public Money(BigInteger arg0, int arg1) {
-        super(arg0, arg1);
+    public Money(BigInteger unscaledVal, int scale) {
+        super(unscaledVal, scale);
     }
 
     /**
-     * @param arg0
-     * @param arg1
-     * @param arg2
+     * Translates a <tt>BigInteger</tt> unscaled value and an
+     * <tt>int</tt> scale into a <tt>Money</tt>, with rounding
+     * according to the context settings.  The value of the
+     * <tt>Money</tt> is <tt>(unscaledVal &times;
+     * 10<sup>-scale</sup>)</tt>, rounded according to the
+     * <tt>precision</tt> and rounding mode settings.
+     *
+     * @param unscaledVal unscaled value of the <tt>Money</tt>.
+     * @param scale       scale of the <tt>Money</tt>.
+     * @param mc          the context to use
+     * @throws ArithmeticException if the result is inexact but the
+     *                             rounding mode is <tt>UNNECESSARY</tt>.
      */
-    public Money(BigInteger arg0, int arg1, MathContext arg2) {
-        super(arg0, arg1, arg2);
+    public Money(BigInteger unscaledVal, int scale, MathContext mc) {
+        super(unscaledVal, scale, mc);
     }
 
     /**
-     * @param arg0
+     * Translates an <tt>int</tt> into a <tt>Money</tt>.  The
+     * scale of the <tt>Money</tt> is zero.
+     *
+     * @param value <tt>int</tt> value to be converted to <tt>Money</tt>.
      */
-    public Money(int arg0) {
-        super(arg0);
+    public Money(int value) {
+        super(value);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates an <tt>int</tt> into a <tt>Money</tt>, with
+     * rounding according to the context settings.  The scale of the
+     * <tt>Money</tt>, before any rounding, is zero.
+     *
+     * @param value <tt>int</tt> value to be converted to <tt>Money</tt>.
+     * @param mc    the context to use.
+     * @throws ArithmeticException if the result is inexact but the
+     *                             rounding mode is <tt>UNNECESSARY</tt>.
      */
-    public Money(int arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(int value, MathContext mc) {
+        super(value, mc);
     }
 
     /**
-     * @param arg0
+     * Translates a <tt>long</tt> into a <tt>Money</tt>.  The
+     * scale of the <tt>Money</tt> is zero.
+     *
+     * @param value <tt>long</tt> value to be converted to <tt>Money</tt>.
      */
-    public Money(long arg0) {
-        super(arg0);
+    public Money(long value) {
+        super(value);
     }
 
     /**
-     * @param arg0
-     * @param arg1
+     * Translates <tt>long</tt> into a <tt>Money</tt>, with rounding according
+     * to the context settings.  The scale of the
+     * <tt>Money</tt>, before any rounding, is zero.
+     *
+     * @param value <tt>long</tt> value to be converted to <tt>Money</tt>.
+     * @param mc    the context to use.
+     * @throws ArithmeticException if the result is inexact but the
+     *                             rounding mode is <tt>UNNECESSARY</tt>.
      */
-    public Money(long arg0, MathContext arg1) {
-        super(arg0, arg1);
+    public Money(long value, MathContext mc) {
+        super(value, mc);
     }
 
 }
