69eaa85dea7ec00ed7110c00a60fb72b241b97ac
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2007-12-06 01:31:50 +0000

    Added act.patientDocumentImage as a document template archetype to entity.documentTemplate for OVPMS-686 Previewing after having selected "Print" for a selected Document generates an application error
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@2517 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/doc/DocumentHandlers.java
+++ src/java/org/openvpms/archetype/rules/doc/DocumentHandlers.java
@@ -153,7 +153,7 @@ public class DocumentHandlers {
      * @param name      the document name
      * @param shortName the document archetype short name
      * @param mimeType  the mime type of the document
-     * @return a handler for the document, or <code>null</code> if none is found
+     * @return a handler for the document 
      * @throws DocumentException if no handler exists
      */
     public DocumentHandler get(String name, String shortName, String mimeType) {
