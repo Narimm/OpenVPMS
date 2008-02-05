cb6d139c84d1d03649820407e26a61549714b527
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2008-02-05 23:41:19 +0000

    Rollback migration changes for OVPMS-728 Expired patient-owner relationships should be able to be filtered in patient lookups for a particular customer
    git-svn-id: svn://localhost/openvpms/release/trunk@2610 9e4fa352-aafb-0310-9071-df624684024b

--- src/update/db/migrate-1.0-to-1.1.sql
+++ src/update/db/migrate-1.0-to-1.1.sql
@@ -2,8 +2,3 @@
 
 alter table assertion_descriptors
     modify property_map mediumtext;
-
-# set the active flag for inactive entity relationships, for OVPMS-728
-
-update entity_relationships
-    set active=0 where active_end_time is not null
