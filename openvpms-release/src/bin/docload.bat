97e560ed86d5f4f8d8b48eccb13f969859900039
Tim Anderson <tma@netspace.net.au>
2008-02-13 00:55:14 +0000

    1.1 branch, excluding transaction support destined for 1.2
    svn path=/vpms/branches/vpms-1.1/; revision=2625

--- openvpms-release/src/bin/docload.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call setenv.bat
-
-java org.openvpms.etl.tools.doc.DocumentLoader -c ../conf/applicationContext.xml %*%
--- openvpms-release/src/bin/genbalance.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call setenv.bat
-
-java org.openvpms.archetype.rules.finance.account.CustomerBalanceGenerator --context ../conf/applicationContext.xml %*%
