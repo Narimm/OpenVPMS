d6bee7cc7af1af166e5b95fea4f69d465597edd3
Tim Anderson <tma@netspace.net.au>
2008-06-02 04:37:47 +0000

    Changes for REL-2 Add SQL to delete dangling entity relationships to migration script
    svn path=/release/trunk/; revision=2809

--- openvpms-release/src/update/db/migrate-1.1-to-1.2.sql
+++ openvpms-release/src/update/db/migrate-1.1-to-1.2.sql
@@ -5,3 +5,14 @@ alter table act_relationships
 
 alter table entity_relationships
     drop column active;
+
+
+# remove dangling entity relationships for OBF-189/REL-2
+
+delete entity_relationships
+from entity_relationships left join entities on source_linkId = entities.linkId
+where entities.linkId is null;
+
+delete entity_relationships
+from entity_relationships left join entities on target_linkId = entities.linkId
+where entities.linkId is null;
