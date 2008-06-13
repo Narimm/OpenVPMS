c541a0dac749dfe5e1e61e13f9a1f76da1e4c6ed
Tim Anderson <tma@netspace.net.au>
2008-06-13 02:11:48 +0000

    Changes for REL-3 Migrate currency details from party.organisationLocation to party.organisationPractice
    svn path=/release/trunk/; revision=2826

--- openvpms-release/src/update/db/migrate-1.1-to-1.2.sql
+++ openvpms-release/src/update/db/migrate-1.1-to-1.2.sql
@@ -16,3 +16,20 @@ where entities.linkId is null;
 delete entity_relationships
 from entity_relationships left join entities on target_linkId = entities.linkId
 where entities.linkId is null;
+
+# move currency details for OVPMS-759/REL-3
+
+insert into entity_details (entity_id, name, type, value)
+select distinct e.entity_id, "currency", "string", d.value
+from entities e,
+     entities l join entity_details d
+         on l.entity_id = d.entity_id
+             and l.arch_short_name="party.organisationLocation"
+             and d.name="currency"
+where e.arch_short_name = "party.organisationPractice";
+
+delete d.*
+from entity_details d join entities e
+         on e.entity_id = d.entity_id
+             and e.arch_short_name = "party.organisationLocation"
+             and d.name="currency";
