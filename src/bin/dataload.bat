2f197d8537509a616414923e814582ccb833c92d
tony <tony@9e4fa352-aafb-0310-9071-df624684024b>
2008-12-05 22:05:08 +0000

    added ability for passed files
    git-svn-id: svn://localhost/openvpms/release/trunk@3157 9e4fa352-aafb-0310-9071-df624684024b

--- src/bin/dataload.bat
+++ src/bin/dataload.bat
@@ -4,6 +4,7 @@ call setenv.bat
 
 if ""%1"" == ""base"" goto doBase
 if ""%1"" == ""setup"" goto doSetup
+goto doOther
 
 echo Usage:  dataload ( commands ... )
 echo commands:
@@ -16,6 +17,9 @@ java org.openvpms.tools.data.loader.StaxArchetypeDataLoader -c ../conf/applicati
 goto end
 
 :doSetup
-java org.openvpms.tools.data.loader.StaxArchetypeDataLoader -c ../conf/applicationContext.xml -f ../import/data/base.xml,../import/data/setup.xml,../import/data/postcodes.xml -b 1000
+java org.openvpms.tools.data.loader.StaxArchetypeDataLoader -c ../conf/applicationContext.xml -f ../import/data/base.xml,../import/data/setup.xml -b 1000
+goto end
 
+:doOther
+java org.openvpms.tools.data.loader.StaxArchetypeDataLoader -c ../conf/applicationContext.xml -f %*% -b 1000
 :end
