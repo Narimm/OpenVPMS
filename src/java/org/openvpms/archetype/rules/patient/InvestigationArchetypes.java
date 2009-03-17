c1bbb854581de20688a95a55b63e352b0a7af836
tony <tony@9e4fa352-aafb-0310-9071-df624684024b>
2009-03-17 04:53:06 +0000

    Investigation Act definitions for new Investigation Workspace and query.
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@3253 9e4fa352-aafb-0310-9071-df624684024b

---
+++ src/java/org/openvpms/archetype/rules/patient/InvestigationArchetypes.java
@@ -0,0 +1,54 @@
+/*
+ *  Version: 1.0
+ *
+ *  The contents of this file are subject to the OpenVPMS License Version
+ *  1.0 (the 'License'); you may not use this file except in compliance with
+ *  the License. You may obtain a copy of the License at
+ *  http://www.openvpms.org/license/
+ *
+ *  Software distributed under the License is distributed on an 'AS IS' basis,
+ *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing rights and limitations under the
+ *  License.
+ *
+ *  Copyright 2008 (C) OpenVPMS Ltd. All Rights Reserved.
+ *
+ *  $Id$
+ */
+
+package org.openvpms.archetype.rules.patient;
+
+
+/**
+ * Customer archetypes.
+ *
+ * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
+ * @version $LastChangedDate$
+ */
+public class InvestigationArchetypes {
+
+    /**
+     * Biochemistry investigation short name.
+     */
+    public static final String BIOCHEMISTRY = "act.patientInvestigationBiochemistry";
+
+    /**
+     * Cytology investigation short name.
+     */
+    public static final String CYTOLOGY = "act.patientInvestigationCytology";
+
+    /**
+     * Haematology investigation short name.
+     */
+    public static final String HAEMATOLOGY = "act.patientInvestigationHaematology";
+
+    /**
+     * Radiology investigation short name.
+     */
+    public static final String RADIOLOGY = "act.patientInvestigationRadiology";
+
+    /**
+     * General investigation short name.
+     */
+    public static final String GENERAL = "act.patientInvestigation";
+}
