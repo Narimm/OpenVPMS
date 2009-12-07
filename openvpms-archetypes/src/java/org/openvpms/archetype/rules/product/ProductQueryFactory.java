13ca7aa1fa3737312ae99fe0c9cdbe1922597089
Tim Anderson <tma@netspace.net.au>
2009-12-07 02:13:45 +0000

    Changes for OVPMS-877 Add support to query entities by identifier
    svn path=/archetypes/branches/openvpms-archetypes-1.5/; revision=3513

--- openvpms-archetypes/src/java/org/openvpms/archetype/rules/product/ProductQueryFactory.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/rules/product/ProductQueryFactory.java
@@ -19,13 +19,9 @@
 package org.openvpms.archetype.rules.product;
 
 import org.apache.commons.lang.StringUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.openvpms.component.business.service.archetype.helper.DescriptorHelper;
 import org.openvpms.component.system.common.query.ArchetypeQuery;
+import org.openvpms.component.system.common.query.Constraints;
 import org.openvpms.component.system.common.query.IArchetypeQuery;
-import org.openvpms.component.system.common.query.NamedQuery;
-import org.openvpms.component.system.common.query.NodeConstraint;
 import org.openvpms.component.system.common.query.SortConstraint;
 
 
@@ -38,23 +34,14 @@ import org.openvpms.component.system.common.query.SortConstraint;
 public class ProductQueryFactory {
 
     /**
-     * The logger.
-     */
-    private static final Log log = LogFactory.getLog(ProductQueryFactory.class);
-
-
-    /**
      * Creates a new product query.
      *
      * @param shortName the product short name. May contain wildcards.
-     * @param name      the product name. May be <code>null</code> or contain
-     *                  wildcards
-     * @param sort      the sort criteria. May be <code>null</code>
+     * @param name      the product name. May be <tt>null</tt> or contain wildcards
+     * @param sort      the sort criteria. May be <tt>null</tt>
      * @return a new product query
      */
-    public static IArchetypeQuery create(String shortName,
-                                         String name,
-                                         SortConstraint[] sort) {
+    public static IArchetypeQuery create(String shortName, String name, SortConstraint[] sort) {
         return create(new String[]{shortName}, name, sort);
     }
 
@@ -62,14 +49,11 @@ public class ProductQueryFactory {
      * Creates a new product query.
      *
      * @param shortNames the product short names
-     * @param name       the product name. May be <code>null</code> or contain
-     *                   wildcards
-     * @param sort       the sort criteria. May be <code>null</code>
+     * @param name       the product name. May be <tt>null</tt> or contain wildcards
+     * @param sort       the sort criteria. May be <tt>null</tt>
      * @return a new product query
      */
-    public static IArchetypeQuery create(String[] shortNames,
-                                         String name,
-                                         SortConstraint[] sort) {
+    public static IArchetypeQuery create(String[] shortNames, String name, SortConstraint[] sort) {
         return create(shortNames, name, null, sort);
     }
 
@@ -77,39 +61,21 @@ public class ProductQueryFactory {
      * Creates a new product query, optionally constrained on species type.
      *
      * @param shortNames the product short names
-     * @param name       the product name. May be <code>null</code> or contain
-     *                   wildcards
-     * @param species    the species type. May be <code>null</code>
-     * @param sort       the sort criteria. May be <code>null</code>
+     * @param name       the product name. May be <tt>null</tt> or contain wildcards
+     * @param species    the species lookup code. May be <tt>null</tt>
+     * @param sort       the sort criteria. May be <tt>null</tt>
      * @return a new product query
      */
-    public static IArchetypeQuery create(String[] shortNames,
-                                         String name, String species,
-                                         SortConstraint[] sort) {
-        shortNames = DescriptorHelper.getShortNames(shortNames);
-        if (species != null && shortNames.length <= 4) {
-            return createNamedQuery(shortNames, name, species);
-        } else if (species != null) {
-            log.warn("Cannot perform query on species: too many products");
-        }
-        return createArchetypeQuery(shortNames, name, sort);
-    }
-
-    /**
-     * Creates a new query.
-     *
-     * @param shortNames the product short names
-     * @param name       the product name. May be <code>null</code> or contain
-     *                   wildcards
-     * @param sort       the sort criteria. May be <code>null</code>
-     * @return a new query
-     */
-    private static ArchetypeQuery createArchetypeQuery(String[] shortNames,
-                                                       String name,
-                                                       SortConstraint[] sort) {
+    public static IArchetypeQuery create(String[] shortNames, String name, String species, SortConstraint[] sort) {
         ArchetypeQuery query = new ArchetypeQuery(shortNames, true, true);
         if (!StringUtils.isEmpty(name)) {
-            query.add(new NodeConstraint("name", name));
+            query.add(Constraints.eq("name", name));
+        }
+        if (!StringUtils.isEmpty(species)) {
+            query.setDistinct(true);
+            query.add(Constraints.leftJoin("species"))
+                    .add(Constraints.or(Constraints.eq("species.code", species),
+                                        Constraints.isNull("species.code")));
         }
         if (sort != null) {
             for (SortConstraint s : sort) {
@@ -119,33 +85,4 @@ public class ProductQueryFactory {
         return query;
     }
 
-    /**
-     * Creates a new named query.
-     *
-     * @param shortNames the product short names
-     * @param name       the product name. May be <code>null</code> or contain
-     *                   wildcards
-     * @param name       the product name. May contain wildcards
-     * @param species    the species type
-     * @return a new named query
-     */
-    private static NamedQuery createNamedQuery(String[] shortNames, String name,
-                                               String species) {
-        NamedQuery query;
-        int count = shortNames.length;
-        if (!StringUtils.isEmpty(name)) {
-            query = new NamedQuery("product.nameAndSpeciesQuery" + count);
-            name = name.replace("*", "%");
-            query.setParameter("name", name);
-        } else {
-            query = new NamedQuery("product.speciesQuery" + count);
-        }
-        query.setParameter("species", species);
-        int index = 1;
-        for (String shortName : shortNames) {
-            query.setParameter("shortName" + index, shortName);
-            ++index;
-        }
-        return query;
-    }
 }
