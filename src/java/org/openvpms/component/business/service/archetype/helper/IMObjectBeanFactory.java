7a26d07e58dedbfe87cd4efe4f3914e653d3a3bc
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2010-04-28 23:44:15 +0000

    Added new factory methods to IMObjectBeanFactory
    git-svn-id: svn://localhost/openvpms/framework/branches/openvpms-framework-1.5@3728 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/component/business/service/archetype/helper/IMObjectBeanFactory.java
+++ src/java/org/openvpms/component/business/service/archetype/helper/IMObjectBeanFactory.java
@@ -20,6 +20,8 @@ package org.openvpms.component.business.service.archetype.helper;
 import org.openvpms.component.business.domain.im.act.Act;
 import org.openvpms.component.business.domain.im.common.Entity;
 import org.openvpms.component.business.domain.im.common.IMObject;
+import org.openvpms.component.business.domain.im.common.IMObjectReference;
+import org.openvpms.component.business.service.archetype.ArchetypeServiceException;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 
 
@@ -38,7 +40,7 @@ public class IMObjectBeanFactory {
 
 
     /**
-     * Constructs a <tt>IMObjectBeanFactory</tt>.
+     * Constructs an <tt>IMObjectBeanFactory</tt>.
      *
      * @param service the archetype service
      */
@@ -47,33 +49,119 @@ public class IMObjectBeanFactory {
     }
 
     /**
-     * Creates a new {@link IMObjectBean} for an object.
+     * Creates an {@link IMObjectBean} for an object.
      *
      * @param object the object
      * @return a new {@link IMObjectBean} for the object
      */
-    public IMObjectBean create(IMObject object) {
+    public IMObjectBean createBean(IMObject object) {
         return new IMObjectBean(object, service);
     }
 
     /**
-     * Creates a new {@link ActBean} for an act.
+     * Creates an object with the specified short name, and wraps it in an {@link IMObjectBean}.
+     *
+     * @param shortName the archetype short name
+     * @return an {@link IMObjectBean} wrapping the new object
+     * @throws ArchetypeServiceException if the object can't be created
+     * @throws IMObjectBeanException     if the archetype short name is invalid
+     */
+    public IMObjectBean createBean(String shortName) {
+        IMObject object = service.create(shortName);
+        if (object == null) {
+            throw new IMObjectBeanException(IMObjectBeanException.ErrorCode.ArchetypeNotFound, shortName);
+        }
+        return createBean(object);
+    }
+
+    /**
+     * Retrieves the object with the specified reference, and wraps it in an {@link IMObjectBean}.
+     *
+     * @param reference the object reference
+     * @return an {@link IMObjectBean} wrapping the retrieved object, or <tt>null</tt> if the object couldn't be
+     *         retrieved
+     * @throws ArchetypeServiceException if the query fails
+     */
+    public IMObjectBean createBean(IMObjectReference reference) {
+        IMObject object = service.get(reference);
+        return (object != null) ? createBean(object) : null;
+    }
+
+    /**
+     * Creates an {@link ActBean} for an act.
      *
      * @param act the act
      * @return a new {@link ActBean} for the act
      */
-    public ActBean create(Act act) {
+    public ActBean createActBean(Act act) {
         return new ActBean(act, service);
     }
 
     /**
-     * Creates a new {@link EntityBean} for an entity.
+     * Creates an act with the specified short name, and wraps it in an {@link ActBean}.
+     *
+     * @param shortName the act archetype short name
+     * @return an {@link ActBean} wrapping the new act
+     * @throws ArchetypeServiceException if the object can't be created
+     * @throws IMObjectBeanException     if the archetype short name is invalid
+     */
+    public ActBean createActBean(String shortName) {
+        Act object = (Act) service.create(shortName);
+        if (object == null) {
+            throw new IMObjectBeanException(IMObjectBeanException.ErrorCode.ArchetypeNotFound, shortName);
+        }
+        return createActBean(object);
+    }
+
+    /**
+     * Retrieves the act with the specified reference, and wraps it in an {@link ActBean}.
+     *
+     * @param reference the object reference
+     * @return an {@link ActBean} wrapping the retrieved object, or <tt>null</tt> if the object couldn't be retrieved
+     * @throws ArchetypeServiceException if the query fails
+     * @throws ClassCastException if the retrieved object isn't an <tt>Act</tt>
+     */
+    public ActBean createActBean(IMObjectReference reference) {
+        Act object = (Act) service.get(reference);
+        return (object != null) ? createActBean(object) : null;
+    }
+
+    /**
+     * Creates an {@link EntityBean} for an entity.
      *
      * @param entity the entity
      * @return a new {@link ActBean} for the entity
      */
-    public EntityBean create(Entity entity) {
+    public EntityBean createEntityBean(Entity entity) {
         return new EntityBean(entity, service);
     }
 
+    /**
+     * Creates an entity with the specified short name, and wraps it in an {@link EntityBean}.
+     *
+     * @param shortName the entity archetype short name
+     * @return an {@link EntityBean} wrapping the new entity
+     * @throws ArchetypeServiceException if the object can't be created
+     * @throws IMObjectBeanException     if the archetype short name is invalid
+     */
+    public EntityBean createEntityBean(String shortName) {
+        Entity object = (Entity) service.create(shortName);
+        if (object == null) {
+            throw new IMObjectBeanException(IMObjectBeanException.ErrorCode.ArchetypeNotFound, shortName);
+        }
+        return createEntityBean(object);
+    }
+
+    /**
+     * Retrieves the act with the specified reference, and wraps it in an {@link ActBean}.
+     *
+     * @param reference the object reference
+     * @return an {@link EntityBean} wrapping the retrieved object, or <tt>null</tt> if the object couldn't be retrieved
+     * @throws ArchetypeServiceException if the query fails
+     * @throws ClassCastException if the retrieved object isn't an <tt>Entity</tt>
+     */
+    public EntityBean createEntityBean(IMObjectReference reference) {
+        Entity object = (Entity) service.get(reference);
+        return (object != null) ? createEntityBean(object) : null;
+    }
 }
