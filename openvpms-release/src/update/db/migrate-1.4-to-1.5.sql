e87bcd87f04ccb67ecb50bdd9597466507522375
Tim Anderson <tma@netspace.net.au>
2010-12-03 04:27:43 +0000

    Changes for ARCH-34 Create customer alert archetypes
    svn path=/release/trunk/; revision=3910

---
+++ openvpms-release/src/update/db/migrate-1.4-to-1.5.sql
@@ -0,0 +1,90 @@
+# 
+# Create customer alerts for ARCH-34
+#
+
+# create lookup.customerAlertType instances for each 
+# lookup.customerNoteCategory that is associated with a note with alert="true"
+# Default the priority to "HIGH" and the colour to "0xff0000" (red)
+
+insert into lookups (version, linkId, arch_short_name, active, arch_version,
+    code, name, description, default_lookup)
+select distinct 0, UUID(), "lookup.customerAlertType", l.active, "1.0", l.code, l.name, l.description, l.default_lookup
+from acts a
+join act_details d on a.act_id = d.act_id and d.name="alert" and d.value="true"
+join act_details c on a.act_id = c.act_id and c.name = "category"
+join lookups l on l.arch_short_name = "lookup.customerNoteCategory"
+where a.arch_short_name = "act.customerNote";
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "priority", "string", "HIGH"
+from lookups l
+where l.arch_short_name = "lookup.customerAlertType";
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "colour", "string", "0xff0000"
+from lookups l
+where l.arch_short_name = "lookup.customerAlertType";
+
+# Migrate all act.customerNote with alert="true" to act.customerAlert
+
+update acts a, act_details d
+set a.arch_short_name = "act.customerAlert"
+where a.arch_short_name = "act.customerNote"
+      and a.act_id = d.act_id
+      and d.name = "alert" and d.value = "true";
+
+update acts a, act_details d
+set d.name = "alertType" 
+where a.arch_short_name = "act.customerAlert"
+      and a.act_id = d.act_id
+      and d.name = "category";
+
+update acts a, act_details d
+set d.name = "notes"
+where a.arch_short_name = "act.customerAlert"
+      and a.act_id = d.act_id
+      and d.name = "note";
+
+# duplicate a portion of the notes to the reason field, truncating long notes
+update acts a,act_details d
+set a.reason = if (length(d.value) > 80, concat(left(d.value, 80), "..."), d.value)
+where a.act_id = d.act_id
+     and a.arch_short_name = "act.customerAlert"
+     and d.name = "notes"
+
+update acts a, participations p
+set p.act_arch_short_name = "act.customerAlert"
+where a.act_id = p.act_id and a.arch_short_name = "act.customerAlert" 
+      and p.act_arch_short_name = "act.customerNote"
+
+# Set the act status to IN_PROGRESS. 
+update acts a
+set a.status = "IN_PROGRESS"
+where a.arch_short_name = "act.customerAlert";
+
+# Delete redundant alert flag
+delete d
+from acts a, act_details d
+where a.arch_short_name in ("act.customerAlert", "act.customerNote")
+      and a.act_id = d.act_id 
+      and d.name = "alert";
+
+# Rename lookup.alertType to lookup.patientAlertType and 
+# default the priority to "HIGH" and the colour to "0xff0000" (red)
+
+update lookups l 
+set l.arch_short_name = "lookup.patientAlertType" 
+where l.arch_short_name = "lookup.alertType";
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "priority", "string", "HIGH"
+from lookups l
+where l.arch_short_name = "lookup.patientAlertType";
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "colour", "string", "0xff0000"
+from lookups l
+where l.arch_short_name = "lookup.patientAlertType";
+
+
+
