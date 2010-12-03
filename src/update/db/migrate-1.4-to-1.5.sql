2036978752268f794dacace3a052511a2d6386fb
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2010-12-03 06:54:08 +0000

    Changes for ARCH-35 Link customer account type to customer alert type
    git-svn-id: svn://localhost/openvpms/release/trunk@3912 9e4fa352-aafb-0310-9071-df624684024b

--- src/update/db/migrate-1.4-to-1.5.sql
+++ src/update/db/migrate-1.4-to-1.5.sql
@@ -18,14 +18,16 @@ where a.arch_short_name = "act.customerNote";
 insert into lookup_details (lookup_id, name, type, value)
 select l.lookup_id, "priority", "string", "HIGH"
 from lookups l
-where l.arch_short_name = "lookup.customerAlertType";
+where l.arch_short_name = "lookup.customerAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name = "priority");
 
 insert into lookup_details (lookup_id, name, type, value)
 select l.lookup_id, "colour", "string", "0xff0000"
 from lookups l
-where l.arch_short_name = "lookup.customerAlertType";
+where l.arch_short_name = "lookup.customerAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name = "colour");
 
-# Migrate all act.customerNote with alert="true" to act.customerAlert
+# migrate all act.customerNote with alert="true" to act.customerAlert
 
 update acts a, act_details d
 set a.arch_short_name = "act.customerAlert"
@@ -50,19 +52,19 @@ update acts a,act_details d
 set a.reason = if (length(d.value) > 80, concat(left(d.value, 80), "..."), d.value)
 where a.act_id = d.act_id
      and a.arch_short_name = "act.customerAlert"
-     and d.name = "notes"
+     and d.name = "notes";
 
 update acts a, participations p
 set p.act_arch_short_name = "act.customerAlert"
 where a.act_id = p.act_id and a.arch_short_name = "act.customerAlert" 
-      and p.act_arch_short_name = "act.customerNote"
+      and p.act_arch_short_name = "act.customerNote";
 
-# Set the act status to IN_PROGRESS. 
+# set the act status to IN_PROGRESS.
 update acts a
 set a.status = "IN_PROGRESS"
 where a.arch_short_name = "act.customerAlert";
 
-# Delete redundant alert flag
+# delete redundant alert flag
 delete d
 from acts a, act_details d
 where a.arch_short_name in ("act.customerAlert", "act.customerNote")
@@ -79,12 +81,76 @@ where l.arch_short_name = "lookup.alertType";
 insert into lookup_details (lookup_id, name, type, value)
 select l.lookup_id, "priority", "string", "HIGH"
 from lookups l
-where l.arch_short_name = "lookup.patientAlertType";
+where l.arch_short_name = "lookup.patientAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name = "priority");
 
 insert into lookup_details (lookup_id, name, type, value)
 select l.lookup_id, "colour", "string", "0xff0000"
 from lookups l
-where l.arch_short_name = "lookup.patientAlertType";
+where l.arch_short_name = "lookup.patientAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name = "colour");
+
+# remove lookup.alertType archetype
+
+delete d
+from assertion_descriptors d, node_descriptors n, archetype_descriptors a
+where d.node_desc_id = n.node_desc_id 
+      and n.archetype_desc_id = a.archetype_desc_id
+      and a.name = "lookup.alertType.1.0";
+
+delete a, n
+from node_descriptors n, archetype_descriptors a
+where n.archetype_desc_id = a.archetype_desc_id 
+      and a.name = "lookup.alertType.1.0"
 
 
+#
+# Link customer account types to customer alerts for ARCH-35
+#
+
+# create a lookup.customerAlertType for each lookup.customerAccountType that has showAlert="true"
+# ensuring that a lookup doesn't already exist
+
+insert into lookups (version, linkId, arch_short_name, active, arch_version,
+    code, name, description, default_lookup)
+select 0, UUID(), "lookup.customerAlertType", l.active, "1.0", l.code, l.name, l.description, l.default_lookup
+from lookups l
+left join lookups e on e.arch_short_name = "lookup.customerAlertType" and e.code = l.code
+join lookup_details d on l.lookup_id = d.lookup_id
+where l.arch_short_name = "lookup.customerAccountType"
+      and d.name = "showAlert"
+      and d.value = "true"
+      and e.code is null;
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "priority", "string", "HIGH"
+from lookups l
+where l.arch_short_name = "lookup.customerAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name ="priority");
+
+insert into lookup_details (lookup_id, name, type, value)
+select l.lookup_id, "colour", "string", "0xff0000"
+from lookups l
+where l.arch_short_name = "lookup.customerAlertType"
+      and not exists (select * from lookup_details d where d.lookup_id = l.lookup_id and d.name ="colour");
+
+# create a relationship between the account type and alert, if it doesn't already exist
+
+insert into lookup_relationships (version, linkId, arch_short_name, arch_version, active, source_id, target_id)
+select 0, UUID(), "lookupRelationship.customerAccountAlert", "1.0", 1, l.lookup_id, alertType.lookup_id
+from lookups l
+join lookup_details d on l.arch_short_name = "lookup.customerAccountType"
+                      and l.lookup_id = d.lookup_id
+                      and d.name = "showAlert"
+                      and d.value = "true"
+join lookups alertType on alertType.arch_short_name = "lookup.customerAlertType" and alertType.code = l.code
+where not exists
+  (select *
+   from lookup_relationships r
+   where r.source_id = l.lookup_id and r.target_id = alertType.lookup_id);
+
+# delete redundant showAlert flag
+delete d
+from lookups l
+join lookup_details d on l.arch_short_name = "lookup.customerAccountType" and d.name = "showAlert";
 
