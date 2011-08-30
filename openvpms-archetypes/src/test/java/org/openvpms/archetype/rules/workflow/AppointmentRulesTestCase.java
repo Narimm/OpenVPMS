06c11fb25a79310ee47c3b4e2b2dc27190317bee
Tim Anderson <tma@netspace.net.au>
2011-08-30 05:44:44 +0000

    merge back changes from trunk
    svn path=/archetypes/branches/1.6/; revision=4362

--- openvpms-archetypes/src/test/java/org/openvpms/archetype/rules/workflow/AppointmentRulesTestCase.java
+++ openvpms-archetypes/src/test/java/org/openvpms/archetype/rules/workflow/AppointmentRulesTestCase.java
@@ -18,7 +18,11 @@
 
 package org.openvpms.archetype.rules.workflow;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.openvpms.archetype.rules.act.ActStatus;
