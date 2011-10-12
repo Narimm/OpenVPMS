dbc4b1ee14dd565e999c3fe6d290b595a2383e39
Tim Anderson <tma@netspace.net.au>
2011-10-12 00:22:17 +0000

    Changes for OVPMS-902 SMS integration through an email-sms gateway
    svn path=/release/trunk/; revision=4413

--- openvpms-release/src/bin/archload.bat
+++ openvpms-release/src/bin/archload.bat
@@ -2,4 +2,4 @@
 
 call setenv.bat
 
-java org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes/org/openvpms/archetype
+java org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes/org/openvpms
