33750df490e1743109dd27b4ad9477eb64c9ebff
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2012-06-08 01:35:44 +0000

    Fix for OVPMS-1007 Stock Adjustments and Transfers dont update stock if status updated directly
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@4691 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/stock/StockUpdater.java
+++ src/java/org/openvpms/archetype/rules/stock/StockUpdater.java
@@ -19,47 +19,30 @@
 package org.openvpms.archetype.rules.stock;
 
 import org.openvpms.archetype.rules.act.ActStatus;
-import org.openvpms.archetype.rules.act.ActStatusHelper;
-import org.openvpms.archetype.rules.product.ProductArchetypes;
 import org.openvpms.component.business.domain.im.act.Act;
-import org.openvpms.component.business.domain.im.act.FinancialAct;
-import org.openvpms.component.business.domain.im.common.Entity;
-import org.openvpms.component.business.domain.im.common.EntityRelationship;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.party.Party;
 import org.openvpms.component.business.domain.im.product.Product;
 import org.openvpms.component.business.service.archetype.ArchetypeServiceException;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.archetype.helper.ActBean;
-import org.openvpms.component.business.service.archetype.helper.EntityBean;
 import org.openvpms.component.business.service.archetype.helper.TypeHelper;
 
 import java.math.BigDecimal;
 import java.util.Collections;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 
 /**
- * Updates stock levels associated with <em>act.stockTransfer</em>,
- * <em>act.stockAdjust</em> and <em>act.customerAccountCharge*</em>
+ * Updates stock levels associated with <em>act.stockTransfer</em>, and <em>act.stockAdjust</em> acts.
  * acts.
  *
  * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
  * @version $LastChangedDate: 2006-05-02 05:16:31Z $
  */
 public class StockUpdater {
-
-    /**
-     * The <em>act.customerAccountCharges*</em> act.
-     */
-    private final Act act;
-
-    /**
-     * The set of objects to save on completion.
-     */
-    private Set<IMObject> toSave = new LinkedHashSet<IMObject>();
 
     /**
      * The archetype service.
@@ -73,34 +56,30 @@ public class StockUpdater {
 
 
     /**
-     * Creates a new <tt>StockUpdater</tt>.
+     * Constructs a {@code StockUpdater}.
      *
-     * @param act     the act
      * @param service the service
      */
-    public StockUpdater(Act act, IArchetypeService service) {
-        this.act = act;
+    public StockUpdater(IArchetypeService service) {
         this.service = service;
         this.rules = new StockRules(service);
     }
 
     /**
-     * Updates stock quantities, if the act is POSTED and hasn't already been
-     * posted.
+     * Updates stock quantities, if the act is POSTED and hasn't already been posted.
      *
+     * @param act the act to update stock quantities
      * @throws ArchetypeServiceException for any archetype service error
      */
-    public void update() {
-        if (ActStatus.POSTED.equals(act.getStatus())
-                && !ActStatusHelper.isPosted(act, service)) {
+    public void update(Act act) {
+        if (ActStatus.POSTED.equals(act.getStatus())) {
+            Set<IMObject> toSave = null;
             if (TypeHelper.isA(act, StockArchetypes.STOCK_TRANSFER)) {
-                transferStock();
+                toSave = transferStock(act);
             } else if (TypeHelper.isA(act, StockArchetypes.STOCK_ADJUST)) {
-                adjustStock();
-            } else {
-                updateChargeStock();
+                toSave = adjustStock(act);
             }
-            if (!toSave.isEmpty()) {
+            if (toSave != null && !toSave.isEmpty()) {
                 service.save(toSave);
             }
         }
@@ -109,9 +88,11 @@ public class StockUpdater {
     /**
      * Transfers stock using an <em>act.stockTransfer</em> act.
      *
+     * @param act the <em>act.stockTransfer</em>
      * @throws ArchetypeServiceException for any archetype service error
      */
-    private void transferStock() {
+    private Set<IMObject> transferStock(Act act) {
+        Set<IMObject> toSave = new HashSet<IMObject>();
         ActBean bean = new ActBean(act, service);
         Party from = (Party) bean.getNodeParticipant("stockLocation");
         Party to = (Party) bean.getNodeParticipant("to");
@@ -120,6 +101,7 @@ public class StockUpdater {
                 toSave.addAll(transferStock(item, from, to));
             }
         }
+        return toSave;
     }
 
     /**
@@ -148,16 +130,19 @@ public class StockUpdater {
     /**
      * Adjusts stock using an <em>act.stockTransfer</em>.
      *
+     * @param act the <em>act.stockTransfer</em> act
      * @throws ArchetypeServiceException for any archetype service error
      */
-    private void adjustStock() {
+    private Set<IMObject> adjustStock(Act act) {
+        Set<IMObject> toSave = new HashSet<IMObject>();
         ActBean bean = new ActBean(act, service);
         Party stockLocation = (Party) bean.getNodeParticipant("stockLocation");
         if (stockLocation != null) {
             for (Act item : bean.getNodeActs("items")) {
-                adjustStock(item, stockLocation);
+                toSave.addAll(adjustStock(item, stockLocation));
             }
         }
+        return toSave;
     }
 
     /**
@@ -165,7 +150,8 @@ public class StockUpdater {
      *
      * @throws ArchetypeServiceException for any archetype service error
      */
-    private void adjustStock(Act item, Party stockLocation) {
+    private Set<IMObject> adjustStock(Act item, Party stockLocation) {
+        Set<IMObject> toSave = new HashSet<IMObject>();
         ActBean itemBean = new ActBean(item, service);
         Product product = (Product) itemBean.getNodeParticipant("product");
         BigDecimal quantity = itemBean.getBigDecimal("quantity",
@@ -173,90 +159,7 @@ public class StockUpdater {
         if (product != null && quantity.compareTo(BigDecimal.ZERO) != 0) {
             toSave.addAll(rules.calcStock(product, stockLocation, quantity));
         }
-    }
-
-    /**
-     * Updates stocks used by an <em>act.customerAccountCharge*</em> act.
-     *
-     * @throws ArchetypeServiceException for any archetype service error
-     */
-    private void updateChargeStock() {
-        ActBean bean = new ActBean(act, service);
-        Party location = (Party) bean.getNodeParticipant("location");
-        if (location != null) {
-            for (FinancialAct item
-                    : bean.getNodeActs("items", FinancialAct.class)) {
-                updateChargeItemStock(item, location);
-            }
-        }
-    }
-
-    /**
-     * Updates stock quantities at the location used by the charge item,
-     * if the associated product is a <em>product.medication</em>
-     * or <em>product.merchandise</em>.
-     *
-     * @param item     the charge item
-     * @param location an <em>party.organisationLocation</em>
-     * @throws ArchetypeServiceException for any archetype service error
-     */
-    private void updateChargeItemStock(FinancialAct item, Party location) {
-        BigDecimal quantity = item.getQuantity();
-        if (quantity.compareTo(BigDecimal.ZERO) != 0) {
-            ActBean itemBean = new ActBean(item, service);
-            Product product = (Product) itemBean.getNodeParticipant("product");
-            if (TypeHelper.isA(product, ProductArchetypes.MEDICATION,
-                               ProductArchetypes.MERCHANDISE)) {
-                if (!((FinancialAct) act).isCredit()) {
-                    quantity = quantity.negate();
-                }
-                updateProductStockLocation(product, location, quantity);
-            }
-        }
-    }
-
-    /**
-     * Updates product quantities at the location.
-     * <p/>
-     * Looks for an <em>party.organisationStockLocation</em> associated with
-     * the supplied <em>party.organisationLocation</em>. If a stock location
-     * exists that already has a relationship to the supplied product, this will
-     * be updated.
-     * <p/>If there are stock locations, but none have a relationship
-     * to the product, then an arbitrary one will be selected, and a
-     * relationship created (todo - not ideal).
-     * <p/>If there are no stock locations associated with the location, the
-     * update is ignored.
-     *
-     * @param product  the product
-     * @param location an <em>party.organisationLocation</em>
-     * @param quantity the quantity used/returned. Negative if quantities are
-     *                 being used
-     * @throws ArchetypeServiceException for any archetype service error
-     */
-    private void updateProductStockLocation(Product product, Party location,
-                                            BigDecimal quantity) {
-        EntityBean locBean = new EntityBean(location, service);
-        List<Entity> entities = locBean.getNodeTargetEntities("stockLocations");
-        EntityRelationship relationship = null;
-        Party stockLocation = null;
-        for (Entity stockLoc : entities) {
-            stockLocation = (Party) stockLoc;
-            relationship = rules.getStockRelationship(product, stockLocation);
-            if (relationship != null) {
-                break;
-            }
-        }
-        if (stockLocation != null) {
-            if (relationship == null) {
-                List<IMObject> objects = rules.calcStock(product, stockLocation,
-                                                         quantity);
-                toSave.addAll(objects);
-            } else {
-                rules.calcStock(relationship, quantity);
-                toSave.add(relationship);
-            }
-        }
+        return toSave;
     }
 
 }
