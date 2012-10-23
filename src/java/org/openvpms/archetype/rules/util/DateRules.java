522434b343df52a967c340c1598371fbf8ded148
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2012-10-23 22:49:19 +0000

    Added DateRules.getToday(), DateRules.getTomorrow()
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@4858 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/util/DateRules.java
+++ src/java/org/openvpms/archetype/rules/util/DateRules.java
@@ -12,8 +12,6 @@
  *  License.
  *
  *  Copyright 2007 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
  */
 
 package org.openvpms.archetype.rules.util;
@@ -29,8 +27,7 @@ import java.util.GregorianCalendar;
 /**
  * Date rules.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 public class DateRules {
 
@@ -38,8 +35,7 @@ public class DateRules {
      * Calculates a date given a start time, interval and the date units.
      *
      * @param startTime the start time
-     * @param interval  the time interval. May be negative to calculate a date
-     *                  in the past
+     * @param interval  the time interval. May be negative to calculate a date in the past
      * @param units     the interval units
      * @return the date
      */
@@ -72,12 +68,28 @@ public class DateRules {
     }
 
     /**
-     * Returns the date part of a date-time, zero-ing out any time
-     * component.
+     * Returns today's date, minus any time component.
      *
-     * @param datetime the date/time. May be <tt>null</tt>
-     * @return the date part of <tt>datetime</tt>, or <tt>null</tt> if
-     *         <tt>datetime</tt> is null
+     * @return today's date
+     */
+    public static Date getToday() {
+        return getDate(new Date());
+    }
+
+    /**
+     * Returns tomorrow's date, minus any time component.
+     *
+     * @return tomorrow's date
+     */
+    public static Date getTomorrow() {
+        return DateRules.getDate(getToday(), 1, DateUnits.DAYS);
+    }
+
+    /**
+     * Returns the date part of a date-time, zero-ing out any time component.
+     *
+     * @param datetime the date/time. May be {@code null}
+     * @return the date part of {@code datetime}, or {@code null} if {@code datetime} is null
      */
     public static Date getDate(Date datetime) {
         if (datetime == null) {
@@ -90,18 +102,18 @@ public class DateRules {
      * Helper to compare two dates.
      * <p/>
      * This is functionally equivalent to the {@link Date#compareTo(Date)}
-     * method, except that it doesn't throw <tt>ClassCastExceptions</tt>
-     * if <tt>lhs</tt> is an instance of a {@link Timestamp Timestamp} and
-     * <tt>rhs</tt> isn't.
+     * method, except that it doesn't throw {@code ClassCastExceptions}
+     * if {@code lhs} is an instance of a {@link Timestamp Timestamp} and
+     * {@code rhs} isn't.
      * <p/>
      * For timestamps, the nanoseconds are ignored.
      *
      * @param lhs the date
      * @param rhs the date to compare with
-     * @return <tt>0</tt> if the <tt>lhs</tt> is equal to <tt>rhs</tt>;
-     *         a value less than <tt>0</tt> if <tt>lhs</tt> is before
-     *         <tt>rhs</tt>; and a value greater than
-     *         <tt>0</tt> if <tt>lhs</tt> is after <tt>rhs</tt>.
+     * @return {@code 0} if the {@code lhs} is equal to {@code rhs};
+     *         a value less than {@code 0} if {@code lhs} is before
+     *         {@code rhs}; and a value greater than
+     *         {@code 0} if {@code lhs} is after {@code rhs}.
      */
     public static int compareTo(Date lhs, Date rhs) {
         long lhsTime = lhs.getTime();
@@ -113,19 +125,19 @@ public class DateRules {
      * Helper to compare two dates.
      * <p/>
      * This is functionally equivalent to the {@link Date#compareTo(Date)}
-     * method, except that it doesn't throw <tt>ClassCastExceptions</tt>
-     * if <tt>lhs</tt> is an instance of a {@link Timestamp Timestamp} and
-     * <tt>rhs</tt> isn't.
+     * method, except that it doesn't throw {@code ClassCastExceptions}
+     * if {@code lhs} is an instance of a {@link Timestamp Timestamp} and
+     * {@code rhs} isn't.
      * <p/>
      * For timestamps, the nanoseconds are ignored.
      *
-     * @param lhs the date
-     * @param rhs the date to compare with
-     * @param ignoreMillis if <tt>true</tt>, ignore milliseconds
-     * @return <tt>0</tt> if the <tt>lhs</tt> is equal to <tt>rhs</tt>;
-     *         a value less than <tt>0</tt> if <tt>lhs</tt> is before
-     *         <tt>rhs</tt>; and a value greater than
-     *         <tt>0</tt> if <tt>lhs</tt> is after <tt>rhs</tt>.
+     * @param lhs          the date
+     * @param rhs          the date to compare with
+     * @param ignoreMillis if {@code true}, ignore milliseconds
+     * @return {@code 0} if the {@code lhs} is equal to {@code rhs};
+     *         a value less than {@code 0} if {@code lhs} is before
+     *         {@code rhs}; and a value greater than
+     *         {@code 0} if {@code lhs} is after {@code rhs}.
      */
     public static int compareTo(Date lhs, Date rhs, boolean ignoreMillis) {
         if (ignoreMillis) {
@@ -142,24 +154,24 @@ public class DateRules {
      * @param to1   the end of the first date range
      * @param from2 the start of the second date range
      * @param to2   the end of the second date range
-     * @return <tt>true</tt> if the date ranges intersect
+     * @return {@code true} if the date ranges intersect
      */
     public static boolean intersects(Date from1, Date to1,
                                      Date from2, Date to2) {
         int f1f2 = compareTo(from1, from2);
         int t1t2 = compareTo(to1, to2);
         return (f1f2 < 0 && compareTo(to1, from2) > 0)
-                || (compareTo(from1, to2) < 0 && t1t2 > 0)
-                || (f1f2 >= 0 && t1t2 <= 0);
+               || (compareTo(from1, to2) < 0 && t1t2 > 0)
+               || (f1f2 >= 0 && t1t2 <= 0);
     }
 
     /**
      * Determines if a date falls between two dates.
      *
-     * @param date the date to compare
+     * @param date       the date to compare
      * @param lowerBound the lower bound
      * @param upperBound the upper bound
-     * @return <tt>true</tt> if the date falls between the lower and upper bounds, otherwise <tt>false</tt>
+     * @return {@code true} if the date falls between the lower and upper bounds, otherwise {@code false}
      */
     public static boolean between(Date date, Date lowerBound, Date upperBound) {
         return (compareTo(date, lowerBound) >= 0 && compareTo(date, upperBound) <= 0);
