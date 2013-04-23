419bd6fb1fb117ee440d8a5855f784db3d80496e
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-04-23 10:44:27 +0000

    git-svn-id: svn://localhost/openvpms/esci-adapter/trunk@4985 9e4fa352-aafb-0310-9071-df624684024b

--- src/test/java/org/openvpms/esci/adapter/client/OrderServiceAdapterTestCase.java
+++ src/test/java/org/openvpms/esci/adapter/client/OrderServiceAdapterTestCase.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2010 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.esci.adapter.client;
 
@@ -41,8 +39,7 @@ import javax.annotation.Resource;
 /**
  * Tests the {@link OrderServiceAdapterImpl} class.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 @ContextConfiguration("/OrderWebServiceTest-context.xml")
 public class OrderServiceAdapterTestCase extends AbstractESCITest {
@@ -130,7 +127,7 @@ public class OrderServiceAdapterTestCase extends AbstractESCITest {
      */
     private InVMSupplierServiceLocator createSupplierServiceLocator() {
         InVMSupplierServiceLocator vmLocator = new InVMSupplierServiceLocator();
-        vmLocator.setSupplierRules(new SupplierRules());
+        vmLocator.setSupplierRules(new SupplierRules(getArchetypeService()));
         vmLocator.setBeanFactory(factory);
         vmLocator.setServiceLocatorFactory(serviceLocatorFactory);
         return vmLocator;
