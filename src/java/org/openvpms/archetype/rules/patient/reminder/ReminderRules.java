af9f24dbc8e9856da4c32a4ed091218287dcb4cc
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-05-06 13:31:35 +0000

    Added ReminderRules.updateReminder(Act reminder, int reminderCount, Date lastSent)
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@5021 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/patient/reminder/ReminderRules.java
+++ src/java/org/openvpms/archetype/rules/patient/reminder/ReminderRules.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.patient.reminder;
@@ -318,7 +318,22 @@ public class ReminderRules {
     public void updateReminder(Act reminder, Date lastSent) {
         ActBean bean = new ActBean(reminder, service);
         int count = bean.getInt("reminderCount");
-        bean.setValue("reminderCount", count + 1);
+        updateReminder(reminder, count + 1, lastSent);
+    }
+
+    /**
+     * Updates a reminder that has been successfully sent.
+     * <p/>
+     * This clears the <em>error</em> node.
+     *
+     * @param reminder      the reminder
+     * @param reminderCount the reminder count
+     * @param lastSent      the date when the reminder was sent
+     * @throws ArchetypeServiceException for any archetype service error
+     */
+    public void updateReminder(Act reminder, int reminderCount, Date lastSent) {
+        ActBean bean = new ActBean(reminder, service);
+        bean.setValue("reminderCount", reminderCount);
         bean.setValue("lastSent", lastSent);
         bean.setValue("error", null);
         bean.save();
