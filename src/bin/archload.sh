dd76be00450e1cacf92f3352e763c8483c3ed49a
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-05-22 02:00:27 +0000

    Changed archload.bat and archload.sh to load everything in update/archetypes
    git-svn-id: svn://localhost/openvpms/release/trunk@5132 9e4fa352-aafb-0310-9071-df624684024b

--- src/bin/archload.sh
+++ src/bin/archload.sh
@@ -2,4 +2,4 @@
 
 . ./setenv.sh
 
-java -classpath $CLASSPATH org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes/org/openvpms
+java -classpath $CLASSPATH org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes
