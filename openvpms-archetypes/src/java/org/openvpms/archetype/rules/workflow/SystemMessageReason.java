e3a88b4db2e9843a9e88562645d1de27a0b2b9a7
Tim Anderson <tma@netspace.net.au>
2013-06-26 04:11:48 +0000

    Changes for OVPMS-1321 Add support to schedule Quartz jobs
    svn path=/archetypes/trunk/; revision=5203

--- openvpms-archetypes/src/java/org/openvpms/archetype/rules/workflow/SystemMessageReason.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/rules/workflow/SystemMessageReason.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2010 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.archetype.rules.workflow;
 
@@ -21,10 +19,19 @@ package org.openvpms.archetype.rules.workflow;
 /**
  * System message reasons.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 public class SystemMessageReason {
+
+    /**
+     * Indicates an error has occurred.
+     */
+    public static final String ERROR = "ERROR";
+
+    /**
+     * Indicates successful completion.
+     */
+    public static final String COMPLETED = "COMPLETED";
 
     /**
      * Accepted order reason.
@@ -34,11 +41,11 @@ public class SystemMessageReason {
     /**
      * Rejected order reason.
      */
-    public static final String ORDER_REJECTED = "ORDER_REJECTED";    
+    public static final String ORDER_REJECTED = "ORDER_REJECTED";
 
     /**
      * Order invoiced reason.
      */
-    public static final String ORDER_INVOICED = "ORDER_INVOICED";    
+    public static final String ORDER_INVOICED = "ORDER_INVOICED";
 
 }
