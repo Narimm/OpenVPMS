9693768a8662f30a6dfb6b298114869ac679624c
Tim Anderson <tma@netspace.net.au>
2013-07-10 09:47:18 +0000

    Fixed LookupHelperException Messages resource path
    svn path=/framework/trunk/; revision=5245

--- openvpms-framework/src/java/org/openvpms/component/business/service/archetype/helper/LookupHelperException.java
+++ openvpms-framework/src/java/org/openvpms/component/business/service/archetype/helper/LookupHelperException.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.component.business.service.archetype.helper;
@@ -22,9 +20,9 @@ import org.apache.commons.resources.Messages;
 import org.openvpms.component.system.common.exception.OpenVPMSException;
 
 /**
- * This exception is the base service exception for the 
- * {@link LookupHelper} service. 
- * 
+ * This exception is the base service exception for the
+ * {@link LookupHelper} service.
+ *
  * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
  * @version $LastChangedDate$
  */
@@ -62,16 +60,14 @@ public class LookupHelperException extends OpenVPMSException {
      * The appropriate resource file is loaded cached into memory when this
      * class is loaded.
      */
-    private static Messages messages = Messages
-            .getMessages("org.openvpms.component.business.service.archetype.helper"
-                    + OpenVPMSException.ERRMESSAGES_FILE);
+    private static Messages messages = Messages.getMessages("org.openvpms.component.business.service.archetype.helper."
+                                                            + OpenVPMSException.ERRMESSAGES_FILE);
 
     /**
      * Instantiate an exception given an error code. The error code corresponds
      * to a message that does not require any parameters to redner
-     * 
-     * @param errorCode
-     *            the error code
+     *
+     * @param errorCode the error code
      */
     public LookupHelperException(ErrorCode errorCode) {
         super(messages.getMessage(errorCode.toString()));
@@ -81,12 +77,9 @@ public class LookupHelperException extends OpenVPMSException {
     /**
      * Instantiate an exception given an error code and a set of associated
      * object parameters. The params are required to render the message
-     * 
-     * @param errorCode
-     *            the error code
-     * @param parama
-     *            the parameters used to render the message associated with the
-     *            error code
+     *
+     * @param errorCode the error code
+     * @param params    the parameters used to render the message associated with the error code
      */
     public LookupHelperException(ErrorCode errorCode, Object[] params) {
         super(messages.getMessage(errorCode.toString(), params));
@@ -96,11 +89,9 @@ public class LookupHelperException extends OpenVPMSException {
     /**
      * Create an exception with the following error code and the root exception.
      * The error code is used to render a local specific message.
-     * 
-     * @param errorCode
-     *            the error code
-     * @param cause
-     *            the root exception
+     *
+     * @param errorCode the error code
+     * @param cause     the root exception
      */
     public LookupHelperException(ErrorCode errorCode, Throwable cause) {
         super(messages.getMessage(errorCode.toString()), cause);
@@ -111,16 +102,13 @@ public class LookupHelperException extends OpenVPMSException {
      * Create an exception with the following error code and the root exception.
      * The params is used to render the messsgae that is associated with the
      * error code
-     * 
-     * @param errorCode
-     *            the error code
-     * @param params
-     *            additional information required to render the message
-     * @param cause
-     *            the root exception
+     *
+     * @param errorCode the error code
+     * @param params    additional information required to render the message
+     * @param cause     the root exception
      */
     public LookupHelperException(ErrorCode errorCode, Object[] params,
-            Throwable cause) {
+                                 Throwable cause) {
         super(messages.getMessage(errorCode.toString(), params), cause);
         this.errorCode = errorCode;
     }
