ca75e6b71943a951b67e7cce5b4503013a2c857f
Tim Anderson <tma@netspace.net.au>
2013-08-07 09:55:21 +0000

    Fix to QueryContext to only use the parent TypeSet if an alias is specified. If the alias is null, it should use the TypeSet on the top of the stack
    svn path=/framework/trunk/; revision=5380

--- openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/query/QueryContext.java
+++ openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/query/QueryContext.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005-2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.component.business.dao.hibernate.im.query;
 
@@ -386,7 +386,7 @@ public class QueryContext {
      */
     TypeSet getTypeSet(String alias) {
         TypeSet result = null;
-        if (parent != null) {
+        if (alias != null && parent != null) {
             result = parent.getTypeSet(alias);
         }
         if (result == null) {
