31c8da6c89428cc9cde366728c86ea57618453a1
Tim Anderson <tma@netspace.net.au>
2013-09-23 08:34:03 +0000

    Changes for OVPMS-1336 Check-In: constrain work list selection to the current practice location work lists
    svn path=/archetypes/trunk/; revision=5460

--- openvpms-archetypes/src/test/java/org/openvpms/archetype/rules/workflow/ScheduleTestHelper.java
+++ openvpms-archetypes/src/test/java/org/openvpms/archetype/rules/workflow/ScheduleTestHelper.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.workflow;
@@ -39,8 +37,7 @@ import java.util.Date;
 /**
  * Scheduling test helper.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 public class ScheduleTestHelper extends TestHelper {
 
@@ -56,7 +53,7 @@ public class ScheduleTestHelper extends TestHelper {
         view.setName("XScheduleView");
         EntityBean bean = new EntityBean(view);
         for (Party schedule : schedules) {
-            bean.addRelationship("entityRelationship.locationView", schedule);
+            bean.addNodeRelationship("schedules", schedule);
         }
         bean.save();
         return view;
@@ -231,8 +228,7 @@ public class ScheduleTestHelper extends TestHelper {
         view.setName("XWorkListView");
         EntityBean bean = new EntityBean(view);
         for (Party workList : workLists) {
-            bean.addRelationship("entityRelationship.locationWorkListView",
-                                 workList);
+            bean.addNodeRelationship("workLists", workList);
         }
         bean.save();
         return view;
