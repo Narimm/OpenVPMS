d836edadc0509e96c5d6065f4f77f17271a4cdf5
Tim Anderson <tma@netspace.net.au>
2013-10-29 07:29:01 +0000

    Changes for  OBF-226 Create tool to compare archetypes
    svn path=/framework/trunk/; revision=5552

--- openvpms-framework/src/java/org/openvpms/tools/archetype/diff/ArchDiff.java
+++ openvpms-framework/src/java/org/openvpms/tools/archetype/diff/ArchDiff.java
@@ -1,3 +1,19 @@
+/*
+ * Version: 1.0
+ *
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
+ *
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ */
+
 package org.openvpms.tools.archetype.diff;
 
 import com.martiansoftware.jsap.FlaggedOption;
@@ -277,7 +293,7 @@ public class ArchDiff {
                                          .setHelp("The first version to compare."));
         parser.registerParameter(new UnflaggedOption("version2")
                                          .setRequired(false)
-                                         .setDefault("database")
+                                         .setDefault("db")
                                          .setHelp("The second version to compare."));
         return parser;
     }
