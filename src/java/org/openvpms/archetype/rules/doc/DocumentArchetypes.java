48990a53c8030ecb4e856321fc2cd5cc3d06afdc
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-11-09 06:56:06 +0000

    Added DocumentArchetypes.DOCUMENT_PARTICIPATION
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@5564 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/doc/DocumentArchetypes.java
+++ src/java/org/openvpms/archetype/rules/doc/DocumentArchetypes.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2010 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 package org.openvpms.archetype.rules.doc;
 
@@ -21,8 +19,7 @@ package org.openvpms.archetype.rules.doc;
 /**
  * Document archetypes.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 public class DocumentArchetypes {
 
@@ -45,6 +42,11 @@ public class DocumentArchetypes {
      * Document template participation.
      */
     public static final String DOCUMENT_TEMPLATE_PARTICIPATION = "participation.documentTemplate";
+
+    /**
+     * Document participation.
+     */
+    public static final String DOCUMENT_PARTICIPATION = "participation.document";
 
     /**
      * The default document archetype.
