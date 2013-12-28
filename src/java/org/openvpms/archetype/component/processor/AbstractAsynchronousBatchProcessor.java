8bf1bb41aa00b2628bc112772020a74ebc0ff54e
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2013-12-28 10:33:50 +0000

    Change to AbstractAsynchronousBatchProcessor.process() to delegate Throwable to notifyError()
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@5691 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/component/processor/AbstractAsynchronousBatchProcessor.java
+++ src/java/org/openvpms/archetype/component/processor/AbstractAsynchronousBatchProcessor.java
@@ -1,24 +1,20 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2007 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.component.processor;
-
-import org.openvpms.component.system.common.exception.OpenVPMSException;
 
 import java.util.Iterator;
 
@@ -80,7 +76,7 @@ public abstract class AbstractAsynchronousBatchProcessor<Type>
                 // processing completed.
                 processingCompleted();
             }
-        } catch (OpenVPMSException exception) {
+        } catch (Throwable exception) {
             notifyError(exception);
         }
     }
