d089c843b21dfc9d4833aeeff791f72473ac2116
Tim Anderson <tma@netspace.net.au>
2014-01-18 01:58:44 +0000

    Changes for OVPMS-1424 Update migrate-1.6-to-1.7.sql script to include default values for new party.organisationSchedule and party.organisationWorkList nodes
    svn path=/release/trunk/; revision=5719

--- openvpms-release/src/update/db/migrate-1.6-to-1.7.sql
+++ openvpms-release/src/update/db/migrate-1.6-to-1.7.sql
@@ -9,3 +9,39 @@ WHERE substring(d.value, 1, 10) NOT IN ("1970-01-01", "1970-01-02")
       AND e.arch_short_name = "party.organisationSchedule"
       AND e.entity_id = d.entity_id
       AND d.name IN ("startTime", "endTime");
+
+#
+# Migrate party.organisationSchedule, party.organisationWorkList, for OVPMS-1424
+#
+
+insert into entity_details (entity_id, name, type, value)
+select e.entity_id, "useAllWorkLists", "boolean", "true"
+from entities e
+where e.arch_short_name = "party.organisationSchedule"
+      and not exists (select * from entity_details d where d.entity_id = e.entity_id and d.name ="useAllWorkLists");
+
+insert into entity_details (entity_id, name, type, value)
+select e.entity_id, "inputWeight", "boolean", "true"
+from entities e
+where e.arch_short_name = "party.organisationSchedule"
+      and not exists (select * from entity_details d where d.entity_id = e.entity_id and d.name ="inputWeight");
+
+insert into entity_details (entity_id, name, type, value)
+select e.entity_id, "useAllTemplates", "boolean", "true"
+from entities e
+where e.arch_short_name = "party.organisationSchedule"
+      and not exists (select * from entity_details d where d.entity_id = e.entity_id and d.name ="useAllTemplates");
+
+insert into entity_details (entity_id, name, type, value)
+select e.entity_id, "inputWeight", "boolean", "true"
+from entities e
+where e.arch_short_name = "party.organisationWorkList"
+      and not exists (select * from entity_details d where d.entity_id = e.entity_id and d.name ="inputWeight");
+
+insert into entity_details (entity_id, name, type, value)
+select e.entity_id, "useAllTemplates", "boolean", "true"
+from entities e
+where e.arch_short_name = "party.organisationWorkList"
+      and not exists (select * from entity_details d where d.entity_id = e.entity_id and d.name ="useAllTemplates");
+
+
