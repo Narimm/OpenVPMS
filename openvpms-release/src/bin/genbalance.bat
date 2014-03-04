a4f2d6983321632d49cbee1c3597ed1b6b304ef0
Tim Anderson <tma@netspace.net.au>
2014-03-04 22:54:51 +0000

    svn path=/framework/branches/1.8-dev/; revision=5773

--- openvpms-release/src/bin/genbalance.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call setenv.bat
-
-java -Xmx512m -Dlog4j.configuration=file:../conf/log4j.properties org.openvpms.archetype.tools.account.AccountBalanceTool --context ../conf/applicationContext.xml %*%
