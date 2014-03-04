5be62ff6d70338dd9b80f2364926f8327b4a7d3a
Tim Anderson <tma@netspace.net.au>
2014-03-04 22:54:09 +0000

    svn path=/etl/branches/1.8-dev/; revision=5772

--- openvpms-release/src/bin/archload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ./setenv.sh
-
-java -Dlog4j.configuration=file:../conf/log4j.properties -classpath $CLASSPATH org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes
