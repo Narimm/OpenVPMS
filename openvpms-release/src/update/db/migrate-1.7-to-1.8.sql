0e40c825f91c4a22af464ee4155a0ea84458cdba
Tim Anderson <tma@netspace.net.au>
2014-04-13 11:23:39 +0000

    changed entity_links to be created prior to entity_links_details to reflect foreign key constraint
    svn path=/release/branches/1.8-dev/; revision=5855

--- openvpms-release/src/update/db/migrate-1.7-to-1.8.sql
+++ openvpms-release/src/update/db/migrate-1.7-to-1.8.sql
@@ -1,13 +1,3 @@
-CREATE TABLE if not exists `entity_link_details` (
-  `id` bigint(20) NOT NULL,
-  `type` varchar(255) NOT NULL,
-  `value` varchar(5000) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`,`name`),
-  KEY `FKD2E4DDD9A03946DF` (`id`),
-  CONSTRAINT `FKD2E4DDD9A03946DF` FOREIGN KEY (`id`) REFERENCES `entity_links` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
 CREATE TABLE if not exists `entity_links` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `version` bigint(20) NOT NULL,
@@ -27,3 +17,14 @@ CREATE TABLE if not exists `entity_links` (
   CONSTRAINT `FK3FCDE05D3EFA2333` FOREIGN KEY (`target_id`) REFERENCES `entities` (`entity_id`),
   CONSTRAINT `FK3FCDE05DA5B2869` FOREIGN KEY (`source_id`) REFERENCES `entities` (`entity_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE if not exists `entity_link_details` (
+  `id` bigint(20) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `value` varchar(5000) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`,`name`),
+  KEY `FKD2E4DDD9A03946DF` (`id`),
+  CONSTRAINT `FKD2E4DDD9A03946DF` FOREIGN KEY (`id`) REFERENCES `entity_links` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
