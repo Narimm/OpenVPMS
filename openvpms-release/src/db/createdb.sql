8f826150579eb5ae1b01b9130af043e7278a13aa
Tim Anderson <tma@netspace.net.au>
2014-04-25 04:00:43 +0000

    merged back changes from trunk
    svn path=/release/branches/1.8-dev/; revision=5878

--- openvpms-release/src/db/createdb.sql
+++ openvpms-release/src/db/createdb.sql
@@ -8,7 +8,11 @@ CREATE DATABASE `openvpms` /*!40100 DEFAULT CHARACTER SET utf8 */;
 GRANT ALL PRIVILEGES ON openvpms.* TO 'openvpms'@'localhost'
     IDENTIFIED BY 'openvpms' WITH GRANT OPTION;
 
-GRANT ALL PRIVILEGES ON openvpms.* TO 'openvpms'@'%'
-    IDENTIFIED BY 'openvpms' WITH GRANT OPTION;
+#
+# Uncomment the following if MySQL is on a different host to Tomcat.
+# The '%' can be changed to the Tomcat host to improve security.
+#
+# GRANT ALL PRIVILEGES ON openvpms.* TO 'openvpms'@'%'
+#     IDENTIFIED BY 'openvpms' WITH GRANT OPTION;
 
 COMMIT;
