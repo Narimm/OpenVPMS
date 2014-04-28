e8bd5a4b151a5fae6a4ee6860b01c82d44065052
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2014-04-28 05:01:56 +0000

    Fix for Product copy should set stock quantities to zero
    git-svn-id: svn://localhost/openvpms/archetypes/trunk@5893 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/product/ProductCopyHandler.java
+++ src/java/org/openvpms/archetype/rules/product/ProductCopyHandler.java
@@ -1,36 +1,36 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.product;
 
 import org.openvpms.archetype.rules.util.MappingCopyHandler;
+import org.openvpms.component.business.domain.im.archetype.descriptor.ArchetypeDescriptor;
+import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
 import org.openvpms.component.business.domain.im.common.Entity;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.common.IMObjectReference;
 import org.openvpms.component.business.domain.im.product.Product;
 import org.openvpms.component.business.service.archetype.helper.IMObjectCopyHandler;
+import org.openvpms.component.business.service.archetype.helper.TypeHelper;
 
 
 /**
- * {@link IMObjectCopyHandler} that creates copies of products.
+ * An {@link IMObjectCopyHandler} that creates copies of products.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-12-12 04:10:40Z $
+ * @author Tim Anderson
  */
 public class ProductCopyHandler extends MappingCopyHandler {
 
@@ -40,7 +40,7 @@ public class ProductCopyHandler extends MappingCopyHandler {
     private final IMObjectReference product;
 
     /**
-     * Creates a <tt>ProductCopyHandler</tt>.
+     * Creates a {@link ProductCopyHandler}.
      *
      * @param product the product being copied
      */
@@ -53,8 +53,8 @@ public class ProductCopyHandler extends MappingCopyHandler {
     /**
      * Determines how an object should be handled.
      * <p/>
-     * This implementation changes the order of evaluation so that {@link #copy}
-     * is evaluated prior to {@link #reference}.
+     * This implementation changes the order of evaluation so that {@link #copy} is evaluated prior to
+     * {@link #reference}.
      *
      * @param object the object
      * @return the type of behaviour to apply to the object
@@ -74,13 +74,34 @@ public class ProductCopyHandler extends MappingCopyHandler {
     /**
      * Determines if an object should be copied.
      * <p/>
-     * This implementation returns <tt>true</tt> if the object is the specified product.
+     * This implementation returns {@code true} if the object is the specified product.
      *
      * @param object the object to check
-     * @return <tt>true</tt> if it should be copied
+     * @return {@code true} if it should be copied
      */
     @Override
     protected boolean copy(IMObject object) {
         return object.getObjectReference().equals(product);
     }
+
+    /**
+     * Determines how a node should be copied.
+     * <p/>
+     * This implementation excludes the quantity node from <em>entityRelationship.productStockLocation</em> when
+     * copying products.
+     *
+     * @param source     the source archetype
+     * @param sourceNode the source node
+     * @param target     the target archetype
+     * @return a node to copy sourceNode to, or {@code null} if the node shouldn't be copied
+     */
+    @Override
+    public NodeDescriptor getNode(ArchetypeDescriptor source, NodeDescriptor sourceNode, ArchetypeDescriptor target) {
+        boolean isStockLocation = TypeHelper.isA(source, "entityRelationship.productStockLocation");
+        if (isStockLocation && "quantity".equals(sourceNode.getName())) {
+            return null;
+        }
+        return super.getNode(source, sourceNode, target);
+    }
+
 }
