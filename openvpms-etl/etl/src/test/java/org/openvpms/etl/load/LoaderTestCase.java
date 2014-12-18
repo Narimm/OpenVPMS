1629efedd490b41552cd950e9a2bc71a37b42d3c
Tim Anderson <tma@netspace.net.au>
2014-12-18 05:47:10 +0000

    Integrated changes for OVPMS-1172 "Remove contact.faxNumber and replace with contact.phoneNumber, update existing fax numbers to the new system" contributed by Ben Charlton
    svn path=/etl/branches/1.8-dev/; revision=6349

--- openvpms-etl/etl/src/test/java/org/openvpms/etl/load/LoaderTestCase.java
+++ openvpms-etl/etl/src/test/java/org/openvpms/etl/load/LoaderTestCase.java
@@ -1,17 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2007 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.etl.load;
@@ -234,14 +234,14 @@ public class LoaderTestCase extends AbstractJUnit4SpringContextTests {
         mappings.setIdColumn("CUSTID");
 
         Mapping mapping = createMapping(
-                "FAXNUMBER",
-                "<party.customerperson>contacts[0]<contact.faxNumber>faxNumber");
+                "TELEPHONE",
+                "<party.customerperson>contacts[0]<contact.phoneNumber>telephoneNumber");
         mapping.setExcludeNull(true);
         mappings.addMapping(mapping);
 
         Loader loader = createLoader("CUSTLOAD", mappings);
         ETLRow row = new ETLRow("CUSTID");
-        row.add("FAXNUMBER", null);
+        row.add("TELEPHONE", null);
         List<IMObject> objects = loader.load(row);
         loader.close();
 
