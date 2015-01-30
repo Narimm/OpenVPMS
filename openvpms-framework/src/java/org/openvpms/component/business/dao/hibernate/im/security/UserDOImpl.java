eaaab2ddf43faf798696d7696c3c77858c521ed2
Tim Anderson <tma@netspace.net.au>
2015-01-30 05:56:36 +0000

    Changes for OVPMS-1555 Add support for multiple To, CC, and BCC addresses when emailing
    svn path=/framework/branches/1.8-dev/; revision=6378

--- openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/security/UserDOImpl.java
+++ openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/security/UserDOImpl.java
@@ -1,24 +1,22 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2008 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.component.business.dao.hibernate.im.security;
 
-import org.openvpms.component.business.dao.hibernate.im.entity.EntityDOImpl;
+import org.openvpms.component.business.dao.hibernate.im.party.PartyDOImpl;
 import org.openvpms.component.business.domain.archetype.ArchetypeId;
 
 import java.util.HashSet;
@@ -28,10 +26,9 @@ import java.util.Set;
 /**
  * Implementation of the {@link UserDO} interface.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate$
+ * @author Tim Anderson
  */
-public class UserDOImpl extends EntityDOImpl implements UserDO {
+public class UserDOImpl extends PartyDOImpl implements UserDO {
 
     /**
      * The login name.
--- openvpms-framework/src/java/org/openvpms/component/business/domain/im/security/User.java
+++ openvpms-framework/src/java/org/openvpms/component/business/domain/im/security/User.java
@@ -1,25 +1,23 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2005 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 
 package org.openvpms.component.business.domain.im.security;
 
-import org.openvpms.component.business.domain.im.common.Entity;
+import org.openvpms.component.business.domain.im.party.Party;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -31,10 +29,10 @@ import java.util.Set;
  * This class represents the user details and the list of associated
  * authorities.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate$
+ * @author Jim Alateras
+ * @author Tim Anderson
  */
-public class User extends Entity implements UserDetails {
+public class User extends Party implements UserDetails {
 
     /**
      * Default UID
