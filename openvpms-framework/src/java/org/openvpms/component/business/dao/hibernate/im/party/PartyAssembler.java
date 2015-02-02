faf6b922afcabd74ee30198579fbaddcc4aa0425
Tim Anderson <tma@netspace.net.au>
2015-02-02 23:01:40 +0000

    Changes for OVPMS-1555 Add support for multiple To, CC, and BCC addresses when emailing
    svn path=/framework/branches/1.8-dev/; revision=6381

--- openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/party/PartyAssembler.java
+++ openvpms-framework/src/java/org/openvpms/component/business/dao/hibernate/im/party/PartyAssembler.java
@@ -1,82 +1,36 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2008 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.component.business.dao.hibernate.im.party;
 
-import org.openvpms.component.business.dao.hibernate.im.common.Context;
-import org.openvpms.component.business.dao.hibernate.im.common.DOState;
-import org.openvpms.component.business.dao.hibernate.im.common.SetAssembler;
-import org.openvpms.component.business.dao.hibernate.im.entity.EntityAssembler;
-import org.openvpms.component.business.domain.im.party.Contact;
 import org.openvpms.component.business.domain.im.party.Party;
 
 
 /**
- * Assembles {@link Party} instances from {@link PartyDO} instances and
- * vice-versa.
+ * Assembles {@link Party} instances from {@link PartyDO} instances and vice-versa.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
-public class PartyAssembler extends EntityAssembler<Party, PartyDO> {
+public class PartyAssembler extends AbstractPartyAssembler<Party, PartyDO> {
 
     /**
-     * Assembles sets of contacts.
-     */
-    private SetAssembler<Contact, ContactDO> CONTACTS
-            = SetAssembler.create(Contact.class, ContactDO.class);
-
-    /**
-     * Creates a new <tt>PartyAssembler</tt>.
+     * Constructs a {@link PartyAssembler}.
      */
     public PartyAssembler() {
         super(Party.class, PartyDO.class, PartyDOImpl.class);
-    }
-
-    /**
-     * Assembles a data object from an object.
-     *
-     * @param target  the object to assemble
-     * @param source  the object to assemble from
-     * @param state   the data object state
-     * @param context the assembly context
-     */
-    @Override
-    protected void assembleDO(PartyDO target, Party source, DOState state,
-                              Context context) {
-        super.assembleDO(target, source, state, context);
-        CONTACTS.assembleDO(target.getContacts(), source.getContacts(),
-                            state, context);
-    }
-
-    /**
-     * Assembles an object from a data object.
-     *
-     * @param target  the object to assemble
-     * @param source  the object to assemble from
-     * @param context the assembly context
-     */
-    @Override
-    protected void assembleObject(Party target, PartyDO source,
-                                  Context context) {
-        super.assembleObject(target, source, context);
-        CONTACTS.assembleObject(target.getContacts(), source.getContacts(),
-                                context);
     }
 
     /**
