fb26bb0e5254633d012b95ef1fb5430ae56bbf5e
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2015-02-05 12:59:20 +0000

    Changes for OVPMS-1557 Integrate docload
    git-svn-id: svn://localhost/openvpms/etl/branches/1.8-dev@6386 9e4fa352-aafb-0310-9071-df624684024b

--- etl/src/java/org/openvpms/etl/tools/doc/DefaultLoaderListener.java
+++ etl/src/java/org/openvpms/etl/tools/doc/DefaultLoaderListener.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2008 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.etl.tools.doc;
@@ -24,19 +22,12 @@ import java.io.File;
 /**
  * Default implementation of the {@link LoaderListener} interface.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
-class DefaultLoaderListener extends AbstractLoaderListener {
+public class DefaultLoaderListener extends AbstractLoaderListener {
 
     /**
-     * Creates a new <tt>DefaultLoaderListener</tt>.
-     */
-    public DefaultLoaderListener() {
-    }
-
-    /**
-     * Creates a new <tt>DefaultLoaderListener</tt>.
+     * Constructs a {@link DefaultLoaderListener}.
      *
      * @param dir if non-null, files will be moved here on successful load
      */
