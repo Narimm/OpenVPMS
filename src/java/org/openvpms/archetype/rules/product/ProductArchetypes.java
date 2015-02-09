23075d6031157669371c3b45f98be5d15a9ad551
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2015-02-09 01:59:18 +0000

    Changes for OVPMS-1560 Weight-based product templates - add support for weight ranges
    git-svn-id: svn://localhost/openvpms/archetypes/branches/1.8-dev@6389 9e4fa352-aafb-0310-9071-df624684024b

--- src/java/org/openvpms/archetype/rules/product/ProductArchetypes.java
+++ src/java/org/openvpms/archetype/rules/product/ProductArchetypes.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.product;
@@ -90,4 +90,9 @@ public class ProductArchetypes {
      * Product batch short name.
      */
     public static final String PRODUCT_BATCH = "entity.productBatch";
+
+    /**
+     * Template product include relationship
+     */
+    public static final String PRODUCT_INCLUDES = "entityLink.produceIncludes";
 }
