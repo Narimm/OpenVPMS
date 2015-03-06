40cc263fc59dc3777bf6ebd37ad1cce696142c01
Tim Anderson <tma@netspace.net.au>
2015-03-06 10:52:53 +0000

    Moved ReadOnlyArchetypeService from openvpms-reports to openvpms-framework
    svn path=/reports/branches/1.8-dev/; revision=6425

--- openvpms-reports/src/java/org/openvpms/report/CachingReportFactory.java
+++ openvpms-reports/src/java/org/openvpms/report/CachingReportFactory.java
@@ -18,19 +18,17 @@ package org.openvpms.report;
 
 import org.openvpms.archetype.function.factory.ArchetypeFunctionsFactory;
 import org.openvpms.archetype.rules.doc.DocumentHandlers;
-import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.common.IMObjectReference;
-import org.openvpms.component.business.service.archetype.ArchetypeServiceException;
+import org.openvpms.component.business.service.archetype.CachingReadOnlyArchetypeService;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.lookup.ILookupService;
-import org.openvpms.component.system.common.cache.IMObjectCache;
 import org.openvpms.component.system.common.cache.LRUIMObjectCache;
 
 /**
  * An {@link ReportFactory} that caches objects to improve performance.
  * <p/>
  * This implementation supports caching of objects returned by the {@link IArchetypeService#get(IMObjectReference)}
- * method. An {@link LRUIMObjectCache} is constructed for each report.
+ * method. A {@link LRUIMObjectCache} is constructed for each report.
  *
  * @author Tim Anderson
  */
@@ -64,28 +62,7 @@ public class CachingReportFactory extends ReportFactory {
      */
     @Override
     protected IArchetypeService proxy(IArchetypeService service) {
-        return new CachingArchetypeService(maxSize, service);
+        return new CachingReadOnlyArchetypeService(maxSize, service);
     }
 
-    private static class CachingArchetypeService extends ReadOnlyArchetypeService {
-
-        private final IMObjectCache cache;
-
-        public CachingArchetypeService(int maxSize, IArchetypeService service) {
-            super(service);
-            cache = new LRUIMObjectCache(maxSize, service);
-        }
-
-        /**
-         * Retrieves an object given its reference.
-         *
-         * @param reference the object reference
-         * @return the corresponding object, or {@code null} if none is found
-         * @throws ArchetypeServiceException if the query fails
-         */
-        @Override
-        public IMObject get(IMObjectReference reference) {
-            return cache.get(reference);
-        }
-    }
 }
--- openvpms-reports/src/java/org/openvpms/report/ReportFactory.java
+++ openvpms-reports/src/java/org/openvpms/report/ReportFactory.java
@@ -20,15 +20,11 @@ import org.apache.commons.jxpath.Functions;
 import org.openvpms.archetype.function.factory.ArchetypeFunctionsFactory;
 import org.openvpms.archetype.rules.doc.DocumentHandlers;
 import org.openvpms.archetype.rules.doc.TemplateHelper;
-import org.openvpms.component.business.domain.archetype.ArchetypeId;
-import org.openvpms.component.business.domain.im.archetype.descriptor.ArchetypeDescriptor;
-import org.openvpms.component.business.domain.im.archetype.descriptor.NodeDescriptor;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.document.Document;
 import org.openvpms.component.business.service.archetype.ArchetypeServiceException;
-import org.openvpms.component.business.service.archetype.DelegatingArchetypeService;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
-import org.openvpms.component.business.service.archetype.IArchetypeServiceListener;
+import org.openvpms.component.business.service.archetype.ReadOnlyArchetypeService;
 import org.openvpms.component.business.service.archetype.helper.DescriptorHelper;
 import org.openvpms.component.business.service.lookup.ILookupService;
 import org.openvpms.component.system.common.query.ObjectSet;
@@ -36,10 +32,6 @@ import org.openvpms.report.jasper.TemplatedJasperIMObjectReport;
 import org.openvpms.report.jasper.TemplatedJasperObjectSetReport;
 import org.openvpms.report.msword.MsWordIMReport;
 import org.openvpms.report.openoffice.OpenOfficeIMReport;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
 import static org.openvpms.report.ReportException.ErrorCode.NoTemplateForArchetype;
 import static org.openvpms.report.ReportException.ErrorCode.UnsupportedTemplate;
@@ -175,178 +167,13 @@ public class ReportFactory {
     }
 
     /**
-     * Proxies the archetype service.
+     * Creates a proxy for the archetype service.
      *
      * @param service service
      * @return the proxied service
      */
     protected IArchetypeService proxy(IArchetypeService service) {
         return service;
-    }
-
-    protected static class ReadOnlyArchetypeService extends DelegatingArchetypeService {
-
-        public ReadOnlyArchetypeService(IArchetypeService service) {
-            super(service);
-        }
-
-        /**
-         * Create a domain object given a short name. The short name is a reference to an {@link ArchetypeDescriptor}.
-         *
-         * @param shortName the short name
-         * @return a new object, or {@code null} if there is no corresponding archetype descriptor for {@code shortName}
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public IMObject create(String shortName) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Create a domain object given an {@link ArchetypeId}.
-         *
-         * @param id the archetype id
-         * @return a new object, or {@code null} if there is no corresponding archetype descriptor for {@code shortName}
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public IMObject create(ArchetypeId id) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Validate the specified {@link IMObject}. To validate the object it will retrieve the archetype and iterate
-         * through the assertions.
-         *
-         * @param object the object to validate
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void validateObject(IMObject object) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Derived values for the specified {@link IMObject}, based on its corresponding {@link ArchetypeDescriptor}.
-         *
-         * @param object the object to derived values for
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void deriveValues(IMObject object) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Derive the value for the {@link NodeDescriptor} with the specified name.
-         *
-         * @param object the object to operate on.
-         * @param node   the name of the {@link NodeDescriptor}, which will be used to derive the value
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void deriveValue(IMObject object, String node) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Remove the specified object.
-         *
-         * @param object the object to remove
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void remove(IMObject object) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Saves an object, executing any <em>save</em> rules associated with its archetype.
-         *
-         * @param object the object to save
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void save(IMObject object) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Saves an object, executing any <em>save</em> rules associated with its archetype.
-         *
-         * @param object   the object to save
-         * @param validate if {@code true} validate the object prior to saving it
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void save(IMObject object, boolean validate) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Save a collection of {@link IMObject} instances. executing any  <em>save</em> rules associated with their
-         * archetypes.
-         * <p/>
-         * Rules will be executed in the order that the objects are supplied.
-         *
-         * @param objects the objects to save
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void save(Collection<? extends IMObject> objects) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Save a collection of {@link IMObject} instances.
-         *
-         * @param objects the objects to save
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void save(Collection<? extends IMObject> objects, boolean validate) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Execute the rule specified by the uri and using the passed in properties and facts.
-         *
-         * @param ruleUri the rule uri
-         * @param props   a set of properties that can be used by the rule engine
-         * @param facts   a list of facts that are asserted in to the working memory
-         * @return a list objects. May be an empty list.
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public List<Object> executeRule(String ruleUri, Map<String, Object> props, List<Object> facts) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Adds a listener to receive notification of changes.
-         * <p/>
-         * In a transaction, notifications occur on successful commit.
-         *
-         * @param shortName the archetype short to receive events for. May contain wildcards.
-         * @param listener  the listener to add
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void addListener(String shortName, IArchetypeServiceListener listener) {
-            throw new UnsupportedOperationException();
-        }
-
-        /**
-         * Removes a listener.
-         *
-         * @param shortName the archetype short to remove the listener for. May contain wildcards.
-         * @param listener  the listener to remove
-         * @throws UnsupportedOperationException
-         */
-        @Override
-        public void removeListener(String shortName, IArchetypeServiceListener listener) {
-            throw new UnsupportedOperationException();
-        }
     }
 
 }
