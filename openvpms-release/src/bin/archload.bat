60615020a69d5250d61c252ad2604cdd86f5b2c1
Tim Anderson <tma@netspace.net.au>
2015-03-16 11:44:57 +0000

    svn path=/vpms/branches/OVPMS-1587/; revision=6455

--- openvpms-release/src/bin/archload.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-call setenv.bat
-
-java -Dlog4j.configuration=file:../conf/log4j.properties org.openvpms.tools.archetype.loader.ArchetypeLoader -v -s -c --context ../conf/applicationContext.xml -o -m ../update/archetypes/org/openvpms/archetype/assertionTypes.xml -d ../update/archetypes
