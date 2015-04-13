20e620dde256a8ca5a0319a5cf3a18275b0da886
Tim Anderson <tma@netspace.net.au>
2015-04-13 09:22:44 +0000

    svn path=/projects/openvpms/trunk/; revision=6529

--- openvpms-release/src/bin/templateload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-. ./setenv.sh
-
-java -classpath $CLASSPATH -Dlog4j.configuration=file:../conf/log4j.properties org.openvpms.report.tools.TemplateLoader -c ../conf/applicationContext.xml -f $*
