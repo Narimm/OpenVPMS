51ba4b81e6abf5a2779a2ae68d71b5dde85996c6
Tim Anderson <tma@netspace.net.au>
2015-04-16 13:15:02 +0000

    Change current day table header highlighting. Now display a white cell instead of using italicised text for OVPMS-1585 Multi-day schedules
    svn path=/projects/openvpms/trunk/; revision=6543

--- openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/appointment/MultiDayTableHeaderRenderer.java
+++ openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/workflow/appointment/MultiDayTableHeaderRenderer.java
@@ -1,3 +1,19 @@
+/*
+ * Version: 1.0
+ *
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
+ *
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
+ */
+
 package org.openvpms.web.workspace.workflow.appointment;
 
 import nextapp.echo2.app.Component;
@@ -37,15 +53,12 @@ class MultiDayTableHeaderRenderer implements TableCellRenderer {
         if (value instanceof Date) {
             Label label = LabelFactory.create();
             Date date = (Date) value;
-            String text;
             if (DateRules.compareDateToToday(date) == 0) {
                 label.setStyleName("MultiDayTable.Today");
-                text = Messages.format("workflow.scheduling.appointment.column.today", date);
             } else {
                 label.setStyleName("Table.Header");
-                text = Messages.format("workflow.scheduling.appointment.column.day", date);
             }
-            label.setText(text);
+            label.setText(Messages.format("workflow.scheduling.appointment.column.day", date));
             result = label;
         } else {
             result = new Label();
