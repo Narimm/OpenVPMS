ef29b50042443d9d4d698b125b02ba260c07e2f0
Tim Anderson <tma@netspace.net.au>
2015-04-28 03:49:03 +0000

    Changes for OVPMS-1609 Sort customer's patients by date
    svn path=/projects/openvpms/trunk/; revision=6562

--- openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/relationship/RelationshipCollectionViewer.java
+++ openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/relationship/RelationshipCollectionViewer.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.relationship;
@@ -116,7 +116,7 @@ public class RelationshipCollectionViewer
      */
     protected ResultSet<RelationshipState> createResultSet() {
         List<RelationshipState> result = getRelationshipStates();
-        return new RelationshipStateResultSet(result, parentIsSource, ROWS);
+        return new RelationshipStateResultSet(result, ROWS);
     }
 
     /**
