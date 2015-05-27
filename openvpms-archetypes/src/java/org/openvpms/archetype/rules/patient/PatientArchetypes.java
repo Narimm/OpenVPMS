7d905045d81f9cf1b908dc66d1eaefd62513110e
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2015-05-27 03:28:17 +0000

    Integrated changes for OVPMS-1534 "Correct rules in Patient Rules to cover the Patient Location relationship" contributed by Ben Charlton
    git-svn-id: svn://localhost/openvpms/projects/openvpms/trunk@6610 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-archetypes/src/java/org/openvpms/archetype/rules/patient/PatientArchetypes.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/rules/patient/PatientArchetypes.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.rules.patient;
@@ -54,6 +54,11 @@ public class PatientArchetypes {
      */
     public static final String PATIENT_OWNER
             = "entityRelationship.patientOwner";
+
+    /**
+     * Patient location relationship short name.
+     */
+    public static final String PATIENT_LOCATION = "entityRelationship.patientLocation";
 
     /**
      * Patient clinical event act short name.
