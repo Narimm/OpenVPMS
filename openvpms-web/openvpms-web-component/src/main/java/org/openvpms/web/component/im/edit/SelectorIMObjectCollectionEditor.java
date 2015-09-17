34955f5e46ee37b2436830ee3c543074c1b86564
Tim Anderson <tma@netspace.net.au>
2015-09-17 06:09:07 +0000

    Fix for property updates in SelectorIMObjectCollectionEditor
    svn path=/projects/openvpms/trunk/; revision=6745

--- openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/SelectorIMObjectCollectionEditor.java
+++ openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/SelectorIMObjectCollectionEditor.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.edit;
@@ -114,12 +114,13 @@ public class SelectorIMObjectCollectionEditor extends AbstractIMObjectCollection
                 for (IMObject object : objects) {
                     result |= editor.remove(object);
                 }
-            } else if ((objects.size() == 1 && !ObjectUtils.equals(objects.get(0), value) || objects.size() > 1)) {
+            } else if ((objects.size() == 1 && !ObjectUtils.equals(objects.get(0), value)) || objects.size() > 1) {
                 for (IMObject object : objects) {
                     editor.remove(object);
                 }
-                editor.add(value);
-                result = true;
+                result = editor.add(value);
+            } else if (objects.isEmpty()) {
+                result = editor.add(value);
             }
             return result;
         }
