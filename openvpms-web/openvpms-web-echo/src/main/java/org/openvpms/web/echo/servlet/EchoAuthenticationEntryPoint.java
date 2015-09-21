0525b0cf73393cd0ec6a3f1e786ca527b585c6bf
Tim Anderson <tma@netspace.net.au>
2015-09-21 03:03:27 +0000

    removed use of deprecated spring methods
    svn path=/projects/openvpms/trunk/; revision=6749

--- openvpms-web/openvpms-web-echo/src/main/java/org/openvpms/web/echo/servlet/EchoAuthenticationEntryPoint.java
+++ openvpms-web/openvpms-web-echo/src/main/java/org/openvpms/web/echo/servlet/EchoAuthenticationEntryPoint.java
@@ -1,19 +1,17 @@
 /*
- *  Version: 1.0
+ * Version: 1.0
  *
- *  The contents of this file are subject to the OpenVPMS License Version
- *  1.0 (the 'License'); you may not use this file except in compliance with
- *  the License. You may obtain a copy of the License at
- *  http://www.openvpms.org/license/
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
  *
- *  Software distributed under the License is distributed on an 'AS IS' basis,
- *  WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- *  for the specific language governing rights and limitations under the
- *  License.
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
  *
- *  Copyright 2006 (C) OpenVPMS Ltd. All Rights Reserved.
- *
- *  $Id$
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.echo.servlet;
@@ -28,32 +26,39 @@ import java.io.IOException;
 
 
 /**
- * An <tt>AuthenticationEntryPoint</tt> for use with echo2.
+ * An {@code AuthenticationEntryPoint} for use with echo2.
  * <p/>
  * This redirects the caller to the login form. However if the caller is
  * an echo2 client, the redirection is initiated by sending an
- * <tt>HttpServletResponse.SC_BAD_REQUEST</tt> status. The echo2 client
- * interprets this as a redirection if
- * <tt>ClientConfiguration.PROPERTY_SESSION_EXPIRATION_URI</tt>
- * has been configured.
+ * {@code HttpServletResponse.SC_BAD_REQUEST} status. The echo2 client interprets this as a redirection if
+ * {@code ClientConfiguration.PROPERTY_SESSION_EXPIRATION_URI} has been configured.
  * <br/>
  * This is required as the echo2 client has no way of detecting that
  * a redirection has occurred as XmlHttpRequest automatically follows
  * redirections.
  *
- * @author <a href="mailto:support@openvpms.org">OpenVPMS Team</a>
- * @version $LastChangedDate: 2006-05-02 05:16:31Z $
+ * @author Tim Anderson
  */
 public class EchoAuthenticationEntryPoint
-    extends LoginUrlAuthenticationEntryPoint {
+        extends LoginUrlAuthenticationEntryPoint {
+
+    /**
+     * Constructs an {@link EchoAuthenticationEntryPoint}.
+     *
+     * @param loginFormUrl URL where the login page can be found. Should either be relative to the web-app context path
+     *                     (include a leading {@code /}) or an absolute URL.
+     */
+    public EchoAuthenticationEntryPoint(String loginFormUrl) {
+        super(loginFormUrl);
+    }
 
     /**
      * Commences an authentication scheme.
      * <p/>
      * This implemenation simply sends an
-     * <tt>HttpServletResponse.SC_BAD_REQUEST</tt> in the response.
+     * {@code HttpServletResponse.SC_BAD_REQUEST} in the response.
      *
-     * @param request       that resulted in an <tt>AuthenticationException</tt>
+     * @param request       that resulted in an {@code AuthenticationException}
      * @param response      so that the user agent can begin authentication
      * @param authException that caused the invocation
      */
