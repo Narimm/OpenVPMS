e7244c1f3b811087a53089578f66e850f79cbf97
Tim Anderson <tma@netspace.net.au>
2015-10-12 07:11:33 +0000

    Changes for OVPMS-1674 Add VeNom 'Reason for visit' codes
    svn path=/projects/openvpms/trunk/; revision=6776

--- openvpms-etl/scripts/VeNom/lookups.pl
+++ openvpms-etl/scripts/VeNom/lookups.pl
@@ -6,7 +6,12 @@ use warnings;
 use Text::CSV;
 use XML::LibXML;
 
-my $file = $ARGV[0] or die "Usage lookups.pl <csv file>\n";
+my $file = shift or die "Usage lookups.pl <csv file> [-all|-diagnosis|-presentingComplaint|-visitReason]\n";
+my $lookups = shift || '-all';
+if ($lookups ne '-all' && $lookups ne "-diagnosis" && $lookups ne "-presentingComplaint"
+   && $lookups ne "-visitReason" ) {
+   die "Usage lookups.pl <csv file> [-all|-diagnosis|-presentingComplaint|-visitReason]\n";
+}
 
 my $csv = Text::CSV->new ({binary => 1, auto_diag => 1, sep_char => ','});
 
@@ -14,11 +19,18 @@ use open ':std', ':encoding(UTF-8)';
 open(my $data, $file) or die "Could not open '$file' $!\n";
 
 my $doc = XML::LibXML::Document->new('1.0', 'UTF-8');
+my $all = $lookups eq "-all";
+my $complaint = $all || $lookups eq "-presentingComplaint";
+my $diagnosis = $all || $lookups eq "-diagnosis";
+my $reason = $all || $lookups eq "-visitReason";
+
 print "<archetype>\n";
 while (my $fields = $csv->getline( $data )) {
     my $status = $fields->[0];
     my $label = $fields->[4];
-    if (($status ne "Active Non-referral" && $label eq "Presenting complaint") || $label eq "Diagnosis") {
+    if (($status ne "Active Non-referral" && $label eq "Presenting complaint" && $complaint)
+        || ($label eq "Diagnosis" && $diagnosis)
+        || ($label eq "Reason for visit" && $reason)) {
       my $id = $fields->[5];
       my $term = $fields->[6];
       my $modelling = $fields->[7];
@@ -32,7 +44,13 @@ while (my $fields = $csv->getline( $data )) {
       $term =~ s/\xb5/&\#181;/g;    # handle micro
 
       my $code = "VENOM_$id";
-      my $archetype = $label eq "Diagnosis" ? "lookup.diagnosisVeNom" : "lookup.presentingComplaintVeNom";
+      my $archetype = "lookup.diagnosisVeNom";
+      if ($label eq "Presenting complaint") {
+        $archetype = "lookup.presentingComplaintVeNom";
+      } elsif ($label eq "Reason for visit") {
+        $archetype = "lookup.visitReasonVeNom";
+      }
+
       print "<data id='$code' archetype='$archetype' code='$code' name='$term' description='$modelling' dataDictionaryId='$id'/>\n";
     }
 }
