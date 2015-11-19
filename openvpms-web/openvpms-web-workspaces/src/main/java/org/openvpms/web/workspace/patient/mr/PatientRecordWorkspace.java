0af985baf9188ae53cb4e200d0022ef3311f7603
Tim Anderson <tma@netspace.net.au>
2015-11-19 02:35:54 +0000

    Fix patient browser to avoid lazy creation problems when laying out workspace
    svn path=/projects/openvpms/trunk/; revision=6829

--- openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientRecordWorkspace.java
+++ openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientRecordWorkspace.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2014 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2015 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.patient.mr;
@@ -187,7 +187,10 @@ public class PatientRecordWorkspace extends BrowserCRUDWorkspace<Party, Act> {
      */
     protected RecordBrowser createRecordBrowser(Party patient, PatientHistoryQuery query, Context context,
                                                 HelpContext help) {
-        return new RecordBrowser(patient, query, context, help);
+        RecordBrowser browser = new RecordBrowser(patient, query, context, help);
+        // force component creation now to avoid lazy creation problems when laying out workspace
+        browser.getComponent();
+        return browser;
     }
 
     /**
