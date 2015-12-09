c53adbd5ed8d62e3a2b947b6792fb18c3e3d804a
Tim Anderson <tma@netspace.net.au>
2015-12-09 06:26:53 +0000

    added support to launch soffice if SWT fails, and soffice is in the path
    svn path=/projects/openvpms/branches/OVPMS-976/; revision=6854

--- openvpms-web/openvpms-webdav-launcher/src/main/java/org/openvpms/web/webdav/launch/EditorLauncher.java
+++ openvpms-web/openvpms-webdav-launcher/src/main/java/org/openvpms/web/webdav/launch/EditorLauncher.java
@@ -1,18 +1,36 @@
 package org.openvpms.web.webdav.launch;
 
-import org.eclipse.swt.SWT;
 import org.eclipse.swt.program.Program;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.MessageBox;
 
 import javax.swing.JOptionPane;
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  * Launches OpenOffice to edit a document.
+ * <p>
+ * Note that this uses the SWT Program class on Windows, Macs and some flavours of Linux to launch OpenOffice.
+ * <p>
+ * On other platforms it falls back to launching it from the PATH.
+ * <p>
+ * SWT is used as it is more likely to support Linux than java.awt.Desktop. The PATH is used as a fallback as users
+ * may not have OpenOffice in the PATH.
  *
  * @author Tim Anderson
  */
 public class EditorLauncher {
+
+    /**
+     * The logger.
+     */
+    private static final Logger logger = Logger.getLogger(EditorLauncher.class.getName());
 
     /**
      * The main line.
@@ -21,45 +39,94 @@ public class EditorLauncher {
      */
     public static void main(String[] args) {
         if (args.length == 1) {
-            String file = args[0];
-            launch(file);
+            logger.info("Launching soffice, os.name=" + System.getProperty("os.name") + ", os.arch="
+                        + System.getProperty("os.arch"));
+            String url = args[0];
+            if (!launchViaSWT(url) && !launchViaProcess(url)) {
+                JOptionPane.showMessageDialog(null, "OpenOffice could not be located to edit the document.",
+                                              "OpenVPMS Editor Launcher", JOptionPane.ERROR_MESSAGE);
+            }
         } else {
-            System.err.println("usage: " + EditorLauncher.class.getName() + " <file>");
+            System.err.println("usage: " + EditorLauncher.class.getName() + " <url>");
         }
     }
 
-    private static void launch(String file) {
-        int index = file.lastIndexOf(".");
-        if (index != -1) {
-            Display display = null;
+
+    /**
+     * Launches OpenOffice using the SWT Program class.
+     *
+     * @param url the document URL
+     * @return {@code true} if the launch was successful
+     */
+    private static boolean launchViaSWT(String url) {
+        boolean result = false;
+        Display display = null;
+        try {
+            display = new Display();
+            Program program = Program.findProgram(".odt");
+            if (program != null) {
+                program.execute(url);
+                result = true;
+            }
+        } catch (Throwable exception) {
+            logger.log(Level.WARNING, "Failed to launch OpenOffice via SWT", exception);
+        } finally {
+            if (display != null) {
+                display.dispose();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Launches OpenOffice using ProcessBuilder.
+     *
+     * @param url the document URL
+     * @return {@code true} if the launch was successful
+     */
+    private static boolean launchViaProcess(String url) {
+        boolean result = false;
+        Path path = getPath();
+        if (path != null) {
+            ProcessBuilder builder = new ProcessBuilder(path.toString(), url);
             try {
-                display = new Display();
-                String suffix = file.substring(index);
-                Program program = Program.findProgram(suffix);
-                if (program != null) {
-                    program.execute(file);
-                } else {
-                    JOptionPane.showMessageDialog(null, "thank you for using java");
-                    MessageBox error = new MessageBox(display.getActiveShell(), SWT.ICON_ERROR | SWT.OK);
-                    error.setText("OpenVPMS Editor Launcher");
-                    error.setMessage("OpenOffice could not be located to edit the document.");
-                    error.open();
-                }
+                Process process = builder.inheritIO().start();
+                result = process.isAlive();
             } catch (Throwable exception) {
-                if (display != null) {
-                    MessageBox error = new MessageBox(display.getActiveShell(), SWT.ICON_ERROR | SWT.OK);
-                    error.setText("OpenVPMS Editor Launcher");
-                    error.setMessage("Failed to launch the editor: " + exception.getMessage());
-                    exception.printStackTrace();
-                } else {
-                    exception.printStackTrace();
-                }
-            } finally {
-                if (display != null) {
-                    display.dispose();
+                logger.log(Level.SEVERE, "Failed to launch OpenOffice ath path: " + path, exception);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Returns the path to soffice, it it is located in the PATH environment variable.
+     *
+     * @return the path, or {@code null} if it doesn't exist
+     */
+    private static Path getPath() {
+        Path result = null;
+        String osName = System.getProperty("os.name");
+        String name = osName != null && osName.startsWith("Windows") ? "soffice.exe" : "soffice";
+        String paths = System.getenv("PATH");
+        if (paths != null) {
+            for (String path : paths.split(Pattern.quote(File.pathSeparator))) {
+                try {
+                    Path dir = Paths.get(path);
+                    Path file = dir.resolve(name);
+                    if (Files.exists(file)) {
+                        result = file;
+                        break;
+                    }
+                } catch (InvalidPathException ignore) {
+                    // do nothing
                 }
             }
         }
+        if (result == null) {
+            logger.warning(name + " not found in PATH: " + paths);
+        }
+        return result;
     }
 }
 
