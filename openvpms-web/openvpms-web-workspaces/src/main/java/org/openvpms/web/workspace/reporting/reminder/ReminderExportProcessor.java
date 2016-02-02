cb93c6a22ce84aa86d3b137d0b72ab800b39da72
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2016-02-02 00:34:11 +0000

    Reminder generation changes: . made ReminderExportProcessor public . added AbstractReminderBatchProcessor.updateReminder(ReminderEvent reminder, Date date)
    git-svn-id: svn://localhost/openvpms/projects/openvpms/trunk@6893 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/reporting/reminder/ReminderExportProcessor.java
+++ openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/reporting/reminder/ReminderExportProcessor.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2016 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.reporting.reminder;
@@ -32,7 +32,7 @@ import java.util.List;
  *
  * @author Tim Anderson
  */
-class ReminderExportProcessor extends AbstractReminderBatchProcessor {
+public class ReminderExportProcessor extends AbstractReminderBatchProcessor {
 
     /**
      * Constructs a {@link ReminderExportProcessor}.
