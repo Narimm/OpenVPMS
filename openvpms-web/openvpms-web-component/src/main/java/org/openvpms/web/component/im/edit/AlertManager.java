011521ec15d2873cc1fc0cf17d71ad1126c9bdbf
Tim Anderson <tma@netspace.net.au>
2016-03-04 05:59:08 +0000

    Refactored alert handling
    svn path=/projects/openvpms/branches/stockonhand/; revision=6926

--- openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/AlertManager.java
+++ openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/edit/AlertManager.java
@@ -16,9 +16,29 @@
 
 package org.openvpms.web.component.im.edit;
 
+import nextapp.echo2.app.ApplicationInstance;
+import nextapp.echo2.app.Button;
 import nextapp.echo2.app.Component;
+import nextapp.echo2.app.ContentPane;
+import nextapp.echo2.app.Extent;
+import nextapp.echo2.app.Label;
+import nextapp.echo2.app.Row;
+import nextapp.echo2.app.WindowPane;
+import nextapp.echo2.app.event.ActionEvent;
+import nextapp.echo2.app.event.WindowPaneEvent;
+import org.apache.commons.lang.StringUtils;
 import org.openvpms.web.component.edit.AlertListener;
-import org.openvpms.web.echo.message.InformationMessage;
+import org.openvpms.web.component.util.StyleSheetHelper;
+import org.openvpms.web.echo.event.ActionListener;
+import org.openvpms.web.echo.event.WindowPaneListener;
+import org.openvpms.web.echo.factory.ButtonFactory;
+import org.openvpms.web.echo.factory.ColumnFactory;
+import org.openvpms.web.echo.factory.LabelFactory;
+import org.openvpms.web.echo.factory.RowFactory;
+import org.openvpms.web.echo.style.Styles;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Manages the display of alerts.
@@ -33,17 +53,34 @@ public class AlertManager {
     private final Component container;
 
     /**
-     * The listener to handle alert messages. These are added to the container.
+     * The listener to handle alert messages.
      */
     private final AlertListener listener;
 
     /**
+     * The maximum number of alerts to display.
+     */
+    private final int maxAlerts;
+
+    /**
+     * The current alerts.
+     */
+    private List<WindowPane> alerts = new ArrayList<>();
+
+    /**
+     * The default offset for alerts.
+     */
+    private static final int OFFSET = 5;
+
+    /**
      * Constructs an {@link AlertManager}.
      *
-     * @param container the container for alerts. This may hold other components; the alerts will be displayed at the head
+     * @param container the container used to locate the parent for alerts; Alerts will be registered on the nearest
+     *                  parent ContentPane
      */
-    public AlertManager(Component container) {
+    public AlertManager(Component container, int maxAlerts) {
         this.container = container;
+        this.maxAlerts = maxAlerts;
         listener = new AlertListener() {
             @Override
             public long onAlert(String message) {
@@ -67,15 +104,94 @@ public class AlertManager {
     }
 
     /**
+     * Clear any alerts.
+     */
+    public void clear() {
+        for (WindowPane alert : alerts) {
+            alert.userClose();
+        }
+    }
+
+    /**
      * Invoked to display an alert.
      *
      * @param message the alert message
      * @return a handle to cancel the alert
      */
     protected long showAlert(String message) {
-        InformationMessage alert = new InformationMessage(message);
-        container.add(alert, 0);
-        return System.identityHashCode(alert);
+        if (alerts.size() < maxAlerts) {
+            final WindowPane pane = new InformationMessage(message);
+            int y = OFFSET;
+            for (WindowPane alert : alerts) {
+                y = getNextY(alert);
+            }
+            pane.setPositionY(new Extent(y));
+            getContentPane().add(pane);
+            pane.addWindowPaneListener(new WindowPaneListener() {
+                @Override
+                public void onClose(WindowPaneEvent event) {
+                    remove(pane);
+                }
+            });
+            alerts.add(pane);
+            return System.identityHashCode(pane);
+        }
+        return -1;
+    }
+
+    /**
+     * Returns the Y position of the next alert.
+     *
+     * @param alert the alert to position relative to
+     * @return the next Y position
+     */
+    private int getNextY(WindowPane alert) {
+        Extent position = alert.getPositionY();
+        int y = (position != null) ? position.getValue() : OFFSET;
+        Extent height = alert.getHeight();
+        if (height != null) {
+            y += height.getValue() + OFFSET;
+        } else {
+            y += OFFSET;
+        }
+        return y;
+    }
+
+    /**
+     * Removes an alert and repositions the remaining alerts.
+     *
+     * @param alert the alert to remove
+     */
+    private void remove(WindowPane alert) {
+        alerts.remove(alert);
+        shuffle();
+    }
+
+    /**
+     * Adjusts the positions of the alerts.
+     */
+    private void shuffle() {
+        int y = OFFSET;
+        for (WindowPane alert : alerts) {
+            alert.setPositionY(new Extent(y));
+            y = getNextY(alert);
+        }
+    }
+
+    /**
+     * Returns the content pane for alerts.
+     *
+     * @return the content pane
+     */
+    protected Component getContentPane() {
+        Component component = container;
+        while (component != null && !(component instanceof ContentPane)) {
+            component = component.getParent();
+        }
+        if (component == null) {
+            component = ApplicationInstance.getActive().getDefaultWindow().getContent();
+        }
+        return component;
     }
 
     /**
@@ -84,12 +200,49 @@ public class AlertManager {
      * @param id the alert identifier
      */
     protected void cancelAlert(long id) {
-        for (Component component : container.getComponents()) {
-            if (component instanceof InformationMessage && System.identityHashCode(component) == id) {
-                container.remove(component);
-                break;
+        for (WindowPane alert : alerts.toArray(new WindowPane[alerts.size()])) {
+            if (System.identityHashCode(alert) == id) {
+                alert.userClose();
             }
         }
     }
 
+    private static class InformationMessage extends WindowPane {
+        public InformationMessage(String message) {
+            setStyleName("InformationMessage");
+            setClosable(false);
+            setPositionX(new Extent(OFFSET));
+            setPositionY(new Extent(OFFSET));
+            Extent height = getHeight(message);
+            setHeight(height);
+            setMinimumHeight(height);
+            Label label = LabelFactory.create(null, true);
+            label.setStyleName("InformationMessage");
+            label.setText(message);
+            Button button = ButtonFactory.create(null, "Message.close");
+            button.addActionListener(new ActionListener() {
+                @Override
+                public void onAction(ActionEvent event) {
+                    userClose();
+                }
+            });
+
+            Row buttonRow = RowFactory.create(button);
+            buttonRow.setLayoutData(RowFactory.rightAlign());
+            add(ColumnFactory.create(Styles.INSET, RowFactory.create(Styles.WIDE_CELL_SPACING, label, buttonRow)));
+        }
+
+        /**
+         * Returns the height of the alert.
+         *
+         * @param message the message
+         * @return the height, or {@code null} if it cannot be determined
+         */
+        private Extent getHeight(String message) {
+            int lines = StringUtils.countMatches(message, "\n") + 1;
+            int height = StyleSheetHelper.getProperty("alert.height", -1);
+            return height > 0 ? new Extent(height * lines) : null;
+        }
+
+    }
 }
