158e04422b99a61f421fa9f55428437bf38e54b8
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2016-04-20 08:13:29 +0000

    Changes for OVPMS-1752 Add history:medication(patient, productTypeName, date) . changed ReminderFunctions to return Iterable instead of List to better handle large no.s of reminders . changed ListFunctions methods to support Iterable instead of Collection . changed reminder:getReminders functions taking a product type to supply the argument  before any dates, to be consistent with history functions
    git-svn-id: svn://localhost/openvpms/projects/openvpms/trunk@7020 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-archetypes/src/java/org/openvpms/archetype/function/history/HistoryFunctions.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/function/history/HistoryFunctions.java
@@ -11,25 +11,32 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2013 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2016 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.archetype.function.history;
 
+import org.apache.commons.jxpath.Function;
 import org.openvpms.archetype.rules.patient.PatientHistory;
+import org.openvpms.archetype.rules.util.DateRules;
+import org.openvpms.archetype.rules.util.DateUnits;
 import org.openvpms.component.business.domain.im.act.Act;
 import org.openvpms.component.business.domain.im.party.Party;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
+import org.openvpms.component.system.common.jxpath.AbstractObjectFunctions;
 
 import java.util.Collections;
 import java.util.Date;
 
 /**
  * Patient history functions, for use in reporting.
+ * <p/>
+ * This extends {@link AbstractObjectFunctions} in order to translate the "medication" function to the appropriate
+ * implementation to avoid ambiguous method calls.
  *
  * @author Tim Anderson
  */
-public class HistoryFunctions {
+public class HistoryFunctions extends AbstractObjectFunctions {
 
     /**
      * The archetype service.
@@ -42,7 +49,44 @@ public class HistoryFunctions {
      * @param service the archetype service
      */
     public HistoryFunctions(IArchetypeService service) {
+        super("history");
+        setObject(this);
         history = new PatientHistory(service);
+    }
+
+    /**
+     * Returns a Function, if any, for the specified namespace name and parameter types.
+     * <p/>
+     * This implementation changes medication to {@link #medicationByProductType} if the second argument is a string.
+     * This is required as JXPath can't resolve which method to call if arguments are null.
+     *
+     * @param namespace if it is not the namespace specified in the constructor, the method returns null
+     * @param name      is a function name.
+     * @return a MethodFunction, or {@code null} if there is no such function.
+     */
+    @Override
+    public Function getFunction(String namespace, String name, Object[] parameters) {
+        if ("medication".equals(name) && parameters != null && parameters.length >= 2
+            && parameters[1] instanceof String) {
+            name = "medicationByProductType";
+        }
+        return super.getFunction(namespace, name, parameters);
+    }
+
+    /**
+     * Returns medication acts for a patient, on the specified date.
+     *
+     * @param patient the patient. May be {@code null}
+     * @param date    the date
+     * @return the medication acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> medication(Party patient, Date date) {
+        if (patient != null && date != null) {
+            Date from = DateRules.getDate(date);
+            Date to = DateRules.getDate(from, 1, DateUnits.DAYS);
+            return history.getMedication(patient, from, to);
+        }
+        return Collections.emptyList();
     }
 
     /**
@@ -56,11 +100,11 @@ public class HistoryFunctions {
     }
 
     /**
-     * Returns medication acts for a patient, between the specified dates, inclusive.
+     * Returns medication acts for a patient, between the specified dates.
      *
      * @param patient the patient. May be {@code null}
-     * @param from    the from date-time. May be {@code null}
-     * @param to      the to date-time. May be {@code null}
+     * @param from    the start of the date range, inclusive. May be {@code null}
+     * @param to      the end of the date range, exclusive. May be {@code null}
      * @return the medication acts for the patient, or an empty list if patient is {@code null}
      */
     public Iterable<Act> medication(Party patient, Date from, Date to) {
@@ -71,11 +115,28 @@ public class HistoryFunctions {
      * Returns medication acts for a patient, with the specified product type name.
      *
      * @param patient         the patient. May be {@code null}
-     * @param productTypeName the product type name. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
      * @return the medication acts for the patient, or an empty list if patient is {@code null}
      */
-    public Iterable<Act> medication(Party patient, String productTypeName) {
+    public Iterable<Act> medicationByProductType(Party patient, String productTypeName) {
         return patient != null ? history.getMedication(patient, productTypeName) : Collections.<Act>emptyList();
+    }
+
+    /**
+     * Returns medication acts for a patient, with the specified product type name on the specified date.
+     *
+     * @param patient         the patient. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
+     * @param date            the date
+     * @return the medication acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> medicationByProductType(Party patient, String productTypeName, Date date) {
+        if (patient != null && date != null) {
+            Date from = DateRules.getDate(date);
+            Date to = DateRules.getDate(from, 1, DateUnits.DAYS);
+            return history.getMedication(patient, productTypeName, from, to);
+        }
+        return Collections.emptyList();
     }
 
     /**
@@ -83,12 +144,12 @@ public class HistoryFunctions {
      * inclusive.
      *
      * @param patient         the patient. May be {@code null}
-     * @param productTypeName the product type name. May be {@code null}
-     * @param from            the from date-time. May be {@code null}
-     * @param to              the to date-time. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
+     * @param from            the start of the date range, inclusive. May be {@code null}
+     * @param to              the end of the date range, exclusive. May be {@code null}
      * @return the medication acts for the patient, or an empty list if patient is {@code null}
      */
-    public Iterable<Act> medication(Party patient, String productTypeName, Date from, Date to) {
+    public Iterable<Act> medicationByProductType(Party patient, String productTypeName, Date from, Date to) {
         return patient != null ? history.getMedication(patient, productTypeName, from, to)
                                : Collections.<Act>emptyList();
     }
