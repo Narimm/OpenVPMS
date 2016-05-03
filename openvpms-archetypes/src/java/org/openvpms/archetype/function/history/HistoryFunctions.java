c6fc32382bb8d39c52567c26e6177766e224bb51
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2016-05-03 11:38:44 +0000

    Changes for OVPMS-1762 Provide history functions to return charges
    git-svn-id: svn://localhost/openvpms/projects/openvpms/trunk@7052 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-archetypes/src/java/org/openvpms/archetype/function/history/HistoryFunctions.java
+++ openvpms-archetypes/src/java/org/openvpms/archetype/function/history/HistoryFunctions.java
@@ -57,7 +57,11 @@ public class HistoryFunctions extends AbstractObjectFunctions {
     /**
      * Returns a Function, if any, for the specified namespace name and parameter types.
      * <p/>
-     * This implementation changes medication to {@link #medicationByProductType} if the second argument is a string.
+     * This implementation changes:
+     * <ul>
+     * <li>medication to {@link #medicationByProductType} if the second argument is a string.</li>
+     * <li>charges to {@link #chargesByProductType} if the second argument is a string.</li>
+     * </ul>
      * This is required as JXPath can't resolve which method to call if arguments are null.
      *
      * @param namespace if it is not the namespace specified in the constructor, the method returns null
@@ -69,24 +73,91 @@ public class HistoryFunctions extends AbstractObjectFunctions {
         if ("medication".equals(name) && parameters != null && parameters.length >= 2
             && parameters[1] instanceof String) {
             name = "medicationByProductType";
+        } else if ("charges".equals(name) && parameters != null && parameters.length >= 2
+                   && parameters[1] instanceof String) {
+            name = "chargesByProductType";
         }
         return super.getFunction(namespace, name, parameters);
     }
 
     /**
-     * Returns medication acts for a patient, on the specified date.
+     * Returns all invoice item acts for a patient.
      *
      * @param patient the patient. May be {@code null}
-     * @param date    the date
-     * @return the medication acts for the patient, or an empty list if patient is {@code null}
+     * @return the invoice items acts for the patient, or an empty list if patient is {@code null}
      */
-    public Iterable<Act> medication(Party patient, Date date) {
+    public Iterable<Act> charges(Party patient) {
+        return patient != null ? history.getCharges(patient) : Collections.<Act>emptyList();
+    }
+
+    /**
+     * Returns invoice item acts for a patient, on the specified date.
+     *
+     * @param patient the patient. May be {@code null}
+     * @param date    the date. May be {@code null}
+     * @return the medication acts for the patient, or an empty list if an argument is {@code null}
+     */
+    public Iterable<Act> charges(Party patient, Date date) {
         if (patient != null && date != null) {
             Date from = DateRules.getDate(date);
             Date to = DateRules.getDate(from, 1, DateUnits.DAYS);
-            return history.getMedication(patient, from, to);
+            return history.getCharges(patient, from, to);
         }
         return Collections.emptyList();
+    }
+
+    /**
+     * Returns invoice item acts for a patient, between the specified dates.
+     *
+     * @param patient the patient. May be {@code null}
+     * @param from    the start of the date range, inclusive. May be {@code null}
+     * @param to      the end of the date range, exclusive. May be {@code null}
+     * @return the invoice item acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> charges(Party patient, Date from, Date to) {
+        return patient != null ? history.getCharges(patient, from, to) : Collections.<Act>emptyList();
+    }
+
+    /**
+     * Returns invoice item acts for a patient, with the specified product type name.
+     *
+     * @param patient         the patient. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
+     * @return the invoice item acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> chargesByProductType(Party patient, String productTypeName) {
+        return patient != null ? history.getCharges(patient, productTypeName) : Collections.<Act>emptyList();
+    }
+
+    /**
+     * Returns invoice item acts for a patient, with the specified product type name on the specified date.
+     *
+     * @param patient         the patient. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
+     * @param date            the date
+     * @return the invoice item acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> chargesByProductType(Party patient, String productTypeName, Date date) {
+        if (patient != null && date != null) {
+            Date from = DateRules.getDate(date);
+            Date to = DateRules.getDate(from, 1, DateUnits.DAYS);
+            return history.getCharges(patient, productTypeName, from, to);
+        }
+        return Collections.emptyList();
+    }
+
+    /**
+     * Returns invoice item acts for a patient, with the specified product type name, between the specified dates.
+     *
+     * @param patient         the patient. May be {@code null}
+     * @param productTypeName the product type name. May be {@code null}, or contain wildcards
+     * @param from            the start of the date range, inclusive. May be {@code null}
+     * @param to              the end of the date range, exclusive. May be {@code null}
+     * @return the medication acts for the patient, or an empty list if patient is {@code null}
+     */
+    public Iterable<Act> chargesByProductType(Party patient, String productTypeName, Date from, Date to) {
+        return patient != null ? history.getCharges(patient, productTypeName, from, to)
+                               : Collections.<Act>emptyList();
     }
 
     /**
@@ -97,6 +168,22 @@ public class HistoryFunctions extends AbstractObjectFunctions {
      */
     public Iterable<Act> medication(Party patient) {
         return patient != null ? history.getMedication(patient) : Collections.<Act>emptyList();
+    }
+
+    /**
+     * Returns medication acts for a patient, on the specified date.
+     *
+     * @param patient the patient. May be {@code null}
+     * @param date    the date. May be {@code null}
+     * @return the medication acts for the patient, or an empty list if an argument is {@code null}
+     */
+    public Iterable<Act> medication(Party patient, Date date) {
+        if (patient != null && date != null) {
+            Date from = DateRules.getDate(date);
+            Date to = DateRules.getDate(from, 1, DateUnits.DAYS);
+            return history.getMedication(patient, from, to);
+        }
+        return Collections.emptyList();
     }
 
     /**
@@ -140,8 +227,7 @@ public class HistoryFunctions extends AbstractObjectFunctions {
     }
 
     /**
-     * Returns medication acts for a patient, with the specified product type name, between the specified dates,
-     * inclusive.
+     * Returns medication acts for a patient, with the specified product type name, between the specified dates.
      *
      * @param patient         the patient. May be {@code null}
      * @param productTypeName the product type name. May be {@code null}, or contain wildcards
