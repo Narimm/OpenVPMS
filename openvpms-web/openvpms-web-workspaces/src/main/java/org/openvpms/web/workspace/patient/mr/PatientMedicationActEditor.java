5e2405a8aa9a6cd2d5101defb99b9d305c57ea3d
tanderson <tanderson@9e4fa352-aafb-0310-9071-df624684024b>
2017-02-06 23:41:32 +0000

    Fix for OVPMS-1850 Prescription medications not restricted by patient species
    git-svn-id: svn://localhost/openvpms/projects/openvpms/trunk@7345 9e4fa352-aafb-0310-9071-df624684024b

--- openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientMedicationActEditor.java
+++ openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/patient/mr/PatientMedicationActEditor.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2016 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2017 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.patient.mr;
@@ -441,7 +441,7 @@ public class PatientMedicationActEditor extends PatientActEditor {
      *
      * @return the product editor, or {@code null} if none exists
      */
-    private ProductParticipationEditor getProductEditor() {
+    protected ProductParticipationEditor getProductEditor() {
         ParticipationEditor<Product> editor = getParticipationEditor(PRODUCT, false);
         return (ProductParticipationEditor) editor;
     }
