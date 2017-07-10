fb9243327e4f2b421c1f4cef814cad4b8153966a
Tim Anderson <tma@netspace.net.au>
2017-07-10 09:22:54 +0000

    . added support for plugins to store and retrieve configuration . added support to register terminals
    svn path=/projects/openvpms/branches/OVPMS-1895/; revision=7534

--- openvpms-eftpos/src/main/java/org/openvpms/eftpos/Terminal.java
+++ openvpms-eftpos/src/main/java/org/openvpms/eftpos/Terminal.java
@@ -1,3 +1,19 @@
+/*
+ * Version: 1.0
+ *
+ * The contents of this file are subject to the OpenVPMS License Version
+ * 1.0 (the 'License'); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.openvpms.org/license/
+ *
+ * Software distributed under the License is distributed on an 'AS IS' basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * Copyright 2017 (C) OpenVPMS Ltd. All Rights Reserved.
+ */
+
 package org.openvpms.eftpos;
 
 import org.openvpms.component.business.domain.im.party.Party;
@@ -12,9 +28,27 @@ import java.math.BigDecimal;
 public interface Terminal {
 
     /**
+     * Determines if the terminal has been registered.
+     *
+     * @return {@code true} if the terminal has been registered
+     * @throws EFTPOSException for any EFTPOS error
+     */
+    boolean isRegistered();
+
+    /**
+     * Registers the terminal prior to use.
+     * <p>
+     * This is required if {@link #isRegistered} returns {@code false}.
+     *
+     * @throws EFTPOSException for any EFTPOS error
+     */
+    void register();
+
+    /**
      * Determines if the terminal is available.
      *
-     * @return {@code true} if the terminal is available, {@code false} if it is in use
+     * @return {@code true} if the terminal is available, {@code false} if it is in use, or has not been registered
+     * @throws EFTPOSException for any EFTPOS error
      */
     boolean isAvailable();
 
@@ -25,7 +59,7 @@ public interface Terminal {
      * @param amount   the amount
      * @param cashout  the cash-out amount
      * @return the transaction corresponding to the payment
-     * @throws EFTPOSException for any POS error
+     * @throws EFTPOSException for any EFTPOS error
      */
     Transaction pay(Party customer, BigDecimal amount, BigDecimal cashout);
 
@@ -34,7 +68,7 @@ public interface Terminal {
      *
      * @param id the transaction identifier
      * @return the transaction
-     * @throws EFTPOSException for any POS error
+     * @throws EFTPOSException for any EFTPOS error
      */
     Transaction get(String id);
 
