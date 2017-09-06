e255b7ccb48553499f8466b22db8cde283d155c8
Tim Anderson <tma@netspace.net.au>
2017-09-06 23:56:29 +0000

    Changes for OVPMS-1871 Smart Flow Sheet integration - billing and notes . changed SmartFlowSheetEventServiceImpl to use a single thread for all locations . added better support to handle updates to the API key on a practice location
    svn path=/projects/openvpms/branches/2.0-dev/; revision=7589

--- openvpms-web/openvpms-smartflow/src/main/java/org/openvpms/smartflow/event/impl/SmartFlowSheetEventServiceImpl.java
+++ openvpms-web/openvpms-smartflow/src/main/java/org/openvpms/smartflow/event/impl/SmartFlowSheetEventServiceImpl.java
@@ -16,37 +16,26 @@
 
 package org.openvpms.smartflow.event.impl;
 
-import org.apache.commons.lang.ObjectUtils;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openvpms.archetype.rules.patient.PatientRules;
 import org.openvpms.archetype.rules.practice.PracticeArchetypes;
 import org.openvpms.archetype.rules.practice.PracticeService;
 import org.openvpms.component.business.domain.im.common.IMObject;
 import org.openvpms.component.business.domain.im.party.Party;
-import org.openvpms.component.business.domain.im.security.User;
 import org.openvpms.component.business.service.archetype.AbstractArchetypeServiceListener;
 import org.openvpms.component.business.service.archetype.IArchetypeService;
 import org.openvpms.component.business.service.archetype.IArchetypeServiceListener;
 import org.openvpms.component.business.service.lookup.ILookupService;
 import org.openvpms.component.system.common.event.Listener;
 import org.openvpms.smartflow.client.FlowSheetServiceFactory;
-import org.openvpms.smartflow.client.ReferenceDataService;
 import org.openvpms.smartflow.event.EventDispatcher;
 import org.openvpms.smartflow.event.SmartFlowSheetEventService;
-import org.openvpms.smartflow.model.ServiceBusConfig;
 import org.openvpms.smartflow.model.event.Event;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.transaction.PlatformTransactionManager;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Default implementation of the {@link SmartFlowSheetEventService}.
@@ -56,19 +45,9 @@ import java.util.concurrent.TimeUnit;
 public class SmartFlowSheetEventServiceImpl implements InitializingBean, DisposableBean, SmartFlowSheetEventService {
 
     /**
-     * The SFS service factory.
-     */
-    private final FlowSheetServiceFactory factory;
-
-    /**
      * The archetype service.
      */
     private final IArchetypeService service;
-
-    /**
-     * The lookup service.
-     */
-    private final ILookupService lookups;
 
     /**
      * The practice service.
@@ -76,39 +55,14 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
     private final PracticeService practiceService;
 
     /**
-     * The transaction manager.
+     * The queue dispatchers.
      */
-    private final PlatformTransactionManager transactionManager;
+    private final QueueDispatchers dispatchers;
 
     /**
-     * The patient rules.
+     * The scheduled event dispatcher.
      */
-    private final PatientRules rules;
-
-    /**
-     * Practice locations keyed on clinic API key.
-     */
-    private Map<String, Set<Party>> locationsByKey = new HashMap<>();
-
-    /**
-     * Clinic API key keyed on location.
-     */
-    private Map<Party, String> keysByLocation = new HashMap<>();
-
-    /**
-     * The queue readers, keyed on clinic API key.
-     */
-    private Map<String, QueueReader> readers = new HashMap<>();
-
-    /**
-     * The event dispatchers, keyed on clinic API key.
-     */
-    private final Map<String, EventDispatcher> dispatchers = new HashMap<>();
-
-    /**
-     * Used to schedule dispatching.
-     */
-    private final ExecutorService executor;
+    private final ScheduledDispatcher dispatcher;
 
     /**
      * Used to handle practice location updates.
@@ -119,21 +73,6 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
      * The listener for practice location updates.
      */
     private final IArchetypeServiceListener listener;
-
-    /**
-     * The default interval between polls, in seconds.
-     */
-    private int interval = 30;
-
-    /**
-     * The user assigned to the dispatch thread.
-     */
-    private User user;
-
-    /**
-     * The logger.
-     */
-    private static final Log log = LogFactory.getLog(SmartFlowSheetEventServiceImpl.class);
 
     /**
      * Constructs a {@link SmartFlowSheetEventServiceImpl}.
@@ -148,15 +87,10 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
     protected SmartFlowSheetEventServiceImpl(FlowSheetServiceFactory factory, IArchetypeService service,
                                              ILookupService lookups, PracticeService practiceService,
                                              PlatformTransactionManager transactionManager, PatientRules rules) {
-        this.factory = factory;
         this.service = service;
-        this.lookups = lookups;
         this.practiceService = practiceService;
-        this.transactionManager = transactionManager;
-        this.rules = rules;
 
         updateService = Executors.newSingleThreadExecutor();
-        executor = Executors.newSingleThreadExecutor();
 
         // listen for practice location update events, and schedule a thread to handle them.
         // This avoids blocking the user thread that updated the location.
@@ -182,7 +116,10 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
                 });
             }
         };
-
+        QueueDispatcherFactory readerFactory = new QueueDispatcherFactory(factory, service, lookups, transactionManager,
+                                                                          rules);
+        dispatchers = new QueueDispatchers(readerFactory);
+        dispatcher = new ScheduledDispatcher(dispatchers, practiceService);
     }
 
     /**
@@ -192,10 +129,7 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
      */
     @Override
     public void setPollInterval(int interval) {
-        if (interval <= 0) {
-            throw new IllegalArgumentException("Argument 'interval' must be > 0");
-        }
-        this.interval = interval;
+        dispatcher.setPollInterval(interval);
     }
 
     /**
@@ -203,13 +137,7 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
      */
     @Override
     public void poll() {
-        QueueReader[] list;
-        synchronized (this) {
-            list = readers.values().toArray(new QueueReader[readers.size()]);
-        }
-        for (QueueReader reader : list) {
-            reader.poll();
-        }
+        dispatcher.dispatch();
     }
 
     /**
@@ -223,7 +151,7 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
      */
     @Override
     public void addListener(Party location, String id, Listener<Event> listener) {
-        EventDispatcher dispatcher = getDispatcher(location);
+        EventDispatcher dispatcher = dispatchers.getEventDispatcher(location);
         if (dispatcher != null) {
             dispatcher.addListener(id, listener);
             poll();
@@ -238,7 +166,7 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
      */
     @Override
     public void removeListener(Party location, String id) {
-        EventDispatcher dispatcher = getDispatcher(location);
+        EventDispatcher dispatcher = dispatchers.getEventDispatcher(location);
         if (dispatcher != null) {
             dispatcher.removeListener(id);
         }
@@ -257,10 +185,7 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
     @Override
     public void afterPropertiesSet() throws Exception {
         for (Party location : practiceService.getLocations()) {
-            String key = factory.getClinicAPIKey(location);
-            if (key != null) {
-                addKey(key, location);
-            }
+            dispatchers.add(location);
         }
         service.addListener(PracticeArchetypes.LOCATION, listener);
         poll();
@@ -274,139 +199,30 @@ public class SmartFlowSheetEventServiceImpl implements InitializingBean, Disposa
     @Override
     public void destroy() throws Exception {
         service.removeListener(PracticeArchetypes.LOCATION, listener);
+        dispatcher.destroy();
         updateService.shutdown();
-        executor.shutdown();
-        try {
-            // Wait a while for existing tasks to terminate
-            if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                executor.shutdownNow(); // Cancel currently executing tasks
-                // Wait a while for tasks to respond to being cancelled
-                if (!executor.awaitTermination(60, TimeUnit.SECONDS)) {
-                    log.error("Pool did not terminate");
-                }
-            }
-        } catch (InterruptedException exception) {
-            // (Re-)Cancel if current thread also interrupted
-            executor.shutdownNow();
-            // Preserve interrupt status
-            Thread.currentThread().interrupt();
-        }
     }
 
     /**
-     * Returns the dispatcher for the specified location.
+     * Invoked when a practice location is saved.
      *
-     * @param location the location
-     * @return the dispatcher, or {@code null} if none is found
-     */
-    private synchronized EventDispatcher getDispatcher(Party location) {
-        EventDispatcher result = null;
-        String key = keysByLocation.get(location);
-        if (key != null) {
-            result = dispatchers.get(key);
-        }
-        return result;
-    }
-
-    /**
-     * Returns the user to set the security context in the dispatch thread.
-     *
-     * @return the user, or {@code null} if none has been configured
-     */
-    private User getServiceUser() {
-        if (user == null) {
-            synchronized (this) {
-                user = practiceService.getServiceUser();
-                if (user == null) {
-                    log.error("Missing party.organisationPractice serviceUser. Messages cannot be processed until " +
-                              "this is configured");
-                }
-            }
-        }
-        return user;
-    }
-
-    /**
-     * Returns the Azure Service Bus configuration for a practice location.
-     *
-     * @return the configuration, or {@code null} if it is not configured
-     */
-    private ServiceBusConfig getConfig(Party location) {
-        ReferenceDataService referenceData = factory.getReferenceDataService(location);
-        return referenceData.getServiceBusConfig();
-    }
-
-    private synchronized void locationSaved(Party location) {
-        String existingKey = keysByLocation.get(location);
-        String newKey = (location.isActive()) ? factory.getClinicAPIKey(location) : null;
-        if (!ObjectUtils.equals(existingKey, newKey)) {
-            QueueReader reader;
-            if (existingKey == null) {
-                reader = addKey(newKey, location);
-            } else {
-                removeKey(existingKey, location);
-                reader = addKey(newKey, location);
-            }
-            if (reader != null) {
-                reader.poll();
-            }
-        }
-    }
-
-    private synchronized void locationRemoved(Party location) {
-        String existingKey = keysByLocation.get(location);
-        if (existingKey != null) {
-            removeKey(existingKey, location);
-        }
-    }
-
-    /**
-     * Adds an API key for the specified practice location.
-     * <p>
-     * If the key is not already registered, this creates an {@link EventDispatcher} to handle messages read from
-     * the SFS Azure Service Bus queue.
-     *
-     * @param key      the clinic API key
      * @param location the practice location
-     * @return the queue reader, or {@code null} if none was added
      */
-    private QueueReader addKey(String key, Party location) {
-        QueueReader reader = null;
-        try {
-            Set<Party> locations = locationsByKey.get(key);
-            if (locations == null) {
-                ServiceBusConfig config = getConfig(location);
-                EventDispatcher dispatcher = new DefaultEventDispatcher(location, service, lookups, factory, rules);
-                locations = new HashSet<>();
-                locationsByKey.put(key, locations);
-                dispatchers.put(key, dispatcher);
-                reader = new QueueReader(config, dispatcher, getServiceUser(), executor, transactionManager, interval);
-                readers.put(key, reader);
-            }
-            locations.add(location);
-            keysByLocation.put(location, key);
-        } catch (Exception exception) {
-            log.error("Failed initialise Smart Flow Sheet queue for location=" + location.getName(), exception);
+    private void locationSaved(Party location) {
+        QueueDispatcher reader = dispatchers.add(location);
+        if (reader != null) {
+            dispatcher.dispatch();
         }
-        return reader;
     }
 
-    private boolean removeKey(String key, Party location) {
-        boolean reset = false;
-        Set<Party> locations = locationsByKey.get(key);
-        if (locations != null) {
-            locations.remove(location);
-            if (locations.isEmpty()) {
-                reset = true;
-            }
-        }
-        if (reset) {
-            QueueReader reader = readers.remove(key);
-            if (reader != null) {
-                reader.destroy();
-            }
-        }
-        return reset;
+    /**
+     * Invoked when a practice location is removed.
+     *
+     * @param location the practice location
+     */
+    private void locationRemoved(Party location) {
+        dispatchers.remove(location);
+        dispatcher.dispatch();
     }
 
 }
