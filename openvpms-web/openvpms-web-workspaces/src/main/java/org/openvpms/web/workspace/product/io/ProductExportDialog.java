304e32f9626c68dfe7bc6a3182336dc550edc89e
Tim Anderson <tma@netspace.net.au>
2018-02-05 02:45:55 +0000

    Fix for OVPMS-1872 Export Prices dialog shows all prices when Current selected
    svn path=/projects/openvpms/branches/2.0-dev/; revision=7768

--- openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/product/io/ProductExportDialog.java
+++ openvpms-web/openvpms-web-workspaces/src/main/java/org/openvpms/web/workspace/product/io/ProductExportDialog.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2016 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2018 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.workspace.product.io;
@@ -49,6 +49,7 @@ import org.openvpms.web.resource.i18n.Messages;
 import org.openvpms.web.system.ServiceHelper;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 
@@ -208,10 +209,11 @@ public class ProductExportDialog extends BrowserDialog<Product> {
             ProductExportQuery query = getQuery();
             ProductExportQuery.Prices prices = query.getPrices();
             if (prices == ProductExportQuery.Prices.CURRENT) {
+                Date now = new Date();
                 if (pricingGroup.isAll()) {
-                    result.addAll(rules.getProductPrices(product, shortName, includeLinkedPrices, pricingGroup));
+                    result.addAll(rules.getProductPrices(product, shortName, now, includeLinkedPrices, pricingGroup));
                 } else {
-                    List<ProductPrice> list = rules.getProductPrices(product, shortName, includeLinkedPrices,
+                    List<ProductPrice> list = rules.getProductPrices(product, shortName, now, includeLinkedPrices,
                                                                      pricingGroup);
                     if (!list.isEmpty()) {
                         result.add(list.get(0));
