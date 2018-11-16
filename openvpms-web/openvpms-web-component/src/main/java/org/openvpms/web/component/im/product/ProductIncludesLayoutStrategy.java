684b8c442bb6e248ba006b95d738c472be64096d
tanderson <tma@netspace.net.au>
2018-11-16 15:35:55 +1100

    Fix for OVPMS-2096 Maximum Weight field duplicated in Product Template includes editor

--- openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/product/ProductIncludesLayoutStrategy.java
+++ openvpms-web/openvpms-web-component/src/main/java/org/openvpms/web/component/im/product/ProductIncludesLayoutStrategy.java
@@ -11,7 +11,7 @@
  * for the specific language governing rights and limitations under the
  * License.
  *
- * Copyright 2016 (C) OpenVPMS Ltd. All Rights Reserved.
+ * Copyright 2018 (C) OpenVPMS Ltd. All Rights Reserved.
  */
 
 package org.openvpms.web.component.im.product;
@@ -59,7 +59,7 @@ public class ProductIncludesLayoutStrategy extends AbstractLayoutStrategy {
     public ComponentState apply(IMObject object, PropertySet properties, IMObject parent, LayoutContext context) {
         boolean useLocationProducts = ProductHelper.useLocationProducts(context.getContext());
         ArchetypeNodes nodes = new ArchetypeNodes();
-        nodes.exclude("source");
+        nodes.exclude("source", MAX_WEIGHT, WEIGHT_UNITS);
         nodes.simple("target");
         nodes.first("target");
         if (!useLocationProducts) {
